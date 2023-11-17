bd57e54935dd8157c448d0130accf8d1824215b6
RussiSunni <jonathandyason@hotmail.com>
2023-11-17 17:59:25 +0200

    ghj

---
+++ dist/assets/SettingsStore-7cf1fdaa.js
@@ -0,0 +1,1 @@
+import{d as s}from"./main-dae9e940.js";const i=s("settings",{state:()=>({settings:{}}),actions:{async getSettings(){const t=await(await fetch("/settings")).json();this.settings=t}}});export{i as u};
