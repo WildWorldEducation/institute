19ed1e633253b1091e543d4d6df751b5843a15e5
RussiSunni <jonathandyason@hotmail.com>
2023-11-23 13:44:49 +0200

    fghfgh

---
+++ dist/assets/SettingsStore-7ae8ea44.js
@@ -0,0 +1,1 @@
+import{d as s}from"./main-2af66977.js";const i=s("settings",{state:()=>({settings:{}}),actions:{async getSettings(){const t=await(await fetch("/settings")).json();this.settings=t}}});export{i as u};
---
+++ dist/assets/TagsStore-ed791829.js
@@ -0,0 +1,1 @@
+import{d as e}from"./main-2af66977.js";const o=e("tags",{state:()=>({tagsList:[]}),actions:{async getTagsList(){const t=await(await fetch("/tags/list")).json();this.tagsList=t},async deleteTag(s){console.log(s);const t=fetch("/tags/"+s,{method:"DELETE"});t.error&&console.log(t.error),this.getTagsList()}}});export{o as u};
