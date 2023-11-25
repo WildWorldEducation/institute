3d09ea21d040420d6902b8247b6f3248fc6efcfb
RussiSunni <jonathandyason@hotmail.com>
2023-11-25 14:49:09 +0200

    asd

--- src/components/components/Assessment.vue
+++ src/components/components/Assessment.vue
@@ -19,9 +19,8 @@ export default {
             mcQuestions: [],
             essayQuestions: [],
             questions: [],
+            question: {},
             questionNumber: 0,
-            // To wait for questions to be loaded, before the DOM renders.
-            isFetching: true,
             score: 0,
             assessmentId: null,
             numMCQuestions: 0,
@@ -33,25 +32,25 @@ export default {
     async created() {
         // Get user skills, in case this is a sub skill. We have to check its siblings.     
         // Need to get the questions for the quiz, before the DOM renders.
-        const fetchMCQuestions = async () => {
-            await fetch('/questions/' + this.skillId + '/multiple-choice')
+        await this.fetchMCQuestions()
+        await this.fetchEssayQuestions();
+    },
+    methods: {
+        fetchMCQuestions() {
+            fetch('/questions/' + this.skillId + '/multiple-choice')
                 .then(function (response) {
                     return response.json();
                 }).then(data => this.mcQuestions = data)
                 .then(() => {
-                    // Add field to elements, for the user's answer and for the question type.
+                    // Add field to elements, for the user's answer.
                     for (let i = 0; i < this.mcQuestions.length; i++) {
                         this.mcQuestions[i].userAnswer = null
                         this.mcQuestions[i].questionType = "mc"
                     }
-                    // To allow the questions to render in the DOM.
-                    this.isFetching = false;
                 })
-        }
-        await fetchMCQuestions();
-
-        const fetchEssayQuestions = async () => {
-            await fetch('/questions/' + this.skillId + '/essay')
+        },
+        fetchEssayQuestions() {
+            fetch('/questions/' + this.skillId + '/essay')
                 .then(function (response) {
                     return response.json();
                 }).then(data => this.essayQuestions = data)
@@ -61,27 +60,24 @@ export default {
                         this.essayQuestions[i].userAnswer = null
                         this.essayQuestions[i].questionType = "essay"
                     }
-                    // To allow the questions to render in the DOM.
-                    this.isFetching = false;
+                }).then(() => {
+                    this.questions = this.essayQuestions.concat(this.mcQuestions);
+                    // Shuffle array
+                    this.questions = this.questions.sort((a, b) => 0.5 - Math.random());
+                    this.question = this.questions[0]
+
+                    // Calculate the total num of questions.
+                    // At the moment, each question is 1 mark, so we get the total score from this.
+                    this.totalNumOfQuestions = this.questions.length;
                 })
-        }
-        await fetchEssayQuestions();
-
-        this.questions = this.essayQuestions.concat(this.mcQuestions);
-        // Shuffle array
-        this.questions = this.questions.sort((a, b) => 0.5 - Math.random());
-
-        // Calculate the total num of questions.
-        // At the moment, each question is 1 mark, so we get the total score from this.
-        this.totalNumOfQuestions = this.questions.length;
-    },
-
-    methods: {
+        },
         Next() {
             this.questionNumber++;
+            this.question = this.questions[this.questionNumber]
         },
         Previous() {
             this.questionNumber--;
+            this.question = this.questions[this.questionNumber]
         },
         Submit() {
             // Mark the MC questions (if there are any).
@@ -89,7 +85,6 @@ export default {
                 // Tally the score.
                 if (this.questions[i].questionType == 'mc') {
                     this.numMCQuestions++
-
                     if (this.questions[i].userAnswer == this.questions[i].correct_answer) {
                         this.score++
                     }
@@ -102,9 +97,9 @@ export default {
             // If there are no essay questions, requiring manual marking, mark the test now.
             if (this.essayQuestions.length == 0) {
                 // Pass mark of 90%.
-                if ((this.score / this.numMCQuestions.length) * 100 >= 90) {
+                if ((this.score / this.numMCQuestions) * 100 >= 90) {
                     // Make skill mastered for this student.                    
-                    this.userSkillsStore.MakeMastered(this.userDetailsStore.userId, skillId)
+                    this.MakeMastered(this.skillId)
                     alert("Well done! You have now mastered this skill.")
                 }
                 else {
@@ -189,20 +184,19 @@ export default {
 </script>
 
 <template>
-    <div class="container mt-3">
+    <div class="container mt-3 mb-3">
         <button @click="TestPass()" class="btn green-btn me-2">Test Pass</button>
         <!-- To wait for questions to be loaded, before the DOM renders. -->
-        <div v-if="!isFetching" class="row mt-3">
-            <h2>Question {{ this.questionNumber + 1 }}</h2>
-            <div class="mb-3">{{ this.questions[this.questionNumber].question }}</div>
-            <div v-if="this.questions[this.questionNumber].questionType == 'mc'">
+        <div class="row mt-3">
+            <h3>Question {{ this.questionNumber + 1 }}: {{ question.question }}</h3>
+            <div v-if="this.question.questionType == 'mc'">
                 <!-- Option 1. -->
                 <div class="form-check">
                     <input @input="UserAnswer()" class="form-check-input" type="radio" value="1"
                         v-model="questions[this.questionNumber].userAnswer">
                     <label class="form-check-label">
                         {{
-                            questions[this.questionNumber].answer_1 }}
+                            question.answer_1 }}
                     </label>
                 </div>
                 <!-- Option 2. -->
@@ -211,7 +205,7 @@ export default {
                         v-model="questions[this.questionNumber].userAnswer">
                     <label class="form-check-label">
                         {{
-                            questions[this.questionNumber].answer_2 }}
+                            question.answer_2 }}
                     </label>
                 </div>
                 <!-- Option 3. -->
@@ -220,7 +214,7 @@ export default {
                         v-model="questions[this.questionNumber].userAnswer">
                     <label class="form-check-label">
                         {{
-                            questions[this.questionNumber].answer_3 }}
+                            question.answer_3 }}
                     </label>
                 </div>
                 <!-- Option 4. -->
@@ -229,7 +223,7 @@ export default {
                         v-model="questions[this.questionNumber].userAnswer">
                     <label class="form-check-label">
                         {{
-                            questions[this.questionNumber].answer_4 }}
+                            question.answer_4 }}
                     </label>
                 </div>
                 <!-- Option 5. -->
@@ -238,23 +232,24 @@ export default {
                         v-model="questions[this.questionNumber].userAnswer">
                     <label class="form-check-label">
                         {{
-                            questions[this.questionNumber].answer_5 }}
+                            question.answer_5 }}
                     </label>
                 </div>
             </div>
-            <div v-else-if="this.questions[this.questionNumber].questionType == 'essay'">
+            <div v-else-if="this.question.questionType == 'essay'">
                 <div class="form-group">
                     <textarea @input="UserAnswer()" class="form-control" v-model="questions[this.questionNumber].userAnswer"
                         rows="3"></textarea>
                 </div>
             </div>
         </div>
-        <div class="mt-3 mb-2 d-flex justify-content-end">
+
+        <div class="mt-3 d-flex justify-content-end">
             <button v-if="this.questionNumber > 0" @click="Previous()" class="btn green-btn me-2">Previous</button>
             <button v-if="this.questionNumber != questions.length - 1" @click="Next()" class="btn green-btn">Next</button>
-            <button disabled v-if="this.questionNumber == questions.length - 1 && !isAllQuestionsAnswered" @click="Submit()"
-                class="btn green-btn">Submit</button>
-            <button v-else-if="this.questionNumber == questions.length - 1 && isAllQuestionsAnswered" @click="Submit()"
+            <!-- <button disabled v-if="this.questionNumber == questions.length - 1 && !isAllQuestionsAnswered" @click="Submit()"
+                class="btn green-btn">Submit</button> -->
+            <button v-if="this.questionNumber == questions.length - 1" @click="Submit()"
                 class="btn green-btn">Submit</button>
         </div>
     </div>
