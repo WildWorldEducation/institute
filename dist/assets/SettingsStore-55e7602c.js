9e44ac64fdac818c5e0fa2d75a49357b5c4bc026
RussiSunni <jonathandyason@hotmail.com>
2023-12-10 13:14:52 +0200

    asd

---
+++ dist/assets/SettingsStore-55e7602c.js
@@ -0,0 +1,1 @@
+import{d as s}from"./main-b39c1666.js";const i=s("settings",{state:()=>({settings:{}}),actions:{async getSettings(){const t=await(await fetch("/settings")).json();this.settings=t}}});export{i as u};
--- dist/assets/SkillTreeStore-75642515.js
+++
@@ -1,1 +0,0 @@
-import{d as t,u as r}from"./main-b55e7e7e.js";const a=t("skillTree",{state:()=>({userSkills:[]}),actions:{async getUserSkills(){const s=await r().getUserDetails(),e=await fetch("/user-skills/"+s.userId);this.userSkills=await e.json()}}});export{a as u};
