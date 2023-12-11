a64600b9db2ff06a50e55cc694e6b7a15e138e0c
RussiSunni <jonathandyason@hotmail.com>
2023-12-11 17:33:04 +0200

    l;'

---
+++ dist/assets/InstructorStudentsStore-820d55a3.js
@@ -0,0 +1,1 @@
+import{d as s}from"./main-b3d53869.js";const e=s("instructorStudents",{state:()=>({instructorStudentsList:[]}),actions:{async getInstructorStudentsList(){const t=await(await fetch("/instructor-students/list")).json();return this.instructorStudentsList=t,this.$state}}});export{e as u};
--- dist/assets/SettingsStore-55e7602c.js
+++
@@ -1,1 +0,0 @@
-import{d as s}from"./main-b39c1666.js";const i=s("settings",{state:()=>({settings:{}}),actions:{async getSettings(){const t=await(await fetch("/settings")).json();this.settings=t}}});export{i as u};
