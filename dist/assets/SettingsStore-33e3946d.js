f050e15d8fb1feed1e196301738d75e45abd718a
RussiSunni <jonathandyason@hotmail.com>
2024-01-03 17:58:00 +0200

    ghj

--- dist/assets/SettingsStore-33e3946d.js
+++
@@ -1,1 +0,0 @@
-import{d as s}from"./main-762f2a6e.js";const i=s("settings",{state:()=>({settings:{}}),actions:{async getSettings(){const t=await(await fetch("/settings")).json();this.settings=t}}});export{i as u};
---
+++ dist/assets/SettingsStore-886bfec0.js
@@ -0,0 +1,1 @@
+import{d as s}from"./main-c2800e64.js";const i=s("settings",{state:()=>({settings:{}}),actions:{async getSettings(){const t=await(await fetch("/settings")).json();this.settings=t}}});export{i as u};
