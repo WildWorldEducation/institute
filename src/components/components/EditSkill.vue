4a10b2fa1a03e83c7e6ecc815e88ff80269f23a6
RussiSunni <jonathandyason@hotmail.com>
2024-01-05 12:01:31 +0200

    sdf

--- src/components/components/EditSkill.vue
+++ src/components/components/EditSkill.vue
@@ -63,12 +63,13 @@ export default {
     methods: {
         getParentSkills() {
             for (let i = 0; i < this.skillsStore.skillsList.length; i++) {
-                if (this.skillsStore.skillsList[i].type == 'super') {
-                    this.superSkills.push(this.skillsStore.skillsList[i])
-                }
-                if (this.skillsStore.skillsList[i].id != this.skillId
-                    && this.skillsStore.skillsList[i].type != 'sub') {
-                    this.skills.push(this.skillsStore.skillsList[i])
+                if (this.skillsStore.skillsList[i].id != this.skillId) {
+                    if (this.skillsStore.skillsList[i].type == 'super') {
+                        this.superSkills.push(this.skillsStore.skillsList[i])
+                    }
+                    if (this.skillsStore.skillsList[i].type != 'sub') {
+                        this.skills.push(this.skillsStore.skillsList[i])
+                    }
                 }
             }
 
