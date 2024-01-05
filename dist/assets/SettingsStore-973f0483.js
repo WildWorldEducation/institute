d8ebc3d79fb5687b3c24a0c49b2ae8f87798f627
RussiSunni <jonathandyason@hotmail.com>
2024-01-05 14:26:24 +0200

    asd

--- dist/assets/SettingsStore-973f0483.js
+++
@@ -1,1 +0,0 @@
-import{d as s}from"./main-c4cbdd79.js";const i=s("settings",{state:()=>({settings:{}}),actions:{async getSettings(){const t=await(await fetch("/settings")).json();this.settings=t}}});export{i as u};
--- dist/assets/TagsStore-cef53ae8.js
+++
@@ -1,1 +0,0 @@
-import{d as e}from"./main-c4cbdd79.js";const o=e("tags",{state:()=>({tagsList:[]}),actions:{async getTagsList(){const t=await(await fetch("/tags/list")).json();this.tagsList=t},async deleteTag(s){console.log(s);const t=fetch("/tags/"+s,{method:"DELETE"});t.error&&console.log(t.error),this.getTagsList()}}});export{o as u};
