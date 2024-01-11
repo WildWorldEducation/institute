7404af3b586f003f5d91167e55a825d238c0844c
Taan <tannguyen1220@gmail.com>
2024-01-12 06:44:21 +0700

    fix padding issue

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,9 @@
 <script>
-import { RouterLink, RouterView } from 'vue-router'
+import { RouterLink, RouterView } from 'vue-router';
 
 // Import the store.
-import { useSessionDetailsStore } from './stores/SessionDetailsStore.js'
-import { useUserDetailsStore } from './stores/UserDetailsStore.js'
+import { useSessionDetailsStore } from './stores/SessionDetailsStore.js';
+import { useUserDetailsStore } from './stores/UserDetailsStore.js';
 
 export default {
   setup() {
@@ -11,11 +11,11 @@ export default {
     const userDetailsStore = useUserDetailsStore();
 
     return {
-      sessionDetailsStore, userDetailsStore
-    }
-  }
-}
-
+      sessionDetailsStore,
+      userDetailsStore,
+    };
+  },
+};
 </script>
 
 <template>
@@ -23,17 +23,26 @@ export default {
     <nav id="navbar" class="navbar navbar-expand-lg navbar-light bg-light">
       <div class="container-fluid">
         <RouterLink to="/" class="nav-link">
-          <img src="/images/logo-red.png" alt="" width="50" height="50">
+          <img src="/images/logo-red.png" alt="" width="50" height="50" />
         </RouterLink>
         <span class="navbar-brand">The Collins Institute</span>
-        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent"
-          aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+        <button
+          class="navbar-toggler"
+          type="button"
+          data-bs-toggle="collapse"
+          data-bs-target="#navbarSupportedContent"
+          aria-controls="navbarSupportedContent"
+          aria-expanded="false"
+          aria-label="Toggle navigation"
+        >
           <span class="navbar-toggler-icon"></span>
         </button>
         <div class="collapse navbar-collapse" id="navbarSupportedContent">
           <ul class="navbar-nav me-auto mb-2 mb-lg-0">
             <li v-if="userDetailsStore.role == 'student'" class="nav-item">
-              <RouterLink to="/skill-tree" class="nav-link">Skill Tree</RouterLink>
+              <RouterLink to="/skill-tree" class="nav-link"
+                >Skill Tree</RouterLink
+              >
             </li>
             <li class="nav-item">
               <RouterLink to="/skills" class="nav-link">Skills</RouterLink>
@@ -41,13 +50,21 @@ export default {
             <li v-if="userDetailsStore.role != 'student'" class="nav-item">
               <RouterLink to="/users" class="nav-link">
                 <span v-if="userDetailsStore.role == 'admin'">Users</span>
-                <span v-else-if="userDetailsStore.role == 'instructor'">Students</span>
+                <span v-else-if="userDetailsStore.role == 'instructor'"
+                  >Students</span
+                >
               </RouterLink>
             </li>
           </ul>
           <ul class="navbar-nav d-flex">
             <li class="nav-item me-2">
-              <RouterLink to="/profile-settings" class="nav-link">Profile</RouterLink>
+              <RouterLink to="/profile-settings" class="nav-link">
+                <img
+                  id="profile-image"
+                  v-bind:src="userDetailsStore.avatar"
+                  alt="profile image"
+                />
+              </RouterLink>
             </li>
           </ul>
         </div>
@@ -65,12 +82,17 @@ export default {
   font-size: 16px;
   margin-left: 20px;
   margin-right: 50px;
-  ;
 }
 
 .nav-link {
-  color: #A48BE5;
+  color: #a48be5;
   font-weight: 700;
   font-size: 16px;
+}
+
+#profile-image {
+  border-radius: 50%;
+  width: 40px;
+  height: 40px;
 }
 </style>
