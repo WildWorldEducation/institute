0956968aeb79ccb3080482ab6f6ac932ba711239
RussiSunni <jonathandyason@hotmail.com>
2024-01-17 12:34:10 +0200

    Post login page styling

---
+++ dist/assets/SettingsStore-b73119f4.js
@@ -0,0 +1,1 @@
+import{d as s}from"./main-06da496c.js";const i=s("settings",{state:()=>({settings:{}}),actions:{async getSettings(){const t=await(await fetch("/settings")).json();this.settings=t}}});export{i as u};
