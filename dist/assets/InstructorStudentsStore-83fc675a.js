f59e0cf9216c09d9677e4ea3e0abdc49fa8dde1e
RussiSunni <jonathandyason@hotmail.com>
2024-01-17 10:55:04 +0200

    skill tree level filter - incomplete

--- dist/assets/InstructorStudentsStore-83fc675a.js
+++
@@ -1,1 +0,0 @@
-import{d as s}from"./main-f3d80ee5.js";const e=s("instructorStudents",{state:()=>({instructorStudentsList:[]}),actions:{async getInstructorStudentsList(){const t=await(await fetch("/instructor-students/list")).json();return this.instructorStudentsList=t,this.$state}}});export{e as u};
