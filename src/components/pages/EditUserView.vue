c3f82d808b343790aa6d0f649ee0a6361e833fda
RussiSunni <jonathandyason@hotmail.com>
2024-01-20 13:54:51 +0200

    edit user styling

--- src/components/pages/EditUserView.vue
+++ src/components/pages/EditUserView.vue
@@ -1,10 +1,12 @@
 <script setup>
-import EditUser from '../components/EditUser.vue'
+import EditUser from '../components/EditUser.vue';
 </script>
 
 <template>
+    <div id="banner">
+        <img src="/images/banners/edit-mastery-skill-banner.png" class="image-fluid" />
+    </div>
     <EditUser />
 </template>
-  
+
 <style></style>
-  
