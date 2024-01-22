eef18d4ee5487e5476989ca0ec1c710d0ae7f321
RussiSunni <jonathandyason@hotmail.com>
2024-01-22 16:06:59 +0200

    skill tree fix

---
+++ dist/assets/SettingsStore-8ae5bde1.js
@@ -0,0 +1,1 @@
+import{d as s}from"./main-02eb85dc.js";const i=s("settings",{state:()=>({settings:{}}),actions:{async getSettings(){const t=await(await fetch("/settings")).json();this.settings=t}}});export{i as u};
