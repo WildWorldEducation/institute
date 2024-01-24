da3fe010a54d6c411450dd6da817313fb0e6a077
RussiSunni <jonathandyason@hotmail.com>
2024-01-24 12:09:03 +0200

    dfg

---
+++ dist/assets/SettingsStore-87ceb8ef.js
@@ -0,0 +1,1 @@
+import{h as s}from"./main-1c0dc890.js";const i=s("settings",{state:()=>({settings:{}}),actions:{async getSettings(){const t=await(await fetch("/settings")).json();this.settings=t}}});export{i as u};
--- dist/assets/SettingsStore-fe8f9d9c.js
+++
@@ -1,1 +0,0 @@
-import{h as s}from"./main-d6aac19c.js";const i=s("settings",{state:()=>({settings:{}}),actions:{async getSettings(){const t=await(await fetch("/settings")).json();this.settings=t}}});export{i as u};
