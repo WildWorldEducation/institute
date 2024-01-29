2a06e20a80709226de7f10fd5bc0ebc4c2f6d7e4
Taan <tannguyen1220@gmail.com>
2024-01-29 15:19:06 +0700

    profile view phone and tablet view

--- src/components/components/EditProfile.vue
+++ src/components/components/EditProfile.vue
@@ -1,41 +1,48 @@
 <script>
-import router from "../../router";
+import router from '../../router';
 // Import the stores.
-import { useUserDetailsStore } from '../../stores/UserDetailsStore'
+import { useUserDetailsStore } from '../../stores/UserDetailsStore';
 
 export default {
     setup() {
         const userDetailsStore = useUserDetailsStore();
+        const showObj = { ...userDetailsStore, avatar: '1' };
+        console.log(JSON.stringify(showObj));
         return {
             userDetailsStore
-        }
+        };
     },
     data() {
         return {
             userName: this.userDetailsStore.userName,
             skillTreeTheme: this.userDetailsStore.skillTreeTheme,
             avatar: this.userDetailsStore.avatar,
+            email: this.userDetailsStore.email,
+            password: this.userDetailsStore.password,
             image: ''
-        }
+        };
     },
-    computed: {
-
-    },
+    computed: {},
     methods: {
         ValidateForm() {
-            if (this.userDetailsStore.userName == "" || this.userDetailsStore.userName == null) {
-                alert("Please add a username.")
-            }
-            else {
-                this.userDetailsStore.updateProfile(this.userName, this.skillTreeTheme, this.avatar);
-                this.$router.push("/profile-settings");
+            if (
+                this.userDetailsStore.userName == '' ||
+                this.userDetailsStore.userName == null
+            ) {
+                alert('Please add a username.');
+            } else {
+                this.userDetailsStore.updateProfile(
+                    this.userName,
+                    this.skillTreeTheme,
+                    this.avatar
+                );
+                this.$router.push('/profile-settings');
             }
         },
         // For image upload.
         onFileChange(e) {
             var files = e.target.files || e.dataTransfer.files;
-            if (!files.length)
-                return;
+            if (!files.length) return;
             this.createImage(files[0]);
         },
         createImage(file) {
@@ -49,40 +56,54 @@ export default {
                 this.avatar = this.image;
             };
             reader.readAsDataURL(file);
-
         },
         removeImage: function (e) {
             this.image = '';
             this.avatar = this.image;
         }
     }
-}
-</script> 
+};
+</script>
 
 <template>
     <div class="container mt-3">
         <h1>Edit Profile</h1>
         <div class="mb-3">
             <label class="form-label">Username</label>
-            <input v-model="userName" type="text" class="form-control">
+            <input v-model="userName" type="text" class="form-control" />
         </div>
         <div class="mb-3 row">
             <label for="avatar" class="form-label">Avatar</label>
             <div v-if="!image">
-                <input class="form-control" type="file" accept="image/*" @change="onFileChange">
+                <input
+                    class="form-control"
+                    type="file"
+                    accept="image/*"
+                    @change="onFileChange"
+                />
                 <p style="font-size: 14px"><em>Maximum file size 15mb</em></p>
             </div>
             <div v-else>
-                <p><img :src="image" height="200" style="background-color: lightgrey" /></p>
-                <p><button class="btn btn-warning" @click="removeImage">Remove image</button></p>
+                <p>
+                    <img
+                        :src="image"
+                        height="200"
+                        style="background-color: lightgrey"
+                    />
+                </p>
+                <p>
+                    <button class="btn btn-warning" @click="removeImage">
+                        Remove image
+                    </button>
+                </p>
             </div>
         </div>
         <div class="mb-3">
-            <label class="form-label">Theme</label><br>
-            <input type="radio" value="light" v-model="skillTreeTheme">
-            <label for="light">Light</label><br>
-            <input type="radio" value="dark" v-model="skillTreeTheme">
-            <label for="dark">Dark</label><br>
+            <label class="form-label">Theme</label><br />
+            <input type="radio" value="light" v-model="skillTreeTheme" />
+            <label for="light">Light</label><br />
+            <input type="radio" value="dark" v-model="skillTreeTheme" />
+            <label for="dark">Dark</label><br />
         </div>
         <div class="d-flex justify-content-between mb-3">
             <router-link class="btn btn-dark" to="/profile-settings">
@@ -93,5 +114,4 @@ export default {
     </div>
 </template>
 
-
-<style scoped></style> 
+<style scoped></style>
