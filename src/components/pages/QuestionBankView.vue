264d57d39fbedb8771dbc5d550ec2ec2fe86d92d
RussiSunni <jonathandyason@hotmail.com>
2024-01-29 11:57:35 +0200

    question bank styling

--- src/components/pages/QuestionBankView.vue
+++ src/components/pages/QuestionBankView.vue
@@ -1,77 +1,91 @@
 <script>
-import QuestionsBankQuestionList from '../components/QuestionsBankQuestionList.vue'
-import { useSkillsStore } from '../../stores/SkillsStore.js';
+import QuestionsBankQuestionList from "../components/QuestionsBankQuestionList.vue";
+import { useSkillsStore } from "../../stores/SkillsStore.js";
 
 export default {
-    setup() {
-        const skillsStore = useSkillsStore();
-        return {
-            skillsStore
-        }
-    },
-    data() {
-        return {
-            skillId: this.$route.params.id,
-            skillName: null,
-            isMultipleChoice: true,
-            isEssay: true,
-        }
-    },
-    async created() {
-        if (this.skillsStore.skillsList.length == 0) {
-            await this.skillsStore.getSkillsList()
-        }
-
-        for (let i = 0; i < this.skillsStore.skillsList.length; i++) {
-            if (this.skillId == this.skillsStore.skillsList[i].id) {
-                this.skillName = this.skillsStore.skillsList[i].name;
-            }
-        }
-    },
-    components: {
-        QuestionsBankQuestionList
+  setup() {
+    const skillsStore = useSkillsStore();
+    return {
+      skillsStore,
+    };
+  },
+  data() {
+    return {
+      skillId: this.$route.params.id,
+      skillName: null,
+      isMultipleChoice: true,
+      isEssay: true,
+    };
+  },
+  async created() {
+    if (this.skillsStore.skillsList.length == 0) {
+      await this.skillsStore.getSkillsList();
     }
-}
+
+    for (let i = 0; i < this.skillsStore.skillsList.length; i++) {
+      if (this.skillId == this.skillsStore.skillsList[i].id) {
+        this.skillName = this.skillsStore.skillsList[i].name;
+      }
+    }
+  },
+  components: {
+    QuestionsBankQuestionList,
+  },
+};
 </script>
 
 <template>
-    <h1>{{ skillName }} Question Bank</h1>
-    <div class="topnav d-flex">
-        <div class="d-flex">
-            <div class="form-check">
-                <input class="form-check-input" type="checkbox" value="true" v-model="isMultipleChoice">
-                <label class="form-check-label">
-                    Multiple choice
-                </label>
-            </div>
-            &nbsp;&nbsp;&nbsp;&nbsp;
-            <div class="form-check">
-                <input class="form-check-input" type="checkbox" value="true" v-model="isEssay">
-                <label class="form-check-label">
-                    Essay
-                </label>
-            </div>
-        </div>
-        <router-link class="purple-btn btn" :to="'/skills/' + skillId + '/question-bank/add'">Add
-            Questions</router-link>
+  <h1>{{ skillName }} Question Bank</h1>
+  <div class="topnav d-flex">
+    <div class="d-flex">
+      <div class="form-check">
+        <input
+          class="form-check-input"
+          type="checkbox"
+          value="true"
+          v-model="isMultipleChoice"
+        />
+        <label class="form-check-label"> Multiple choice </label>
+      </div>
+      &nbsp;&nbsp;&nbsp;&nbsp;
+      <div class="form-check">
+        <input
+          class="form-check-input"
+          type="checkbox"
+          value="true"
+          v-model="isEssay"
+        />
+        <label class="form-check-label"> Essay </label>
+      </div>
     </div>
-    <QuestionsBankQuestionList :isMultipleChoice="isMultipleChoice" :isEssay="isEssay" />
+    <router-link
+      class="purple-btn btn"
+      :to="'/skills/' + skillId + '/question-bank/add'"
+      >Add Questions</router-link
+    >
+  </div>
+  <QuestionsBankQuestionList
+    :isMultipleChoice="isMultipleChoice"
+    :isEssay="isEssay"
+  />
+  <div class="d-flex justify-content-between">
+    <a class="btn btn-dark" @click="$router.go(-1)">Cancel</a>
+  </div>
 </template>
-  
+
 <style>
 .purple-btn {
-    background-color: #A48BE6;
-    color: white;
-    border: 1px solid #7F56D9;
-    font-family: 'Inter', sans-serif;
-    font-weight: 600;
-    font-size: 16px;
-    line-height: 24px;
-    max-width: fit-content;
-    height: 44px;
-    display: flex;
-    align-items: center;
-    margin-left: auto;
+  background-color: #a48be6;
+  color: white;
+  border: 1px solid #7f56d9;
+  font-family: "Inter", sans-serif;
+  font-weight: 600;
+  font-size: 16px;
+  line-height: 24px;
+  max-width: fit-content;
+  height: 44px;
+  display: flex;
+  align-items: center;
+  margin-left: auto;
 }
-</style>    
-  
+</style>
