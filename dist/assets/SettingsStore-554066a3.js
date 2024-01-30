c49cc296f5d53b901fd2d8755e34760936e3571f
RussiSunni <jonathandyason@hotmail.com>
2024-01-30 12:22:39 +0200

    show subskills d3

---
+++ dist/assets/SettingsStore-554066a3.js
@@ -0,0 +1,1 @@
+import{h as s}from"./main-8ebb6752.js";const i=s("settings",{state:()=>({settings:{}}),actions:{async getSettings(){const t=await(await fetch("/settings")).json();this.settings=t}}});export{i as u};
