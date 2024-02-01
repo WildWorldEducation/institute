834c5fc1b0b7b63bde26b5984f66895cb670be0c
RussiSunni <jonathandyason@hotmail.com>
2024-02-01 17:54:15 +0200

    dfg

---
+++ dist/assets/EditAssessmentView-83bb7850.js
@@ -0,0 +1,1 @@
+import{_ as e}from"./main-14cc4b8f.js";const s={};function t(n,r){return" edit assessment "}const _=e(s,[["render",t]]);export{_ as default};
---
+++ dist/assets/SettingsStore-984617cf.js
@@ -0,0 +1,1 @@
+import{h as s}from"./main-14cc4b8f.js";const i=s("settings",{state:()=>({settings:{}}),actions:{async getSettings(){const t=await(await fetch("/settings")).json();this.settings=t}}});export{i as u};
