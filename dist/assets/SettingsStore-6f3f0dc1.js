c673f7df6623af9a4da022635a99e765463d4735
RussiSunni <jonathandyason@hotmail.com>
2024-02-01 16:50:23 +0200

    sdf

--- dist/assets/SettingsStore-6f3f0dc1.js
+++
@@ -1,1 +0,0 @@
-import{h as s}from"./main-9e950be5.js";const i=s("settings",{state:()=>({settings:{}}),actions:{async getSettings(){const t=await(await fetch("/settings")).json();this.settings=t}}});export{i as u};
