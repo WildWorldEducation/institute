65b9f3f0011d4ec4506021ba48e575bbfbcdfe7e
RussiSunni <jonathandyason@hotmail.com>
2024-02-05 14:41:47 +0200

    new csv format

--- dist/assets/SettingsStore-775b3334.js
+++
@@ -1,1 +0,0 @@
-import{h as s}from"./main-1d0d88d5.js";const i=s("settings",{state:()=>({settings:{}}),actions:{async getSettings(){const t=await(await fetch("/settings")).json();this.settings=t}}});export{i as u};
