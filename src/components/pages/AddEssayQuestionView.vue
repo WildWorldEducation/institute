957764b2834db0ba48dc7d04794a54d289c8e6ce
RussiSunni <jonathandyason@hotmail.com>
2024-02-05 12:24:27 +0200

    Add questions manually

---
+++ src/components/pages/AddEssayQuestionView.vue
@@ -0,0 +1,108 @@
+<script>
+import router from '../../router';
+
+export default {
+    data() {
+        return {
+            skillId: this.$route.params.skillId,
+            question: {}
+        };
+    },
+    methods: {
+        Submit() {
+            const requestOptions = {
+                method: 'POST',
+                headers: { 'Content-Type': 'application/json' },
+                body: JSON.stringify({
+                    name: this.question.name,
+                    question: this.question.question,
+                    skill_id: this.skillId
+                })
+            };
+            var url = '/questions/essay-questions/add';
+            fetch(url, requestOptions)
+                .then(() => {
+                    alert('Question added');
+                })
+                .then(() => {
+                    this.$router.go(-1);
+                });
+        }
+    }
+};
+</script>
+
+<template>
+    <div class="container mt-3 pb-3">
+        <div class="row">
+            <div class="col-10 d-flex align-items-end">
+                <h2 id="header-tile">Add Question</h2>
+            </div>
+        </div>
+        <div class="main-content-container container-fluid p-4">
+            <div class="row">
+                <div class="col-lg-6">
+                    <div class="mb-3">
+                        <label for="first_name" class="form-label"
+                            >Question Name</label
+                        >
+                        <input
+                            v-model="question.name"
+                            type="text"
+                            class="form-control"
+                        />
+                    </div>
+                    <div class="mb-3">
+                        <label for="last_name" class="form-label"
+                            >Question</label
+                        >
+                        <input
+                            v-model="question.question"
+                            type="text"
+                            class="form-control"
+                        />
+                    </div>
+
+                    <div class="d-flex justify-content-end gap-4">
+                        <a class="btn red-btn" @click="$router.go(-1)"
+                            >Cancel</a
+                        >
+                        <button class="btn purple-btn" @click="Submit()">
+                            Submit
+                        </button>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</template>
+
+<style>
+.red-btn {
+    background-color: #e24d4d;
+    color: white;
+    border: 1px solid #d33622;
+    font-family: 'Poppins', sans-serif;
+    font-weight: 600;
+    font-size: 16px;
+    line-height: 24px;
+    display: flex;
+    align-items: center;
+}
+
+.red-btn:hover {
+    background-color: #cc3535;
+}
+
+.purple-btn {
+    background-color: #a48be6;
+    color: white;
+    border: 1px solid #7f56d9;
+    font-family: 'Poppins', sans-serif;
+    font-weight: 600;
+    font-size: 16px;
+    line-height: 24px;
+    display: flex;
+    align-items: center;
+}
+</style>
--- src/components/pages/AddQuestionView.vue
+++ src/components/pages/AddQuestionView.vue
@@ -1,14 +1,15 @@
 <script setup>
-import AddMCQuestion from "../components/AddMCQuestion.vue";
-import AddEssayQuestion from "../components/AddEssayQuestion.vue";
+import AddMCQuestion from '../components/AddMCQuestion.vue';
+import AddEssayQuestion from '../components/AddEssayQuestion.vue';
 </script>
 
 <template>
-  <AddMCQuestion />
-  <AddEssayQuestion />
-  <div class="d-flex justify-content-between">
-    <a class="btn btn-dark" @click="$router.go(-1)">Cancel</a>
-  </div>
+    <AddMCQuestion />
+    <p>&nbsp;</p>
+    <AddEssayQuestion />
+    <div class="d-flex justify-content-between">
+        <a class="btn btn-dark" @click="$router.go(-1)">Cancel</a>
+    </div>
 </template>
 
 <style></style>
