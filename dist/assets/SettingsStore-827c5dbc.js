2ec4f715f0ed3338afefbb780fb9f1a5a2c5158f
RussiSunni <jonathandyason@hotmail.com>
2024-02-05 15:04:48 +0200

    accept txt files for questions

---
+++ dist/assets/SettingsStore-827c5dbc.js
@@ -0,0 +1,1 @@
+import{h as s}from"./main-2dd493f4.js";const i=s("settings",{state:()=>({settings:{}}),actions:{async getSettings(){const t=await(await fetch("/settings")).json();this.settings=t}}});export{i as u};
