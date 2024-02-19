a81a26f9e7e74717e4b682c387d43836ad3e614d
RussiSunni <jonathandyason@hotmail.com>
2024-02-19 03:18:33 +0200

    asd

--- dist/assets/UsersStore-6baa8d81.js
+++
@@ -1,1 +0,0 @@
-import{h as r}from"./main-db5376a2.js";const u=r("users",{state:()=>({users:[]}),actions:{async getUsers(){const s=await(await fetch("/users/list")).json();this.users=s},async deleteUser(e){this.users=this.users.filter(t=>t.id!==e);const s=fetch("/users/"+e,{method:"DELETE"});s.error&&console.log(s.error)}}});export{u};
