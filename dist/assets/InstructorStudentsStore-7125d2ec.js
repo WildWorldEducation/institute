83ab20db9d41e99677064bb93e039d7a09ec5bef
RussiSunni <jonathandyason@hotmail.com>
2024-03-03 13:17:46 +0200

    Truncated version of mastery requirements to skill preview panel

--- dist/assets/InstructorStudentsStore-7125d2ec.js
+++
@@ -1,1 +0,0 @@
-import{R as s}from"./main-01f320ec.js";const e=s("instructorStudents",{state:()=>({instructorStudentsList:[]}),actions:{async getInstructorStudentsList(){const t=await(await fetch("/instructor-students/list")).json();return this.instructorStudentsList=t,this.$state}}});export{e as u};
