b48a5913b580d4e944613310f3917fdd14871df1
RussiSunni <jonathandyason@hotmail.com>
2024-03-04 13:44:05 +0200

    print radial tree

---
+++ dist/assets/InstructorStudentsStore-89d8ce74.js
@@ -0,0 +1,1 @@
+import{R as s}from"./main-51d14fc5.js";const e=s("instructorStudents",{state:()=>({instructorStudentsList:[]}),actions:{async getInstructorStudentsList(){const t=await(await fetch("/instructor-students/list")).json();return this.instructorStudentsList=t,this.$state}}});export{e as u};
