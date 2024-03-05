3f1df55293d574bae92ade450d3fd8c32db8f478
RussiSunni <jonathandyason@hotmail.com>
2024-03-05 14:16:54 +0200

    Bitmap text for radial tree

---
+++ dist/assets/InstructorStudentsStore-0160ffae.js
@@ -0,0 +1,1 @@
+import{V as s}from"./main-0670b140.js";const e=s("instructorStudents",{state:()=>({instructorStudentsList:[]}),actions:{async getInstructorStudentsList(){const t=await(await fetch("/instructor-students/list")).json();return this.instructorStudentsList=t,this.$state}}});export{e as u};
--- dist/assets/TagsStore-6dac9368.js
+++
@@ -1,1 +0,0 @@
-import{R as e}from"./main-51d14fc5.js";const o=e("tags",{state:()=>({tagsList:[]}),actions:{async getTagsList(){const t=await(await fetch("/tags/list")).json();this.tagsList=t},async deleteTag(s){console.log(s);const t=fetch("/tags/"+s,{method:"DELETE"});t.error&&console.log(t.error),this.getTagsList()}}});export{o as u};
