cf438660d090ba6a7425be7773a04575a9a5d1f2
RussiSunni <jonathandyason@hotmail.com>
2024-03-08 15:38:19 +0200

    bulk question upload

--- dist/assets/InstructorStudentsStore-0160ffae.js
+++
@@ -1,1 +0,0 @@
-import{V as s}from"./main-0670b140.js";const e=s("instructorStudents",{state:()=>({instructorStudentsList:[]}),actions:{async getInstructorStudentsList(){const t=await(await fetch("/instructor-students/list")).json();return this.instructorStudentsList=t,this.$state}}});export{e as u};
