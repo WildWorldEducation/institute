a1ac78aa6df5da30ce0518e76ab3e6ea91a08203
RussiSunni <jonathandyason@hotmail.com>
2024-03-09 14:18:40 +0200

    bezier curves radial tree

--- dist/assets/InstructorStudentsStore-3fd833a4.js
+++
@@ -1,1 +0,0 @@
-import{V as s}from"./main-a2c0de58.js";const e=s("instructorStudents",{state:()=>({instructorStudentsList:[]}),actions:{async getInstructorStudentsList(){const t=await(await fetch("/instructor-students/list")).json();return this.instructorStudentsList=t,this.$state}}});export{e as u};
