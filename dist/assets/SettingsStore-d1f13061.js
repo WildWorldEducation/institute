239326fa3dce390137f6b9eb31b348a0761ce0c7
RussiSunni <jonathandyason@hotmail.com>
2024-03-15 10:36:05 +0200

    g

--- dist/assets/SettingsStore-d1f13061.js
+++
@@ -1,1 +0,0 @@
-import{V as s}from"./main-97a31e2f.js";const e=s("settings",{state:()=>({skillDegradationDays:null,quizMaxQuestions:null}),actions:{async getSettings(){const t=await(await fetch("/settings")).json();this.skillDegradationDays=t[0].skill_degradation_days,this.quizMaxQuestions=t[0].quiz_max_questions}}});export{e as u};
