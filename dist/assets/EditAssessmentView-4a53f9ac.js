7b6761c1baa507d96551130a00fd99a51573b089
RussiSunni <jonathandyason@hotmail.com>
2024-03-20 11:01:58 +0200

    ghj

--- dist/assets/EditAssessmentView-4a53f9ac.js
+++
@@ -1,1 +0,0 @@
-import{_ as e}from"./main-9fcd655c.js";const s={};function t(n,r){return" edit assessment "}const _=e(s,[["render",t]]);export{_ as default};
---
+++ dist/assets/InstructorStudentsStore-7c7f4eba.js
@@ -0,0 +1,1 @@
+import{W as s}from"./main-a65ba546.js";const e=s("instructorStudents",{state:()=>({instructorStudentsList:[]}),actions:{async getInstructorStudentsList(){const t=await(await fetch("/instructor-students/list")).json();return this.instructorStudentsList=t,this.$state}}});export{e as u};
