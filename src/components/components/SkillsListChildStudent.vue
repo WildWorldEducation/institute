4f2ec2830fed4838c0ad1029236d70a51d452453
RussiSunni <jonathandyason@hotmail.com>
2024-03-22 17:48:47 +0200

    dfg

--- src/components/components/SkillsListChildStudent.vue
+++ src/components/components/SkillsListChildStudent.vue
@@ -69,6 +69,10 @@ export default {
         }
     },
     methods: {
+        mainButtonPress() {
+            if (this.type != 'domain') this.$router.push('/skills/' + this.id);
+            else this.toggleChildren();
+        },
         toggleChildren() {
             this.showChildren = !this.showChildren;
         },
@@ -103,7 +107,7 @@ export default {
             'has-children': children.length > 0
         }"
         class="skill-button d-flex justify-content-between"
-        @click="toggleChildren"
+        @click.stop="mainButtonPress()"
     >
         <!-- Emoticons -->
         <div v-if="level != 'domain'">
@@ -186,21 +190,34 @@ export default {
                 </svg>
             </button>
             <button
-                v-if="type != 'domain'"
-                v-show="isUnlocked == 1"
-                @click.stop="$router.push('/skills/' + id)"
+                v-if="children.length != 0"
+                @click.stop="toggleChildren"
                 class="btn me-2 ci-btn"
             >
-                <!-- Eye icon -->
+                <!-- Collapse icon -->
                 <svg
+                    v-if="!showChildren"
                     xmlns="http://www.w3.org/2000/svg"
-                    height="25"
-                    width="30"
-                    viewBox="0 0 576 512"
+                    viewBox="0 0 320 512"
+                    width="18"
+                    height="18"
                 >
-                    <!--! Font Awesome Free 6.4.2 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license (Commercial License) Copyright 2023 Fonticons, Inc. -->
+                    <!--!Font Awesome Free 6.5.1 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license/free Copyright 2024 Fonticons, Inc.-->
                     <path
-                        d="M288 32c-80.8 0-145.5 36.8-192.6 80.6C48.6 156 17.3 208 2.5 243.7c-3.3 7.9-3.3 16.7 0 24.6C17.3 304 48.6 356 95.4 399.4C142.5 443.2 207.2 480 288 480s145.5-36.8 192.6-80.6c46.8-43.5 78.1-95.4 93-131.1c3.3-7.9 3.3-16.7 0-24.6c-14.9-35.7-46.2-87.7-93-131.1C433.5 68.8 368.8 32 288 32zM144 256a144 144 0 1 1 288 0 144 144 0 1 1 -288 0zm144-64c0 35.3-28.7 64-64 64c-7.1 0-13.9-1.2-20.3-3.3c-5.5-1.8-11.9 1.6-11.7 7.4c.3 6.9 1.3 13.8 3.2 20.7c13.7 51.2 66.4 81.6 117.6 67.9s81.6-66.4 67.9-117.6c-11.1-41.5-47.8-69.4-88.6-71.1c-5.8-.2-9.2 6.1-7.4 11.7c2.1 6.4 3.3 13.2 3.3 20.3z"
+                        d="M310.6 233.4c12.5 12.5 12.5 32.8 0 45.3l-192 192c-12.5 12.5-32.8 12.5-45.3 0s-12.5-32.8 0-45.3L242.7 256 73.4 86.6c-12.5-12.5-12.5-32.8 0-45.3s32.8-12.5 45.3 0l192 192z"
+                        fill="#9C7EEC"
+                    />
+                </svg>
+                <svg
+                    v-else
+                    xmlns="http://www.w3.org/2000/svg"
+                    viewBox="0 0 512 512"
+                    width="18"
+                    height="18"
+                >
+                    <!--!Font Awesome Free 6.5.1 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license/free Copyright 2024 Fonticons, Inc.-->
+                    <path
+                        d="M233.4 406.6c12.5 12.5 32.8 12.5 45.3 0l192-192c12.5-12.5 12.5-32.8 0-45.3s-32.8-12.5-45.3 0L256 338.7 86.6 169.4c-12.5-12.5-32.8-12.5-45.3 0s-12.5 32.8 0 45.3l192 192z"
                         fill="#9C7EEC"
                     />
                 </svg>
