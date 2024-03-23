818b7f67489257f139dc9549507393e2baa492d2
RussiSunni <jonathandyason@hotmail.com>
2024-03-23 15:01:27 +0200

    list changes

--- dist/assets/EditAssessmentView-5803b25f.js
+++
@@ -1,1 +0,0 @@
-import{_ as e}from"./main-cfa8f01b.js";const s={};function t(n,r){return" edit assessment "}const _=e(s,[["render",t]]);export{_ as default};
--- dist/assets/InstructorStudentsStore-0aaca969.js
+++
@@ -1,1 +0,0 @@
-import{D as s}from"./main-cfa8f01b.js";const e=s("instructorStudents",{state:()=>({instructorStudentsList:[]}),actions:{async getInstructorStudentsList(){const t=await(await fetch("/instructor-students/list")).json();return this.instructorStudentsList=t,this.$state}}});export{e as u};
