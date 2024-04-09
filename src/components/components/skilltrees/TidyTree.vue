41a181b3e5fe2c8b46c94c84bfe0df041abb1848
RussiSunni <jonathandyason@hotmail.com>
2024-04-09 11:07:09 +0200

    center zooming

--- src/components/components/skilltrees/TidyTree.vue
+++ src/components/components/skilltrees/TidyTree.vue
@@ -2,11 +2,6 @@
 // Import the stores.
 import { useUserDetailsStore } from '../../../stores/UserDetailsStore';
 import { useSkillTreeStore } from '../../../stores/SkillTreeStore';
-// Nested component.
-import SkillPanel from './../SkillPanel.vue';
-// Joystick control component.
-import JoystickControl from './JoystickControl.vue';
-import SliderControl from './SliderControl.vue';
 
 // Algorithm.
 import * as d3 from 'd3';
@@ -59,11 +54,7 @@ export default {
             d3Zoom: null
         };
     },
-    components: {
-        SkillPanel,
-        JoystickControl,
-        SliderControl
-    },
+    components: {},
     async mounted() {
         // ------------
 
@@ -83,64 +74,12 @@ export default {
 
         this.getAlgorithm();
 
-        // Set up the Hidden Canvas for Interactivity.
-        let hiddenCanvas = document.getElementById('hidden-canvas');
-        this.hiddenCanvasContext = hiddenCanvas.getContext('2d');
-        hiddenCanvas.style.display = 'none';
-
-        // Listen for clicks on the main canvas
-        canvas.addEventListener('click', (e) => {
-            // We actually only need to draw the hidden canvas when
-            // there is an interaction. This sketch can draw it on
-            // each loop, but that is only for demonstration.
-
-            var data = this.nodes;
-            //Figure out where the mouse click occurred.
-            var mouseX = e.layerX;
-            var mouseY = e.layerY;
-
-            // Get the corresponding pixel color on the hidden canvas
-            // and look up the node in our map.
-            var ctx = this.hiddenCanvasContext;
-
-            // This will return that pixel's color
-            var col = ctx.getImageData(mouseX, mouseY, 1, 1).data;
-            //var col = ctx.getImageData(mouseX, mouseY, 1, 1);
-
-            //Our map uses these rgb strings as keys to nodes.
-            var colString = 'rgb(' + col[0] + ',' + col[1] + ',' + col[2] + ')';
-            var node = this.colToNode[colString];
-
-            // console.log(this.colToNode);
-
-            if (node) {
-                // We clicked on something, lets set the color of the node
-                // we also have access to the data associated with it, which in
-                // this case is just its original index in the data array.
-                node.renderCol = node.__pickColor;
-
-                //Update the display with some data
-                this.skill.name = node.data.skill_name;
-                this.skill.id = node.data.id;
-                this.skill.type = node.data.type;
-                this.showInfoPanel();
-            }
-        });
-
-        this.d3Zoom = d3
-            .zoom()
-            .scaleExtent([0.05, 5])
-            .on('zoom', ({ transform }) => {
-                this.handleMouseZoom(transform);
-            });
-
-        // Zoom and Panning with mouse, d3 handler
-        d3.select(this.context.canvas).call(this.d3Zoom);
-
-        // Save the default scale value to d3
+        //Zoom.
         d3.select(this.context.canvas).call(
-            this.d3Zoom.transform,
-            d3.zoomIdentity.scale(this.scale)
+            d3
+                .zoom()
+                .scaleExtent([0.1, 5])
+                .on('zoom', ({ transform }) => this.drawTree(transform))
         );
     },
     methods: {
@@ -248,17 +187,14 @@ export default {
             canvas.width = this.width;
             canvas.height = this.height;
             this.context = canvas.getContext('2d');
-            let hiddenCanvas = document.getElementById('hidden-canvas');
-            this.hiddenCanvasContext = hiddenCanvas.getContext('2d');
 
-            this.drawTree();
+            this.drawTree(d3.zoomIdentity);
         },
-        drawTree() {
+        drawTree(transform) {
             this.nodes = this.root.descendants();
+
             // Zoom and pan.
             this.context.save();
-            this.hiddenCanvasContext.save();
-
             // Clear canvases.
             this.context.clearRect(
                 0,
@@ -266,20 +202,11 @@ export default {
                 this.context.canvas.width,
                 this.context.canvas.height
             );
-            this.hiddenCanvasContext.clearRect(
-                0,
-                0,
-                this.hiddenCanvasContext.canvas.width,
-                this.hiddenCanvasContext.canvas.height
-            );
+            this.context.translate(transform.x, transform.y);
+            this.context.scale(transform.k, transform.k);
 
-            // Zoom.
-            this.context.scale(this.scale, this.scale);
-            this.hiddenCanvasContext.scale(this.scale, this.scale);
-
-            // Pan.
-            this.context.translate(this.panX, this.panY);
-            this.hiddenCanvasContext.translate(this.panX, this.panY);
+            // For node labels to appear at correct zoom level.
+            this.scale = transform.k;
 
             // Draw links.
             const links = this.root.links();
@@ -291,39 +218,14 @@ export default {
             // Draw nodes.
             this.context.beginPath();
             for (const node of this.nodes) {
-                if (node.renderCol) {
-                    // Render clicked nodes in the color of their corresponding node
-                    // on the hidden canvas.
-                    this.context.fillStyle = node.renderCol;
-                } else {
-                    this.context.fillStyle = 'RGBA(105, 105, 105, 0.8)';
-                }
-
-                //
-                //  If we are rendering to the hidden canvas each element
-                // should get its own color.
-                //
-
-                if (node.__pickColor === undefined) {
-                    // If we have never drawn the node to the hidden canvas get a new
-                    // color for it and put it in the dictionary. genColor returns a new color
-                    // every time it is called.
-                    node.__pickColor = this.genColor();
-                    this.colToNode[node.__pickColor] = node;
-                }
-                // On the hidden canvas each rectangle gets a unique color.
-                this.hiddenCanvasContext.fillStyle = node.__pickColor;
-
                 // Draw the actual shape
                 this.drawNode(node);
             }
 
-            this.hiddenCanvasContext.restore();
             this.context.restore();
         },
         drawNode(node) {
             let ctx1 = this.context;
-            let ctx2 = this.hiddenCanvasContext;
 
             // Visible context.
             // If not a domain, make node a circle.
@@ -389,28 +291,6 @@ export default {
                 ctx1.fillStyle = '#000';
                 ctx1.fillText(node.data.skill_name, node.y + 15, node.x + 2);
             }
-
-            // Hidden context.
-            if (node.data.type != 'domain') {
-                ctx2.beginPath();
-                ctx2.moveTo(node.y, node.x);
-                ctx2.arc(node.y, node.x, 10, 0, 2 * Math.PI);
-                ctx2.fill();
-            } else {
-                ctx2.beginPath();
-                ctx2.moveTo(node.y, node.x - 10);
-                // top left edge.
-                ctx2.lineTo(node.y - 20 / 2, node.x - 10 + 20 / 2);
-                // bottom left edge.
-                ctx2.lineTo(node.y, node.x - 10 + 20);
-                // bottom right edge.
-                ctx2.lineTo(node.y + 20 / 2, node.x - 10 + 20 / 2);
-                // closing the path automatically creates the top right edge.
-                ctx2.closePath();
-                ctx2.lineWidth = 2;
-                ctx2.fill();
-                ctx2.stroke();
-            }
         },
         drawLink(link) {
             const linkGenerator = d3
@@ -438,278 +318,9 @@ export default {
             this.context.strokeStyle = '#000';
             this.context.stroke();
         },
-        genColor() {
-            var ret = [];
-            // via http://stackoverflow.com/a/15804183
-            if (this.nextCol < 16777215) {
-                ret.push(this.nextCol & 0xff); // R
-                ret.push((this.nextCol & 0xff00) >> 8); // G
-                ret.push((this.nextCol & 0xff0000) >> 16); // B
 
-                this.nextCol += 100; // This is exagerated for this example and would ordinarily be 1.
-            }
-            var col = 'rgb(' + ret.join(',') + ')';
-            return col;
-        },
-        showInfoPanel() {
-            // If panel is not showing.
-            if (!this.isSkillInfoPanelShown) {
-                this.isSkillInfoPanelShown = true;
-                // To display the panel.
-                // Responsive.
-                // Laptop etc.
-                if (screen.width > 800) {
-                    document.getElementById('skillInfoPanel').style.width =
-                        '474px';
-                }
-                // Mobile device.
-                else {
-                    document.getElementById('skillInfoPanel').style.height =
-                        '474px';
-                }
-            }
-        },
-        hideInfoPanel() {
-            // If panel is showing.
-            if (this.isSkillInfoPanelShown) {
-                // Responsive.
-                // Laptop etc.
-                if (screen.width > 800) {
-                    document.getElementById('skillInfoPanel').style.width =
-                        '0px';
-                }
-                // Mobile device.
-                else {
-                    document.getElementById('skillInfoPanel').style.height =
-                        '0px';
-                }
-                // Hide the background.
-                document.getElementById('sidepanel-backdrop').style.display =
-                    'none';
-
-                this.isSkillInfoPanelShown = false;
-            }
-        },
-        async printPDF() {
-            // Build the SVG tree.
-            await this.createSVGTree();
-
-            // Select the element from the DOM.
-            var svg = document.getElementById('linearTree');
-            // Then select with D3
-            var d3Svg = d3.select(svg);
-            // Then select the SVG code with D3
-            var d3SvgNode = d3Svg.node();
-
-            // Make it a string, to send to server.
-            var s = new XMLSerializer();
-            var str = s.serializeToString(d3SvgNode);
-
-            // Create a JSON object.
-            var dataObject = { svg: str, treeType: 'linear' };
-            var data = JSON.stringify(dataObject);
-
-            // POST request.
-            var xhttp = new XMLHttpRequest();
-            xhttp.responseType = 'arraybuffer';
-            xhttp.open('POST', '/skilltree/print-pdf', true);
-            xhttp.setRequestHeader(
-                'Content-type',
-                'application/json;charset=UTF-8'
-            );
-            xhttp.setRequestHeader(
-                'Accept',
-                'application/json, text/plain, */*'
-            );
-            xhttp.send(data);
-
-            // To download the file client side.
-            xhttp.onload = function () {
-                if (this.readyState == 4 && this.status == 200) {
-                    // Typical action to be performed when the document is ready:
-                    let pdfBlob = new Blob([xhttp.response], {
-                        type: 'application/pdf'
-                    });
-                    const url = window.URL.createObjectURL(pdfBlob);
-
-                    // To download and name the file.
-                    var a = document.createElement('a');
-                    document.body.appendChild(a);
-                    a.style = 'display: none';
-                    a.href = url;
-                    a.download = 'My-Skill-Tree.pdf';
-                    a.click();
-                    window.URL.revokeObjectURL(url);
-                }
-            };
-        },
-        createSVGTree() {
-            // Redo D3 algorithm.
-            const root = d3.hierarchy(this.data);
-            // Different node size for the PDF, as doesnt need to be clickable.
-            const dx = 15;
-            const dy = this.width / (root.height + 1);
-            // Create a tree layout.
-            const tree = d3.tree().nodeSize([dx, dy]);
-            // Sort the tree and apply the layout.
-            tree(root);
-
-            // Compute the extent of the tree. Note that x and y are swapped here
-            // because in the tree layout, x is the breadth, but when displayed, the
-            // tree extends right rather than down.
-            let x0 = Infinity;
-            let x1 = -x0;
-            root.each((d) => {
-                if (d.x > x1) x1 = d.x;
-                if (d.x < x0) x0 = d.x;
-            });
-
-            // Compute the adjusted height of the tree.
-            const height = x1 - x0 + dx * 2;
-
-            let svg = d3
-                .create('svg')
-                // Add ID for the printing to PDF.
-                .attr('id', 'linearTree')
-                .attr('width', this.width)
-                .attr('height', height)
-                .attr('viewBox', [-dy / 3, x0 - dx, this.width, height])
-                .attr(
-                    'style',
-                    'max-width: 100%; height: auto; font: 10px sans-serif;'
-                );
-
-            const g = svg.append('g');
-
-            // Links or connecting lines.
-            g.append('g')
-                .attr('fill', 'none')
-                .attr('stroke-opacity', 1)
-                .selectAll()
-                .data(root.links())
-                .join('path')
-                .attr(
-                    'd',
-                    d3
-                        .linkHorizontal()
-                        .x((d) => d.y)
-                        .y((d) => d.x)
-                )
-                .attr('stroke', function (d) {
-                    return '#000';
-                })
-                .attr('stroke-width', function (d) {
-                    if (d.target.data.is_mastered == 1) {
-                        return 8;
-                    } else return 1.5;
-                })
-                .style('stroke-dasharray', function (d) {
-                    // If the node is a sub node.
-                    if (
-                        (d.source.data.type == 'super' &&
-                            d.target.data.position == 'end') ||
-                        d.target.data.type == 'sub'
-                    ) {
-                        return 5;
-                    }
-                });
-
-            const node = g
-                .append('g')
-                .attr('stroke-linejoin', 'round')
-                .attr('stroke-width', 3)
-                .selectAll()
-                .data(root.descendants())
-                .join('g')
-                .attr('transform', (d) => `translate(${d.y},${d.x})`);
-
-            node.append('circle')
-                .attr('fill', (d) => (d.children ? '#555' : '#000'))
-                .attr('r', 2.5);
-
-            // Labels.
-            node.append('text')
-                .style('font-weight', function (d) {
-                    // If the node is a super node.
-                    if (d.data.type == 'super') {
-                        return '700';
-                    } else return '400';
-                })
-                .style('font-style', function (d) {
-                    // If the node is a sub node.
-                    if (d.data.type == 'sub') {
-                        return 'italic';
-                    }
-                })
-                .attr('dy', '0.31em')
-                .attr('x', (d) => (d.children ? -6 : 6))
-                .attr('text-anchor', (d) => (d.children ? 'end' : 'start'))
-                .text(function (d) {
-                    // If the node is a super node end node.
-                    if (d.data.position == 'end') {
-                        return '';
-                    } else return d.data.skill_name;
-                })
-                .clone(true)
-                .lower()
-                .attr('stroke', function (d) {
-                    return 'white';
-                });
-
-            // Append the SVG element.
-            document.querySelector('#SVGskilltree').append(svg.node());
-        },
         // handle mouse zoom
-        handleMouseZoom(transform) {
-            // only show label when user zoom
-            if (transform.k != this.scale) {
-                // show scale label
-                this.$refs.sliderControl.showScaleLabel();
-            }
-            // update slider percent
-            this.$refs.sliderControl.changeGradientBG();
-            this.panX =
-                this.scale >= 1 ? transform.x : transform.x / this.scale;
-            this.panY =
-                this.scale >= 1 ? transform.y : transform.y / this.scale;
-            this.scale = transform.k;
-
-            this.drawTree();
-        },
-        // programmatic d3 zoom
-        zoomInD3(scale, panX, panY) {
-            d3.select(this.context.canvas).call(
-                this.d3Zoom.transform,
-                d3.zoomIdentity.translate(panX, panY).scale(scale)
-            );
-            this.$refs.sliderControl.showScaleLabel();
-        },
-        // programmatic d3 panning
-        panInD3(panX, panY) {
-            /*
-                 because we divide scale to handle mouse drag so we have to
-                 multiply the pan value with scale here in order to cancel out the divide
-            */
-            const smallPanX = this.scale >= 1 ? panX : panX * this.scale;
-            const smallPanY = this.scale >= 1 ? panY : panY * this.scale;
-
-            d3.select(this.context.canvas).call(
-                this.d3Zoom.transform,
-                d3.zoomIdentity
-                    .translate(smallPanX, smallPanY)
-                    .scale(this.scale)
-            );
-        },
-        resetPos() {
-            d3.select(this.context.canvas)
-                .transition()
-                .duration(700)
-                .call(
-                    this.d3Zoom.transform,
-                    d3.zoomIdentity.translate(0, 0).scale(0.3)
-                );
-            this.$refs.sliderControl.showScaleLabel();
-        }
+        handleMouseZoom() {}
     }
 };
 </script>
@@ -723,7 +334,6 @@ export default {
     </button>
     <!-- Wrapper is for the dark overlay, when the sidepanel is displayed -->
     <div id="wrapper">
-        <SkillPanel :skill="skill" />
         <canvas
             id="canvas"
             width="1500"
@@ -733,8 +343,6 @@ export default {
         ></canvas>
         <canvas id="hidden-canvas" width="1500" height="1500"></canvas>
         <div id="SVGskilltree"></div>
-        <JoystickControl />
-        <SliderControl ref="sliderControl" />
         <div id="sidepanel-backdrop"></div>
     </div>
 </template>
