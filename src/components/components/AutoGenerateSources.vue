807bac4947d515e4dd9ad5500a2d3f2e1b4422aa
RussiSunni <jonathandyason@hotmail.com>
2024-04-12 12:12:31 +0200

    .

--- src/components/components/AutoGenerateSources.vue
+++ src/components/components/AutoGenerateSources.vue
@@ -28,9 +28,22 @@ export default {
         <hr />
         <h1>Auto Generate Sources</h1>
         <div class="row">
-            <div class="col-sm-4">
-                <p>Number of sources per skill to generate:</p>
-                <button class="btn green-btn" @click="GenerateSources">
+            <div class="col">
+                <label for="numSourcesPerSkill" class="form-label"
+                    >Number of sources per skill to generate (max 10):</label
+                >
+                <input
+                    v-model="numOfSourcesPerSkill"
+                    type="number"
+                    id="numSourcesPerSkill"
+                    min="1"
+                    max="10"
+                    step="1"
+                    class="form-control"
+                    aria-describedby="numSourcesPerSkill"
+                    onkeypress="return event.charCode >= 48 && event.charCode <= 57"
+                />
+                <button class="btn green-btn mt-3" @click="GenerateSources">
                     Generate Sources
                 </button>
             </div>
