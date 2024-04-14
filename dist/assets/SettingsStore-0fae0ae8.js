2a870fd94c56ccf00385b2ae96c7480efb729393
RussiSunni <jonathandyason@hotmail.com>
2024-04-14 12:47:34 +0200

    remove messaging feature

---
+++ dist/assets/SettingsStore-0fae0ae8.js
@@ -0,0 +1,1 @@
+import{H as s}from"./main-cadc5ca7.js";const e=s("settings",{state:()=>({skillDegradationDays:null,quizMaxQuestions:null}),actions:{async getSettings(){const t=await(await fetch("/settings")).json();this.skillDegradationDays=t[0].skill_degradation_days,this.quizMaxQuestions=t[0].quiz_max_questions}}});export{e as u};
