580f7016304ae7176091499d11e8c6ac0903eb13
RussiSunni <jonathandyason@hotmail.com>
2024-04-20 21:59:48 +0200

    fixing bug with panning with arrow and joystick

---
+++ dist/assets/EditAssessmentView-53ae9421.js
@@ -0,0 +1,1 @@
+import{_ as e}from"./main-fb7c5d36.js";const s={};function t(n,r){return" edit assessment "}const _=e(s,[["render",t]]);export{_ as default};
--- dist/assets/InstructorStudentsStore-4d9ac290.js
+++
@@ -1,1 +0,0 @@
-import{H as s}from"./main-47dbbc89.js";const e=s("instructorStudents",{state:()=>({instructorStudentsList:[]}),actions:{async getInstructorStudentsList(){const t=await(await fetch("/instructor-students/list")).json();return this.instructorStudentsList=t,this.$state}}});export{e as u};
