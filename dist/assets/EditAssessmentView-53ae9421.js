c34c60767987293247ce69bf82e709d5f11fc065
RussiSunni <jonathandyason@hotmail.com>
2024-04-23 13:08:28 +0200

    crop portrait images

--- dist/assets/EditAssessmentView-53ae9421.js
+++
@@ -1,1 +0,0 @@
-import{_ as e}from"./main-fb7c5d36.js";const s={};function t(n,r){return" edit assessment "}const _=e(s,[["render",t]]);export{_ as default};
--- dist/assets/InstructorStudentsStore-e178327f.js
+++
@@ -1,1 +0,0 @@
-import{H as s}from"./main-fb7c5d36.js";const e=s("instructorStudents",{state:()=>({instructorStudentsList:[]}),actions:{async getInstructorStudentsList(){const t=await(await fetch("/instructor-students/list")).json();return this.instructorStudentsList=t,this.$state}}});export{e as u};
