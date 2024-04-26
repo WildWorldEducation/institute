35ac91a0d5d5847107009ec2c627cb7abb6f57d0
Taan <tannguyen1220@gmail.com>
2024-04-26 09:15:55 +0700

    fix table header position

--- src/components/components/StudentProgress.vue
+++ src/components/components/StudentProgress.vue
@@ -1,92 +1,112 @@
 <script>
 export default {
-  data() {
-    return {
-      userSkills: [],
-      noSkills: true,
-    };
-  },
-  props: ['userId'],
-  async created() {
-    const result = await fetch('/user-skills/unnested-list/' + this.userId);
-    this.userSkills = await result.json();
-  },
-  computed: {
-    availableSkills() {
-      const availableSkills = [];
-      for (let i = 0; i < this.userSkills.length; i++) {
-        if (
-          (this.userSkills[i].is_accessible == 1) &
-          (this.userSkills[i].is_mastered != 1)
-        ) {
-          availableSkills.push(this.userSkills[i].name);
-        }
-      }
-      if (availableSkills.length > 0) {
-        this.noSkills = false;
-      }
-      return availableSkills;
+    data() {
+        return {
+            userSkills: [],
+            noSkills: true
+        };
     },
-  },
-  methods: {},
+    props: ['userId'],
+    async created() {
+        const result = await fetch('/user-skills/unnested-list/' + this.userId);
+        this.userSkills = await result.json();
+    },
+    computed: {
+        availableSkills() {
+            const availableSkills = [];
+            for (let i = 0; i < this.userSkills.length; i++) {
+                if (
+                    (this.userSkills[i].is_accessible == 1) &
+                    (this.userSkills[i].is_mastered != 1)
+                ) {
+                    availableSkills.push(this.userSkills[i].name);
+                }
+            }
+            if (availableSkills.length > 0) {
+                this.noSkills = false;
+            }
+            return availableSkills;
+        }
+    },
+    methods: {}
 };
 </script>
 
 <template>
-  <!-- <h1 class="text-center text-md-start d-lg-none">My Progress</h1>
+    <!-- <h1 class="text-center text-md-start d-lg-none">My Progress</h1>
   <h2 class="text-center text-md-start d-lg-none">2023 - Active</h2> -->
-  <div class="table-responsive">
-    <table class="table table-bordered">
-      <thead>
-        <tr>
-          <th scope="col">Available Skills</th>
-        </tr>
-      </thead>
-      <tbody>
-        <tr v-if="noSkills">
-          <td id="no-skill-cell"></td>
-        </tr>
-        <tr v-for="availableSkill in availableSkills">
-          <td>{{ availableSkill }}</td>
-        </tr>
-      </tbody>
-    </table>
-  </div>
+    <div class="table-responsive">
+        <table class="table table-bordered">
+            <thead>
+                <tr id="th-row">
+                    <th scope="col">Available Skills</th>
+                </tr>
+            </thead>
+            <tbody>
+                <tr v-if="noSkills">
+                    <td id="no-skill-cell"></td>
+                </tr>
+                <tr v-for="availableSkill in availableSkills">
+                    <td>{{ availableSkill }}</td>
+                </tr>
+            </tbody>
+        </table>
+    </div>
 </template>
 
 <style scoped>
-.table-responsive {
-  max-height: 300px;
+/* separate the content of the table from the header of the table. */
+table tbody,
+table thead {
+    display: block;
 }
 
-h1 {
-  color: #ad9af3;
-  font-family: 'Poppins', sans-serif;
-  font-weight: 900;
+table tbody {
+    overflow: auto;
+    height: 200px;
 }
 
-h2 {
-  color: #dcd1fd;
-  font-family: 'Poppins', sans-serif;
-  font-weight: 900;
+table {
+    width: 352px; /* can be dynamic */
 }
 
 th {
-  background-color: #e8e2f9;
-  border-color: #dbd0f9;
-  color: #ad9af3;
-  font-family: 'Poppins', sans-serif;
-  font-weight: 900;
+    width: 352px;
+}
+
+/* -==- */
+.table-responsive {
+    max-height: 300px;
+}
+
+h1 {
+    color: #ad9af3;
+    font-family: 'Poppins', sans-serif;
+    font-weight: 900;
+}
+
+h2 {
+    color: #dcd1fd;
+    font-family: 'Poppins', sans-serif;
+    font-weight: 900;
+}
+
+th {
+    background-color: #e8e2f9;
+    border-color: #dbd0f9;
+    color: #ad9af3;
+    font-family: 'Poppins', sans-serif;
+    font-weight: 900;
 }
 
 td {
-  font-family: 'Poppins', sans-serif;
-  font-weight: 500;
-  color: #857d99;
-  border-color: #dbd0f9;
+    font-family: 'Poppins', sans-serif;
+    font-weight: 500;
+    color: #857d99;
+    border-color: #dbd0f9;
 }
 
 #no-skill-cell {
-  height: 41px;
+    height: 41px;
 }
 </style>
