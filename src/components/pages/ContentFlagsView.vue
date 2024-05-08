4058e6a36983eaf4593c0ad62f6dfb3eb48cfc4d
Taan <tannguyen1220@gmail.com>
2024-05-09 04:29:49 +0700

    data table phone style

--- src/components/pages/ContentFlagsView.vue
+++ src/components/pages/ContentFlagsView.vue
@@ -193,11 +193,12 @@ export default {
             class="img-fluid"
         />
     </div>
-    <div class="container">
+    <div class="container-md">
         <div class="mt-3">
             <h1>Content Flags</h1>
         </div>
-        <div class="mt-5 pb-5">
+        <!-- Vue Data Table Desktop  -->
+        <div class="mt-5 pb-5 d-none d-md-block">
             <Vue3EasyDataTable
                 :headers="headers"
                 :items="rows"
@@ -207,19 +208,23 @@ export default {
                 :loading="!isContentFlagsLoaded"
                 table-class-name="customize-table"
             >
+                <!-- --- Loading Part --- -->
                 <template #loading>
                     <img src="/images/loading.gif" alt="loading data" />
                 </template>
+
+                <!-- --- Name Router Column --- -->
                 <template #item-name="{ name, nameUrl }">
                     <RouterLink class="flag-name" :to="`/${nameUrl}`">{{
                         name
                     }}</RouterLink>
                 </template>
+
+                <!-- --- Action Buttons Column --- -->
                 <template #item-action="{ flagId, editUrl, type }">
                     <div
                         class="pb-2 pt-2 d-flex flex-column flex-lg-row gap-1 align-items-center"
                     >
-                        <!-- ___+++___ The Two Button in Desktop View  ___+++___ -->
                         <router-link
                             :to="editUrl"
                             class="btn purple-btn"
@@ -270,6 +275,8 @@ export default {
                         <!---------------------------------------------------------->
                     </div>
                 </template>
+
+                <!-- ---  Expand Part --- -->
                 <template #expand="{ expandContent, type }">
                     <div id="expand-div" style="padding: 15px">
                         <!-- _+_+_+_+_+_+_+_ MC Question Expand _+_+_+_+_+_+_+_  -->
@@ -387,7 +394,7 @@ export default {
                                 v-html="expandContent.masteryRequirements"
                             ></div>
                         </div>
-                        <!-- _+_+_+_+_+_+_+_  Skill Expand content _+_+_+_+_+_+_+_  -->
+                        <!-- _+_+_+_+_+_+_+_  Resource Expand content _+_+_+_+_+_+_+_  -->
                         <div v-if="type == 'resource'">
                             <div class="d-flex mb-2">
                                 <div class="expand-tile">Skill:</div>
@@ -407,6 +414,241 @@ export default {
                         </div>
                     </div>
                 </template>
+
+                <!-- --- Student Column --- -->
+                <template #item-student="{ student }">
+                    <div class="student-cell">
+                        <img
+                            :src="student.avatar"
+                            alt="student avatar"
+                            class="student-avatar"
+                        />
+                        <div
+                            class="student-name"
+                            @click="handleRewardStudent(student.id)"
+                        >
+                            {{ student.username }}
+                        </div>
+                    </div>
+                </template>
+            </Vue3EasyDataTable>
+        </div>
+        <!-- ++++++++++++++++++++++++++++_______________________________ ++++++++++++++++++++++++++++ -->
+
+        <!-- Vue Data Table Phone -->
+        <div class="mt-5 pb-5 d-md-none">
+            <Vue3EasyDataTable
+                :headers="headers"
+                :items="rows"
+                alternating
+                hide-footer
+                :loading="!isContentFlagsLoaded"
+                table-class-name="customize-table"
+            >
+                <!-- --- Loading Part --- -->
+                <template #loading>
+                    <img src="/images/loading.gif" alt="loading data" />
+                </template>
+                <!-- --- Flag Name Column ---  -->
+                <template #item-name="{ name, nameUrl }">
+                    <RouterLink class="flag-name" :to="`/${nameUrl}`">{{
+                        name
+                    }}</RouterLink>
+                </template>
+                <!-- --- Action Buttons Column -->
+                <template #item-action="{ flagId, editUrl, type }">
+                    <div
+                        class="pb-2 pt-2 d-flex flex-column flex-lg-row gap-1 align-items-center"
+                    >
+                        <router-link
+                            :to="editUrl"
+                            class="btn purple-btn"
+                            target="_blank"
+                            b-tooltip.hover
+                            :title="'Go To Edit Page For This ' + type"
+                        >
+                            <!-- Pencil icon -->
+                            <svg
+                                width="10"
+                                height="12"
+                                viewBox="0 0 19 20"
+                                fill="none"
+                                xmlns="http://www.w3.org/2000/svg"
+                            >
+                                <path
+                                    d="M0.75558 19.3181C0.77635 19.5132 0.87137 19.6928 1.02096 19.8198C1.17055 19.9468 1.36325 20.0114 1.55915 20.0002L5.27701 19.8288L0.398438 15.6145L0.75558 19.3181Z"
+                                    fill="white"
+                                />
+                                <path
+                                    d="M11.8467 2.24484L0.801758 15.0315L5.6802 19.2454L16.7251 6.45877L11.8467 2.24484Z"
+                                    fill="white"
+                                />
+                                <path
+                                    d="M18.2555 3.11796L14.934 0.260817C14.832 0.172259 14.7134 0.104756 14.5852 0.0621907C14.4569 0.0196256 14.3215 0.00283902 14.1868 0.0127967C14.052 0.0227543 13.9205 0.0592596 13.7999 0.120212C13.6793 0.181165 13.572 0.265362 13.484 0.36796L12.4805 1.50725L17.359 5.71439L18.3519 4.56082C18.5289 4.35602 18.6181 4.08969 18.6 3.81958C18.582 3.54948 18.4582 3.29738 18.2555 3.11796Z"
+                                    fill="white"
+                                />
+                            </svg>
+                        </router-link>
+                        <button
+                            b-tooltip.hover
+                            :title="'Dismiss This Flag'"
+                            class="btn red-btn"
+                            @click="handleOpenDismissModal(flagId)"
+                        >
+                            <svg
+                                xmlns="http://www.w3.org/2000/svg"
+                                viewBox="0 0 576 512"
+                                width="10"
+                                height="12"
+                                fill="white"
+                            >
+                                <path
+                                    d="M290.7 57.4L57.4 290.7c-25 25-25 65.5 0 90.5l80 80c12 12 28.3 18.7 45.3 18.7H288h9.4H512c17.7 0 32-14.3 32-32s-14.3-32-32-32H387.9L518.6 285.3c25-25 25-65.5 0-90.5L381.3 57.4c-25-25-65.5-25-90.5 0zM297.4 416H288l-105.4 0-80-80L227.3 211.3 364.7 348.7 297.4 416z"
+                                />
+                            </svg>
+                        </button>
+                        <!---------------------------------------------------------->
+                    </div>
+                </template>
+                <!-- --- Expand Part --- -->
+                <template #expand="{ expandContent, type }">
+                    <div id="expand-div" style="padding: 15px">
+                        <!-- _+_+_+_+_+_+_+_ MC Question Expand _+_+_+_+_+_+_+_  -->
+                        <div v-if="type == 'mc question'">
+                            <div class="d-flex mb-2">
+                                <div class="expand-tile">Belong to skill:</div>
+                                <div>
+                                    <router-link
+                                        :to="'skills/' + expandContent.skillId"
+                                        target="_blank"
+                                        b-tooltip.hover
+                                        :style="{ color: '#8f7bd6' }"
+                                        :title="'Go To Skill '"
+                                        >{{
+                                            expandContent.skillName
+                                        }}</router-link
+                                    >
+                                </div>
+                            </div>
+                            <div class="d-flex mb-2">
+                                <div class="expand-tile">Name:</div>
+                                <div>
+                                    {{ expandContent.name }}
+                                </div>
+                            </div>
+                            <div class="d-flex mb-2">
+                                <div class="expand-tile">Question:</div>
+                                <div>
+                                    {{ expandContent.question }}
+                                </div>
+                            </div>
+                            <div
+                                id="expand-mc-answer"
+                                class="d-flex flex-column"
+                            >
+                                <div
+                                    class="d-flex mb-2 border-bottom border-opacity-75 pb-2"
+                                >
+                                    <div class="expand-tile">
+                                        Correct Answer:
+                                    </div>
+                                    {{ expandContent.correctAnswer }}
+                                </div>
+                                <div class="d-flex mb-2 border-bottom pb-2">
+                                    <div class="expand-tile">
+                                        Incorrect Answer 1:
+                                    </div>
+                                    {{ expandContent.incorrectAnswer1 }}
+                                </div>
+                                <div class="d-flex mb-2 border-bottom pb-2">
+                                    <div class="expand-tile">
+                                        Incorrect Answer 2:
+                                    </div>
+                                    {{ expandContent.incorrectAnswer2 }}
+                                </div>
+                                <div class="d-flex mb-2 border-bottom pb-2">
+                                    <div class="expand-tile">
+                                        Incorrect Answer 3:
+                                    </div>
+                                    {{ expandContent.incorrectAnswer3 }}
+                                </div>
+                                <div class="d-flex mb-2 border-bottom pb-2">
+                                    <div class="expand-tile">
+                                        Incorrect Answer 4:
+                                    </div>
+                                    {{ expandContent.incorrectAnswer4 }}
+                                </div>
+                            </div>
+
+                            <div class="d-flex mb-2">
+                                <div class="expand-tile">Explanation:</div>
+                                {{ expandContent.explanation }}
+                            </div>
+                        </div>
+                        <!-- _+_+_+_+_+_+_+_  Essay Expand content _+_+_+_+_+_+_+_  -->
+                        <div v-if="type == 'essay question'">
+                            <div class="d-flex mb-2">
+                                <div class="expand-tile">Belong to skill:</div>
+                                <div>
+                                    <router-link
+                                        :to="'skills/' + expandContent.skillId"
+                                        target="_blank"
+                                        b-tooltip.hover
+                                        :style="{ color: '#8f7bd6' }"
+                                        :title="'Go To Skill '"
+                                        >{{
+                                            expandContent.skillName
+                                        }}</router-link
+                                    >
+                                </div>
+                            </div>
+                            <div class="d-flex mb-2">
+                                <div class="expand-tile">Name:</div>
+                                {{ expandContent.name }}
+                            </div>
+                            <div class="d-flex mb-2">
+                                <div class="expand-tile">Question:</div>
+                                {{ expandContent.question }}
+                            </div>
+                        </div>
+                        <!-- _+_+_+_+_+_+_+_  Skill Expand content _+_+_+_+_+_+_+_  -->
+                        <div v-if="type == 'skill'">
+                            <div id="skill-expand-head">
+                                <div class="d-flex mb-2">
+                                    <div class="expand-tile">Skill:</div>
+                                    {{ expandContent.name }}
+                                </div>
+                                <div class="mb-2 border-bottom pb-2">
+                                    {{ expandContent.description }}
+                                </div>
+                            </div>
+                            <div class="expand-tile">Mastery Requirements:</div>
+                            <div
+                                class="expand-skill-requirement"
+                                v-html="expandContent.masteryRequirements"
+                            ></div>
+                        </div>
+                        <!-- _+_+_+_+_+_+_+_  Resource Expand content _+_+_+_+_+_+_+_  -->
+                        <div v-if="type == 'resource'">
+                            <div class="d-flex mb-2">
+                                <div class="expand-tile">Skill:</div>
+                                <div>{{ expandContent.skill }}</div>
+                            </div>
+                            <div class="d-flex mb-2">
+                                <div class="expand-tile">User:</div>
+                                <div>{{ expandContent.user }}</div>
+                            </div>
+                            <div class="d-flex flex-column mb-2">
+                                <div class="expand-tile">Content:</div>
+                                <div
+                                    class="expand-skill-requirement"
+                                    v-html="expandContent.content"
+                                ></div>
+                            </div>
+                        </div>
+                    </div>
+                </template>
+                <!-- --- Student Column --- -->
                 <template #item-student="{ student }">
                     <div class="student-cell">
                         <img
@@ -633,5 +875,21 @@ h2 {
     color: #a48be6;
     text-decoration: underline;
     cursor: pointer;
+}
+
+/* View Specific On Phone */
+@media (min-width: 0px) and (max-width: 576px) {
+    .student-avatar {
+        display: none;
+    }
+
+    /* +-+-+ Vue Easy Table Custom CSS +-+-+  */
+    .customize-table {
+        --easy-table-body-row-font-size: 13px;
+
+        --easy-table-header-font-size: 13px;
+
+        --easy-table-header-item-padding: 5px 5px;
+    }
 }
 </style>
