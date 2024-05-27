358dae075d1f45c019853fc55297b84d58f6eb43
RussiSunni <jonathandyason@hotmail.com>
2024-05-27 15:23:14 +0200

    .

--- server/routes/resources.js
+++ server/routes/resources.js
@@ -250,7 +250,7 @@ router.post('/generate-sources', (req, res, next) => {
         // As we are posting sources for all skills, we get all skills.
         let sqlQuery = `SELECT * FROM skills 
         WHERE type <> 'domain'              
-        AND id > 2314
+        AND id > 2331
         
         ORDER BY id`;
         let query = conn.query(sqlQuery, (err, results) => {
