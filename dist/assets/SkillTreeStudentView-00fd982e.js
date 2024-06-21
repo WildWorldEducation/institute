72d2041d688956b22de84ef445dd779cb08022f3
RussiSunni <jonathandyason@hotmail.com>
2024-06-21 10:38:53 +0200

    .

---
+++ dist/assets/SkillTreeStudentView-00fd982e.js
@@ -0,0 +1,1 @@
+import{_ as l,i as s,o as e,c as o,k as c,w as t,S as _,F as r,d as n,a as d}from"./main-5e6f8c53.js";const i={},u=n("div",{id:"purple-banner"},null,-1),p=n("span",null,"Loading...",-1);function S(k,f){const a=s("SkillTreeStudent");return e(),o(r,null,[u,(e(),c(_,null,{default:t(()=>[d(a)]),fallback:t(()=>[p]),_:1}))],64)}const x=l(i,[["render",S]]);export{x as default};
