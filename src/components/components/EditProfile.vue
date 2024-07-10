ff5cc8de0ef6e91f9dc92646223e261509750f8c
Taan <tannguyen1220@gmail.com>
2024-07-11 05:05:40 +0700

    add check password complex feature

--- src/components/components/EditProfile.vue
+++ src/components/components/EditProfile.vue
@@ -3,9 +3,12 @@ import router from '../../router';
 // Import the stores.
 import { useUserDetailsStore } from '../../stores/UserDetailsStore';
 import { useUsersStore } from '../../stores/UsersStore';
+// import cropper component and it styles
 import { Cropper, Preview } from 'vue-advanced-cropper';
 import 'vue-advanced-cropper/dist/style.css';
 import 'vue-advanced-cropper/dist/theme.compact.css';
+// others component import
+import CheckPasswordComplexity from './CheckPasswordComplexity.vue';
 
 export default {
     setup() {
@@ -38,8 +41,11 @@ export default {
                 // this validate is fire when image profile upload is not square
                 notSquareImg: false,
                 // flag to show warning when cancel crop
-                notCropped: false
+                notCropped: false,
+                passwordComplex: false
             },
+            // show/hide password flag
+            passwordVisible: false,
             // Flag and data of crop image component
             showCropModal: false,
             cropCanvas: '',
@@ -54,7 +60,8 @@ export default {
     },
     components: {
         Cropper,
-        Preview
+        Preview,
+        CheckPasswordComplexity
     },
     computed: {},
 
@@ -68,7 +75,7 @@ export default {
                 this.validate.username = true;
             } else if (this.email == '' || this.email == null) {
                 this.validate.email = true;
-            } else {
+            } else if (this.validate.passwordComplex) {
                 this.Submit();
             }
         },
@@ -94,33 +101,37 @@ export default {
                     email: this.email,
                     avatar: this.avatar,
                     password: this.password,
-                    instructorID :this.instructorID
+                    instructorID: this.instructorID
                 })
             };
 
             var url = '/users/profile/' + this.id + '/edit';
             fetch(url, requestOptions).then(() => {
-                    if (this.userDetailsStore.role == 'student' && this.instructorID != '') {
-                        const requestOptions = {
-                            method: 'PUT',
-                            headers: { 'Content-Type': 'application/json' },
-                            body: JSON.stringify({
-                                instructor_id: this.instructorID
-                            })
-                        };
-                        var url = '/users/' + this.userDetailsStore.userId + '/edit/instructor';
-                        fetch(url, requestOptions).then(() => {
-                            // refresh user list so the users page will show the update data
-                            this.userDetailsStore.getUserDetails();
-                            this.$router.push('/profile-settings');
-                        });
-                    }else{
+                if (
+                    this.userDetailsStore.role == 'student' &&
+                    this.instructorID != ''
+                ) {
+                    const requestOptions = {
+                        method: 'PUT',
+                        headers: { 'Content-Type': 'application/json' },
+                        body: JSON.stringify({
+                            instructor_id: this.instructorID
+                        })
+                    };
+                    var url =
+                        '/users/' +
+                        this.userDetailsStore.userId +
+                        '/edit/instructor';
+                    fetch(url, requestOptions).then(() => {
                         // refresh user list so the users page will show the update data
                         this.userDetailsStore.getUserDetails();
                         this.$router.push('/profile-settings');
-                    }
-                
-                
+                    });
+                } else {
+                    // refresh user list so the users page will show the update data
+                    this.userDetailsStore.getUserDetails();
+                    this.$router.push('/profile-settings');
+                }
             });
         },
 
@@ -234,6 +245,7 @@ export default {
             Edit Profile
         </h1>
         <div class="row mt-4">
+            <!-- Avatar section -->
             <div class="col-12 col-lg-5">
                 <div class="row mx-0 px-md-0 mb-4 mb-lg-0">
                     <div
@@ -387,6 +399,7 @@ export default {
                     </div>
                 </div>
             </div>
+            <!-- User info section -->
             <div class="col-12 px-4 px-md-0 col-lg-4 px-0 px-md-4 px-lg-0">
                 <div class="d-flex gap-4">
                     <div class="mb-3">
@@ -461,13 +474,57 @@ export default {
                         please enter a valid email !
                     </div>
                 </div>
+                <!-- Password Section -->
                 <div class="mb-3">
                     <label class="form-label">Password</label>
-                    <input
-                        v-model="password"
-                        type="text"
-                        class="form-control"
-                    />
+                    <div class="password-div">
+                        <input
+                            v-model="password"
+                            :type="passwordVisible ? 'text' : 'password'"
+                            placeholder="Password"
+                            class="form-control"
+                            required
+                        />
+                        <!-- Show and Hide Password Section -->
+                        <div
+                            class="eye-icon"
+                            b-tooltip.hover
+                            :title="
+                                passwordVisible
+                                    ? 'hide password'
+                                    : 'show password'
+                            "
+                        >
+                            <!-- Eye Icon -->
+                            <svg
+                                v-if="passwordVisible"
+                                xmlns="http://www.w3.org/2000/svg"
+                                viewBox="0 0 576 512"
+                                width="20"
+                                height="20"
+                                fill="gray"
+                                @click="passwordVisible = false"
+                            >
+                                <path
+                                    d="M288 80c-65.2 0-118.8 29.6-159.9 67.7C89.6 183.5 63 226 49.4 256c13.6 30 40.2 72.5 78.6 108.3C169.2 402.4 222.8 432 288 432s118.8-29.6 159.9-67.7C486.4 328.5 513 286 526.6 256c-13.6-30-40.2-72.5-78.6-108.3C406.8 109.6 353.2 80 288 80zM95.4 112.6C142.5 68.8 207.2 32 288 32s145.5 36.8 192.6 80.6c46.8 43.5 78.1 95.4 93 131.1c3.3 7.9 3.3 16.7 0 24.6c-14.9 35.7-46.2 87.7-93 131.1C433.5 443.2 368.8 480 288 480s-145.5-36.8-192.6-80.6C48.6 356 17.3 304 2.5 268.3c-3.3-7.9-3.3-16.7 0-24.6C17.3 208 48.6 156 95.4 112.6zM288 336c44.2 0 80-35.8 80-80s-35.8-80-80-80c-.7 0-1.3 0-2 0c1.3 5.1 2 10.5 2 16c0 35.3-28.7 64-64 64c-5.5 0-10.9-.7-16-2c0 .7 0 1.3 0 2c0 44.2 35.8 80 80 80zm0-208a128 128 0 1 1 0 256 128 128 0 1 1 0-256z"
+                                />
+                            </svg>
+                            <!-- Eye Slash Icon -->
+                            <svg
+                                v-else
+                                xmlns="http://www.w3.org/2000/svg"
+                                viewBox="0 0 640 512"
+                                width="20"
+                                height="20"
+                                fill="gray"
+                                @click="passwordVisible = true"
+                            >
+                                <path
+                                    d="M38.8 5.1C28.4-3.1 13.3-1.2 5.1 9.2S-1.2 34.7 9.2 42.9l592 464c10.4 8.2 25.5 6.3 33.7-4.1s6.3-25.5-4.1-33.7L525.6 386.7c39.6-40.6 66.4-86.1 79.9-118.4c3.3-7.9 3.3-16.7 0-24.6c-14.9-35.7-46.2-87.7-93-131.1C465.5 68.8 400.8 32 320 32c-68.2 0-125 26.3-169.3 60.8L38.8 5.1zm151 118.3C226 97.7 269.5 80 320 80c65.2 0 118.8 29.6 159.9 67.7C518.4 183.5 545 226 558.6 256c-12.6 28-36.6 66.8-70.9 100.9l-53.8-42.2c9.1-17.6 14.2-37.5 14.2-58.7c0-70.7-57.3-128-128-128c-32.2 0-61.7 11.9-84.2 31.5l-46.1-36.1zM394.9 284.2l-81.5-63.9c4.2-8.5 6.6-18.2 6.6-28.3c0-5.5-.7-10.9-2-16c.7 0 1.3 0 2 0c44.2 0 80 35.8 80 80c0 9.9-1.8 19.4-5.1 28.2zm51.3 163.3l-41.9-33C378.8 425.4 350.7 432 320 432c-65.2 0-118.8-29.6-159.9-67.7C121.6 328.5 95 286 81.4 256c8.3-18.4 21.5-41.5 39.4-64.8L83.1 161.5C60.3 191.2 44 220.8 34.5 243.7c-3.3 7.9-3.3 16.7 0 24.6c14.9 35.7 46.2 87.7 93 131.1C174.5 443.2 239.2 480 320 480c47.8 0 89.9-12.9 126.2-32.5zm-88-69.3L302 334c-23.5-5.4-43.1-21.2-53.7-42.3l-56.1-44.2c-.2 2.8-.3 5.6-.3 8.5c0 70.7 57.3 128 128 128c13.3 0 26.1-2 38.2-5.8z"
+                                />
+                            </svg>
+                        </div>
+                    </div>
                     <div
                         v-if="
                             validate.password &&
@@ -475,14 +532,27 @@ export default {
                         "
                         class="form-validate"
                     >
-                        please enter a password !
+                        please enter a password!
                     </div>
+                    <CheckPasswordComplexity
+                        :formData="{
+                            username: userName,
+                            password: password,
+                            firstName: firstName,
+                            lastName: lastName
+                        }"
+                    />
                 </div>
-                <div  v-if="userDetailsStore.role == 'student'" class="mb-3">
+                <div v-if="userDetailsStore.role == 'student'" class="mb-3">
                     <label class="form-label">Instructor</label>
                     <select v-model="instructorID" class="form-control">
-                        <option value="" disabled selected> - Select instructor -</option>
-                        <option v-for="instructor in userStore.instructors" :value="instructor.id">
+                        <option value="" disabled selected>
+                            - Select instructor -
+                        </option>
+                        <option
+                            v-for="instructor in userStore.instructors"
+                            :value="instructor.id"
+                        >
                             {{ instructor.username }}
                         </option>
                     </select>
@@ -692,6 +762,22 @@ export default {
 
 #img-background {
     border-radius: 12px;
+}
+
+.password-div {
+    position: relative;
+    display: flex;
+    flex-direction: row;
+    align-items: center;
+}
+
+.eye-icon {
+    position: absolute;
+    right: 20px;
+}
+
+.eye-icon:hover {
+    cursor: pointer;
 }
 
 #page-tile {
