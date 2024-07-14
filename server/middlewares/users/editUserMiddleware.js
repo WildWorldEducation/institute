fbaf31c0c044c8d4739b692525b59faa63b1cbc2
RussiSunni <jonathandyason@hotmail.com>
2024-07-14 07:41:18 +0200

    Correcting logic for edit users

--- server/middlewares/users/editUserMiddleware.js
+++ server/middlewares/users/editUserMiddleware.js
@@ -5,36 +5,18 @@ const editUserPermission = (req, res, next) => {
     const userId = req.session.userId;
     const userToEditId = req.params.id;
 
-    // Admin can add any instructor to student
+    // Admin can edit any
     if (userRole === 'admin') {
         return next();
     }
-
-    // Instructor can only add themselves to student
-    else if (userToEditId == userId ) {
+    // Student can edit themselves
+    else if (userToEditId == userId) {
         return next();
-    }
-
-    // Student can't change instructor if one already exists
-    else if (userRole === 'instructor') {
-        let sqlQuery = "SELECT COUNT(*) as count FROM instructor_students WHERE student_id = ? AND instructor_id = ?";
-        let query = conn.query(sqlQuery, [userToEditId, userId], (err, results) => {
-            if (err) {
-                return next(err);
-            }
-            try {
-                if (results[0].count > 0) {
-                    return next();
-                } else {
-                    return res.status(403).json({ message: 'Forbidden: You do not have permission to edit this user' });
-                }
-            } catch (err) {
-                next(err);
-            }
-        });
     } else {
         // If the conditions are not met, forbid the action
-        return res.status(403).json({ message: 'Forbidden: You do not have permission to edit this user' });
+        return res.status(403).json({
+            message: 'Forbidden: You do not have permission to edit this user'
+        });
     }
 };
 
