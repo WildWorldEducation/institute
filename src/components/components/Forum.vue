41a8cae14d5f3caf1c3e761258ab6379fcbc066f
RussiSunni <jonathandyason@hotmail.com>
2024-07-14 17:05:18 +0200

    .

--- src/components/components/Forum.vue
+++ src/components/components/Forum.vue
@@ -332,9 +332,10 @@ export default {
             if (source.type != 'tutor') {
                 fetch('/resources/delete/' + source.id, { method: 'DELETE' });
             } else {
-                fetch('/tutor-posts/delete/' + source.id, { method: 'DELETE' })
-                .then(() => {
-                    this.isAlreadyTutoring = false
+                fetch('/tutor-posts/delete/' + source.id, {
+                    method: 'DELETE'
+                }).then(() => {
+                    this.isAlreadyTutoring = false;
                 });
             }
 
@@ -722,6 +723,33 @@ export default {
                                                     fill="#857D99"
                                                 />
                                             </svg>
+                                        </button>
+                                        <!-- Flag button -->
+                                        <button
+                                            b-tooltip.hover
+                                            title="Flag This Source For Review"
+                                            type="button"
+                                            class="btn dropdown-btn"
+                                            @click="
+                                                handleOpenFlagModal(post.id)
+                                            "
+                                        >
+                                            <div
+                                                class="d-flex flex-row-reverse"
+                                            >
+                                                <svg
+                                                    xmlns="http://www.w3.org/2000/svg"
+                                                    viewBox="0 0 448 512"
+                                                    style="opacity: 0.5"
+                                                    height="20"
+                                                    width="20"
+                                                >
+                                                    <path
+                                                        fill="#8f7bd6"
+                                                        d="M64 32C64 14.3 49.7 0 32 0S0 14.3 0 32V64 368 480c0 17.7 14.3 32 32 32s32-14.3 32-32V352l64.3-16.1c41.1-10.3 84.6-5.5 122.5 13.4c44.2 22.1 95.5 24.8 141.7 7.4l34.7-13c12.5-4.7 20.8-16.6 20.8-30V66.1c0-23-24.2-38-44.8-27.7l-9.6 4.8c-46.3 23.2-100.8 23.2-147.1 0c-35.1-17.6-75.4-22-113.5-12.5L64 48V32z"
+                                                    />
+                                                </svg>
+                                            </div>
                                         </button>
                                     </div>
                                 </Transition>
