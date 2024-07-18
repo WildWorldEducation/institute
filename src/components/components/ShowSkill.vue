b4e35d9fabaa55dc870b9d2a3507f3e71cf5b438
Taan <tannguyen1220@gmail.com>
2024-07-18 18:33:18 +0700

    add a new popup for flag reason

--- src/components/components/ShowSkill.vue
+++ src/components/components/ShowSkill.vue
@@ -45,6 +45,7 @@ export default {
             filters: [],
             showModal: false,
             showThankModal: false,
+            showReasonPopup: false,
             ancestor: this.$route.params.id,
             // accessible List that will use to find nearest un-lockable node
             accessibleSkills: [],
@@ -233,11 +234,21 @@ export default {
                         v-if="isUnlocked && !isMastered"
                         class="btn purple-btn assessment-btn"
                         :to="skillId + '/assessment'"
-                        >
-                        <svg fill="#ffffff" height="45px" width="45px" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 640 512"><!--!Font Awesome Free 6.5.2 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license/free Copyright 2024 Fonticons, Inc. --><path d="M320 32c-8.1 0-16.1 1.4-23.7 4.1L15.8 137.4C6.3 140.9 0 149.9 0 160s6.3 19.1 15.8 22.6l57.9 20.9C57.3 229.3 48 259.8 48 291.9v28.1c0 28.4-10.8 57.7-22.3 80.8c-6.5 13-13.9 25.8-22.5 37.6C0 442.7-.9 448.3 .9 453.4s6 8.9 11.2 10.2l64 16c4.2 1.1 8.7 .3 12.4-2s6.3-6.1 7.1-10.4c8.6-42.8 4.3-81.2-2.1-108.7C90.3 344.3 86 329.8 80 316.5V291.9c0-30.2 10.2-58.7 27.9-81.5c12.9-15.5 29.6-28 49.2-35.7l157-61.7c8.2-3.2 17.5 .8 20.7 9s-.8 17.5-9 20.7l-157 61.7c-12.4 4.9-23.3 12.4-32.2 21.6l159.6 57.6c7.6 2.7 15.6 4.1 23.7 4.1s16.1-1.4 23.7-4.1L624.2 182.6c9.5-3.4 15.8-12.5 15.8-22.6s-6.3-19.1-15.8-22.6L343.7 36.1C336.1 33.4 328.1 32 320 32zM128 408c0 35.3 86 72 192 72s192-36.7 192-72L496.7 262.6 354.5 314c-11.1 4-22.8 6-34.5 6s-23.5-2-34.5-6L143.3 262.6 128 408z"/></svg>
-                        <span>Take Assessment</span>
-                    </router-link
                     >
+                        <svg
+                            fill="#ffffff"
+                            height="45px"
+                            width="45px"
+                            xmlns="http://www.w3.org/2000/svg"
+                            viewBox="0 0 640 512"
+                        >
+                            <!--!Font Awesome Free 6.5.2 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license/free Copyright 2024 Fonticons, Inc. -->
+                            <path
+                                d="M320 32c-8.1 0-16.1 1.4-23.7 4.1L15.8 137.4C6.3 140.9 0 149.9 0 160s6.3 19.1 15.8 22.6l57.9 20.9C57.3 229.3 48 259.8 48 291.9v28.1c0 28.4-10.8 57.7-22.3 80.8c-6.5 13-13.9 25.8-22.5 37.6C0 442.7-.9 448.3 .9 453.4s6 8.9 11.2 10.2l64 16c4.2 1.1 8.7 .3 12.4-2s6.3-6.1 7.1-10.4c8.6-42.8 4.3-81.2-2.1-108.7C90.3 344.3 86 329.8 80 316.5V291.9c0-30.2 10.2-58.7 27.9-81.5c12.9-15.5 29.6-28 49.2-35.7l157-61.7c8.2-3.2 17.5 .8 20.7 9s-.8 17.5-9 20.7l-157 61.7c-12.4 4.9-23.3 12.4-32.2 21.6l159.6 57.6c7.6 2.7 15.6 4.1 23.7 4.1s16.1-1.4 23.7-4.1L624.2 182.6c9.5-3.4 15.8-12.5 15.8-22.6s-6.3-19.1-15.8-22.6L343.7 36.1C336.1 33.4 328.1 32 320 32zM128 408c0 35.3 86 72 192 72s192-36.7 192-72L496.7 262.6 354.5 314c-11.1 4-22.8 6-34.5 6s-23.5-2-34.5-6L143.3 262.6 128 408z"
+                            />
+                        </svg>
+                        <span>Take Assessment</span>
+                    </router-link>
                 </div>
             </div>
             <!-- Edit skill only available for Admin -->
@@ -464,6 +475,7 @@ export default {
                         for admin review?
                     </p>
                 </div>
+                <!-- Buttons row -->
                 <div
                     class="d-flex justify-content-lg-between justify-content-md-end justify-content-between gap-2"
                 >
@@ -490,7 +502,10 @@ export default {
                     <button
                         type="button"
                         class="btn green-btn modal-btn"
-                        @click="flagSkill"
+                        @click="
+                            showModal = false;
+                            showReasonPopup = true;
+                        "
                     >
                         <span class="d-none d-md-block"> Yes </span>
                         <!-- X icon Only show when in Phone View -->
@@ -508,6 +523,70 @@ export default {
                         </svg>
                     </button>
                 </div>
+            </div>
+        </div>
+    </div>
+    <!-- An Popup for user to type reason for flagging -->
+    <div v-if="showReasonPopup">
+        <div id="myModal" class="modal">
+            <!-- Modal content -->
+            <div class="modal">
+                <div class="d-flex flex-column">
+                    <div>Please enter reason to flag this skill:</div>
+                    <textarea
+                        id="story"
+                        name="story"
+                        rows="5"
+                        cols="33"
+                        placeholder="should be no more than 40 word"
+                    >
+                    </textarea>
+                </div>
+            </div>
+            <!-- Buttons row -->
+            <div
+                class="d-flex justify-content-lg-between justify-content-md-end justify-content-between gap-2"
+            >
+                <button
+                    type="button"
+                    class="btn red-btn modal-btn"
+                    @click="showReasonModal = false"
+                >
+                    <span class="d-none d-md-block"> Cancel </span>
+                    <!-- Tick Icon ONLY show when in Phone View -->
+                    <svg
+                        xmlns="http://www.w3.org/2000/svg"
+                        viewBox="0 0 512 512"
+                        width="18"
+                        height="18"
+                        fill="white"
+                        class="d-md-none"
+                    >
+                        <path
+                            d="M256 512A256 256 0 1 0 256 0a256 256 0 1 0 0 512zM175 175c9.4-9.4 24.6-9.4 33.9 0l47 47 47-47c9.4-9.4 24.6-9.4 33.9 0s9.4 24.6 0 33.9l-47 47 47 47c9.4 9.4 9.4 24.6 0 33.9s-24.6 9.4-33.9 0l-47-47-47 47c-9.4 9.4-24.6 9.4-33.9 0s-9.4-24.6 0-33.9l47-47-47-47c-9.4-9.4-9.4-24.6 0-33.9z"
+                        />
+                    </svg>
+                </button>
+                <button
+                    type="button"
+                    class="btn green-btn modal-btn"
+                    @click="flagSkill"
+                >
+                    <span class="d-none d-md-block"> Submit </span>
+                    <!-- X icon Only show when in Phone View -->
+                    <svg
+                        xmlns="http://www.w3.org/2000/svg"
+                        viewBox="0 0 512 512"
+                        width="18"
+                        height="18"
+                        fill="white"
+                        class="d-md-none"
+                    >
+                        <path
+                            d="M256 512A256 256 0 1 0 256 0a256 256 0 1 0 0 512zM369 209L241 337c-9.4 9.4-24.6 9.4-33.9 0l-64-64c-9.4-9.4-9.4-24.6 0-33.9s24.6-9.4 33.9 0l47 47L335 175c9.4-9.4 24.6-9.4 33.9 0s9.4 24.6 0 33.9z"
+                        />
+                    </svg>
+                </button>
             </div>
         </div>
     </div>
@@ -721,10 +800,10 @@ export default {
 .modal-btn {
     width: 25%;
 }
-.assessment-btn svg{
+.assessment-btn svg {
     margin-right: 10px;
 }
-.assessment-btn{
+.assessment-btn {
     height: 60px;
     padding-left: 20px;
     padding-right: 20px;
