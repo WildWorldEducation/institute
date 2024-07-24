3f44d2b48fefd88fd4345f35cdb3d826a3228b55
RussiSunni <jonathandyason@hotmail.com>
2024-07-24 18:17:35 +0200

    .

--- src/components/pages/EditsListView.vue
+++ src/components/pages/EditsListView.vue
@@ -10,6 +10,20 @@ export default {
 };
 </script>
 
-<template>List of edits</template>
+<template>
+    <div class="container">
+        <div class="mt-4 mb-4">
+            <h1 class="page-title">List of edits</h1>
+            <h2>Skills</h2>
+            <h2>Questions</h2>
+        </div>
+    </div>
+</template>
 
-<style></style>
+<style>
+.page-title {
+    color: #a48be7;
+    font-family: 'Poppins', sans-serif;
+    font-weight: 600;
+}
+</style>
