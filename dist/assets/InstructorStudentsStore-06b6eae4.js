d4f4e5ba43c099d6ab4bfa2297d988b1b38460dd
RussiSunni <jonathandyason@hotmail.com>
2024-07-24 12:07:35 +0200

    .

--- dist/assets/InstructorStudentsStore-06b6eae4.js
+++
@@ -1,1 +0,0 @@
-import{L as s}from"./main-c2e9845b.js";const e=s("instructorStudents",{state:()=>({instructorStudentsList:[]}),actions:{async getInstructorStudentsList(){const t=await(await fetch("/instructor-students/list")).json();return this.instructorStudentsList=t,this.$state}}});export{e as u};
---
+++ dist/assets/SkillTreeStudentView-999062ee.js
@@ -0,0 +1,1 @@
+import{_ as s,r as a,o as e,c as o,f as c,g as t,S as r,F as _,b as n,d}from"./main-16cd7587.js";const i={},u=n("div",{id:"purple-banner"},null,-1),p=n("span",null,"Loading...",-1);function S(f,k){const l=a("SkillTreeStudent");return e(),o(_,null,[u,(e(),c(r,null,{default:t(()=>[d(l)]),fallback:t(()=>[p]),_:1}))],64)}const x=s(i,[["render",S]]);export{x as default};
