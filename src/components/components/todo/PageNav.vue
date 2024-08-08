08d1c3b3090b6fd038a53fc471e8dd49a2c5cfd4
Taan <tannguyen1220@gmail.com>
2024-08-09 05:46:24 +0700

    todo list nav container

---
+++ src/components/components/todo/PageNav.vue
@@ -0,0 +1,29 @@
+<script>
+export default {
+    setup() {},
+    data() {},
+    methods: {}
+};
+</script>
+
+<template>
+    <div class="nav-bar-container d-flex flex-column ps-2">
+        <div class="todo-tile">Todo List</div>
+        <hr />
+        <div class="">Approval Edit Content</div>
+        <div class="">Approval Student Question Suggestion</div>
+        <div class="">Checking Content Flag</div>
+    </div>
+</template>
+
+<style scoped>
+.nav-bar-container {
+    height: 100%;
+    background-color: #e8e2f9;
+}
+.todo-tile {
+    font-size: 30px;
+    font-weight: 600;
+    color: #8666ca;
+}
+</style>
--- src/components/pages/TodoListView.vue
+++ src/components/pages/TodoListView.vue
@@ -1,6 +1,7 @@
 <script>
 import CheckStudentQuestions from '../components/CheckStudentQuestions.vue';
 import ContentEditsList from '../components/ContentEditsList.vue';
+import PageNav from '../components/todo/PageNav.vue';
 export default {
     setup() {},
     data() {
@@ -8,20 +9,26 @@ export default {
     },
     components: {
         CheckStudentQuestions,
-        ContentEditsList
+        ContentEditsList,
+        PageNav
     },
     methods: {}
 };
 </script>
 
 <template>
-    <div class="container">
-        <h1 class="page-title">Todo List</h1>
-        <h2>Student Edited Content</h2>
-        <ContentEditsList />
-        <h2>Student Suggested Questions</h2>
-        <CheckStudentQuestions />
-        <h2>Content Flags</h2>
+    <div class="container-fluid h-100 p-0">
+        <div class="d-flex h-100">
+            <PageNav />
+            <div class="d-flex flex-column">
+                <h1 class="page-title">Todo List</h1>
+                <h2>Student Edited Content</h2>
+                <ContentEditsList />
+                <h2>Student Suggested Questions</h2>
+                <CheckStudentQuestions />
+                <h2>Content Flags</h2>
+            </div>
+        </div>
     </div>
 </template>
 
