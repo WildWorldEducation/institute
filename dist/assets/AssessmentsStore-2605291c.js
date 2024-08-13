a4bff6a1c5c80c7d361298fd8940ed1997711fab
RussiSunni <jonathandyason@hotmail.com>
2024-08-13 13:15:03 +0200

    .

---
+++ dist/assets/AssessmentsStore-2605291c.js
@@ -0,0 +1,1 @@
+import{M as t}from"./main-d9facfc8.js";const n=t("assessments",{state:()=>({assessments:[]}),actions:{async getAssessments(){const s=await(await fetch("/assessments/list")).json();return this.assessments=s,this.$state},async deleteAssessment(e){const s=await fetch("/assessments/"+e,{method:"DELETE"});s.error&&console.log(s.error)}}});export{n as u};
