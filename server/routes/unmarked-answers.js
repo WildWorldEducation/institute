395d7ba8e18698094c0de0c993c63dc56a8ecd85
RussiSunni <jonathandyason@hotmail.com>
2024-09-01 08:55:56 +0200

    .

--- server/routes/unmarked-answers.js
+++ server/routes/unmarked-answers.js
@@ -25,7 +25,7 @@ router.get('/list', (req, res, next) => {
     if (req.session.userName) {
         res.setHeader('Content-Type', 'application/json');
         let sqlQuery = 'SELECT * FROM `unmarked_essay_answers`';
-        let query = conn.query(sqlQuery, (err, results) => {
+        conn.query(sqlQuery, (err, results) => {
             try {
                 if (err) {
                     throw err;
@@ -97,17 +97,42 @@ router.post('/add/essay/:assessmentId', (req, res, next) => {
  */
 router.post('/add/image/:assessmentId', (req, res, next) => {
     if (req.session.userName) {
-        // No need to escape single quotes for SQL to accept,
-        // as using '?'.
-        // Add data.
-        let data = {};
-        data = {
-            assessment_id: req.params.assessmentId,
-            answer: req.body.answer,
-            question_id: req.body.questionId
-        };
-        let sqlQuery = 'INSERT INTO unmarked_image_answers SET ?';
-        conn.query(sqlQuery, data, (err, results) => {
+        let questionId = req.body.questionId;
+        let assessmentId = req.params.assessmentId;
+        let answer = req.body.answer;
+        // There may be from 1 to 5 answers.
+        let answer_1, answer_2, answer_3, answer_4, answer_5;
+        if (answer[0]) {
+            answer_1 = answer[0];
+        } else {
+            answer_1 = '';
+        }
+        if (answer[1]) {
+            answer_2 = answer[1];
+        } else {
+            answer_2 = '';
+        }
+        if (answer[2]) {
+            answer_3 = answer[2];
+        } else {
+            answer_3 = '';
+        }
+        if (answer[3]) {
+            answer_4 = answer[3];
+        } else {
+            answer_4 = '';
+        }
+        if (answer[4]) {
+            answer_5 = answer[4];
+        } else {
+            answer_5 = '';
+        }
+
+        let sqlQuery = `INSERT INTO unmarked_image_answers 
+        (assessment_id, answer_1, answer_2, answer_3, answer_4, answer_5, question_id)
+        VALUES (${assessmentId}, '${answer_1}', '${answer_2}', '${answer_3}' ,'${answer_4}' ,'${answer_5}' , ${questionId});`;
+
+        conn.query(sqlQuery, (err) => {
             try {
                 if (err) {
                     throw err;
