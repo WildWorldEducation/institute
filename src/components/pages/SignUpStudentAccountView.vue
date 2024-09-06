6f53bbfdd4cba05509d7fcbe6b270d4e16359f2e
RussiSunni <jonathandyason@hotmail.com>
2024-09-06 10:28:38 +0200

    .

--- src/components/pages/SignUpStudentAccountView.vue
+++ src/components/pages/SignUpStudentAccountView.vue
@@ -25,7 +25,8 @@ export default {
             },
             passwordVisible: false,
             // For Google sign up absolute API url.
-            isProduction: import.meta.env.PROD
+            isProduction: import.meta.env.PROD,
+            showVideoModal: true
         };
     },
     async created() {},
@@ -35,6 +36,15 @@ export default {
         script.setAttribute('src', 'https://accounts.google.com/gsi/client');
         script.setAttribute('defer', '');
         document.head.appendChild(script);
+
+        if (window.innerWidth < 800) {
+            this.showVideoModal = false;
+        }
+
+        // Close modal
+        document.addEventListener('click', () => {
+            this.toggleModal();
+        });
     },
     methods: {
         ValidateForm() {
@@ -98,6 +108,9 @@ export default {
                         alert(data.account);
                     }
                 });
+        },
+        toggleModal() {
+            this.showVideoModal = false;
         }
     }
 };
@@ -105,8 +118,22 @@ export default {
 
 <template>
     <div class="signup-page">
-        <div class="form-signin">
-            <div class="text-center">
+        <div
+            v-if="!showVideoModal"
+            class="embed-responsive embed-responsive-16by9"
+        >
+            <iframe
+                class="intro-video"
+                src="https://www.youtube.com/embed/hu_hjfLLwY0?si=TyvLiAgxcQgmY92q"
+                title="YouTube video player"
+                frameborder="0"
+                allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+                referrerpolicy="strict-origin-when-cross-origin"
+                allowfullscreen
+            ></iframe>
+        </div>
+        <div class="form-signin mt-3">
+            <div v-if="showVideoModal" class="text-center">
                 <img
                     class="mb-4"
                     src="/images/logo-red.png"
@@ -264,15 +291,166 @@ export default {
             </div>
         </div>
     </div>
+    <!-- The flagging Modal -->
+    <div v-if="showWarnModal" id="myModal" class="modal">
+        <!-- Modal content -->
+        <div class="modal-content asking-modal">
+            <div class="d-flex gap-4">
+                <!-- Warn Triangle Icon -->
+                <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    viewBox="0 0 512 512"
+                    fill="grey"
+                    width="45"
+                    height="45"
+                >
+                    <path
+                        d="M256 32c14.2 0 27.3 7.5 34.5 19.8l216 368c7.3 12.4 7.3 27.7 .2 40.1S486.3 480 472 480H40c-14.3 0-27.6-7.7-34.7-20.1s-7-27.8 .2-40.1l216-368C228.7 39.5 241.8 32 256 32zm0 128c-13.3 0-24 10.7-24 24V296c0 13.3 10.7 24 24 24s24-10.7 24-24V184c0-13.3-10.7-24-24-24zm32 224a32 32 0 1 0 -64 0 32 32 0 1 0 64 0z"
+                    />
+                </svg>
+                <p>
+                    Would you like to flag this as unhelpful or incorrect for
+                    admin review?
+                </p>
+            </div>
+            <!-- Buttons row -->
+            <div
+                class="d-flex justify-content-lg-between justify-content-md-end justify-content-between gap-2"
+            >
+                <button
+                    type="button"
+                    class="btn red-btn modal-btn"
+                    @click="closeModal"
+                >
+                    <span class="d-none d-md-block"> No </span>
+                    <!-- Tick Icon ONLY show when in Phone View -->
+                    <svg
+                        xmlns="http://www.w3.org/2000/svg"
+                        viewBox="0 0 512 512"
+                        width="18"
+                        height="18"
+                        fill="white"
+                        class="d-md-none"
+                    >
+                        <path
+                            d="M256 512A256 256 0 1 0 256 0a256 256 0 1 0 0 512zM175 175c9.4-9.4 24.6-9.4 33.9 0l47 47 47-47c9.4-9.4 24.6-9.4 33.9 0s9.4 24.6 0 33.9l-47 47 47 47c9.4 9.4 9.4 24.6 0 33.9s-24.6 9.4-33.9 0l-47-47-47 47c-9.4 9.4-24.6 9.4-33.9 0s-9.4-24.6 0-33.9l47-47-47-47c-9.4-9.4-9.4-24.6 0-33.9z"
+                        />
+                    </svg>
+                </button>
+                <button
+                    type="button"
+                    class="btn green-btn modal-btn"
+                    @click="
+                        showReasonPopup = true;
+                        showWarnModal = false;
+                    "
+                >
+                    <span class="d-none d-md-block"> Yes </span>
+                    <!-- X icon Only show when in Phone View -->
+                    <svg
+                        xmlns="http://www.w3.org/2000/svg"
+                        viewBox="0 0 512 512"
+                        width="18"
+                        height="18"
+                        fill="white"
+                        class="d-md-none"
+                    >
+                        <path
+                            d="M256 512A256 256 0 1 0 256 0a256 256 0 1 0 0 512zM369 209L241 337c-9.4 9.4-24.6 9.4-33.9 0l-64-64c-9.4-9.4-9.4-24.6 0-33.9s24.6-9.4 33.9 0l47 47L335 175c9.4-9.4 24.6-9.4 33.9 0s9.4 24.6 0 33.9z"
+                        />
+                    </svg>
+                </button>
+            </div>
+        </div>
+    </div>
+
+    <!-- An Popup for user to type reason for flagging -->
+    <div v-if="showVideoModal">
+        <div id="myModal" class="modal">
+            <!-- Modal content -->
+            <div class="modal-content">
+                <div
+                    id="modal-iframe"
+                    class="embed-responsive embed-responsive-16by9"
+                >
+                    <iframe
+                        class="intro-video"
+                        src="https://www.youtube.com/embed/hu_hjfLLwY0?si=TyvLiAgxcQgmY92q"
+                        title="YouTube video player"
+                        frameborder="0"
+                        allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+                        referrerpolicy="strict-origin-when-cross-origin"
+                        allowfullscreen
+                    ></iframe>
+                </div>
+            </div>
+        </div>
+    </div>
 </template>
 
 <style>
+/* The Warning Modal */
+.modal {
+    display: block;
+    /* Hidden by default */
+    position: fixed;
+    /* Stay in place */
+    z-index: 1;
+    /* Sit on top */
+    left: 0;
+    top: 0;
+    width: 100%;
+    /* Full width */
+    height: 100%;
+    /* Full height */
+    overflow: hidden;
+    /* Enable scroll if needed */
+    background-color: rgb(0, 0, 0);
+    /* Fallback color */
+    background-color: rgba(0, 0, 0, 0.4);
+    /* Black w/ opacity */
+}
+
+/* Modal Content/Box */
+.modal-content {
+    background-color: #fefefe;
+    margin: 15% auto;
+    /* 15% from the top and centered */
+    padding: 20px;
+    border: 1px solid #888;
+    width: 600px;
+    font-size: 18px;
+    /* Could be more or less, depending on screen size */
+}
+
+.intro-video {
+    display: block;
+    border-radius: 5%;
+    aspect-ratio: 16 / 9;
+    margin: auto;
+}
+
+/* Small devices (portrait phones) */
+@media (max-width: 800px) {
+    .intro-video {
+        width: 100%;
+    }
+}
+
+/* Bigger devices */
+
+#modal-iframe .intro-video {
+    width: 560px;
+}
+
 .signup-page {
     height: 100%;
     padding: 10px;
     background-repeat: no-repeat;
     width: 100%;
     font-family: 'Inter', sans-serif;
+    display: flex;
+    flex-direction: column;
 }
 
 .welcome-message {
