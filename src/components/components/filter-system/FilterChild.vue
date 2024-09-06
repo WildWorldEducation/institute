63519d997acf392d4a9b42c567fbf6fd24415edc
RussiSunni <jonathandyason@hotmail.com>
2024-09-06 15:45:18 +0200

    .

--- src/components/components/filter-system/FilterChild.vue
+++ src/components/components/filter-system/FilterChild.vue
@@ -38,8 +38,11 @@ export default {
     },
     async created() {},
     methods: {
-        showChildSkills() {
+        toggleChildSkills() {
             this.showChildren = !this.showChildren;
+        },
+        filter() {
+            console.log('test');
         }
     },
     components: {}
@@ -50,9 +53,62 @@ export default {
     <button
         :style="indent"
         class="skill-button d-flex justify-content-between align-items-center"
-        @click="showChildSkills()"
+        @click="toggleChildSkills()"
     >
         <span>{{ name }}</span>
+        <div>
+            <!-- Filter Button -->
+            <button class="btn" @click.stop="filter">
+                <!-- Collapse icon -->
+                <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    viewBox="0 0 512 512"
+                    height="18"
+                    width="18"
+                >
+                    <!--!Font Awesome Free 6.6.0 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license/free Copyright 2024 Fonticons, Inc.-->
+                    <path
+                        d="M3.9 54.9C10.5 40.9 24.5 32 40 32l432 0c15.5 0 29.5 8.9 36.1 22.9s4.6 30.5-5.2 42.5L320 320.9 320 448c0 12.1-6.8 23.2-17.7 28.6s-23.8 4.3-33.5-3l-64-48c-8.1-6-12.8-15.5-12.8-25.6l0-79.1L9 97.3C-.7 85.4-2.8 68.8 3.9 54.9z"
+                        fill="#9C7EEC"
+                    />
+                </svg>
+            </button>
+            <!-- Collapse / Expand Button -->
+            <button
+                v-if="children.length != 0"
+                @click.stop="toggleChildSkills"
+                class="btn"
+            >
+                <!-- Collapse icon -->
+                <svg
+                    v-if="!showChildren"
+                    height="18"
+                    width="18"
+                    xmlns="http://www.w3.org/2000/svg"
+                    viewBox="0 0 448 512"
+                >
+                    <!--!Font Awesome Free 6.5.2 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license/free Copyright 2024 Fonticons, Inc. -->
+                    <path
+                        fill="#9C7EEC"
+                        d="M201.4 374.6c12.5 12.5 32.8 12.5 45.3 0l160-160c12.5-12.5 12.5-32.8 0-45.3s-32.8-12.5-45.3 0L224 306.7 86.6 169.4c-12.5-12.5-32.8-12.5-45.3 0s-12.5 32.8 0 45.3l160 160z"
+                    />
+                </svg>
+                <svg
+                    v-else
+                    height="18"
+                    width="18"
+                    xmlns="http://www.w3.org/2000/svg"
+                    viewBox="0 0 448 512"
+                >
+                    <!--!Font Awesome Free 6.5.2 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license/free Copyright 2024 Fonticons, Inc.
+                    -->
+                    <path
+                        fill="#9C7EEC"
+                        d="M201.4 137.4c12.5-12.5 32.8-12.5 45.3 0l160 160c12.5 12.5 12.5 32.8 0 45.3s-32.8 12.5-45.3 0L224 205.3 86.6 342.6c-12.5 12.5-32.8 12.5-45.3 0s-12.5-32.8 0-45.3l160-160z"
+                    />
+                </svg>
+            </button>
+        </div>
     </button>
     <FilterChild
         v-for="child in children"
--- src/components/pages/cohorts/CohortView.vue
+++ src/components/pages/cohorts/CohortView.vue
@@ -1,14 +1,8 @@
 <script>
-// import { useSkillsStore } from '../../../stores/SkillsStore.js';
 import FilterParent from '../../components/filter-system/FilterParent.vue';
 
 export default {
-    setup() {
-        // const skillsStore = useSkillsStore();
-        // return {
-        //     skillsStore
-        // };
-    },
+    setup() {},
     data() {
         return {
             cohortId: this.$route.params.cohortId,
@@ -19,14 +13,6 @@ export default {
     async created() {
         await this.getCohort();
         await this.getCohortMembers();
-        // if (this.skillsStore.nestedSkillsList.length == 0) {
-        //     await this.skillsStore.getNestedSkillsList();
-        // }
-        // console.log(this.skillsStore.nestedSkillsList);
-        // this.renderList(
-        //     document.getElementById('nested-skills'),
-        //     this.skillsStore.nestedSkillsList
-        // );
     },
     components: {
         FilterParent
@@ -50,20 +36,6 @@ export default {
                     this.members = data;
                 });
         }
-        // renderList(parent, array) {
-        //     array.forEach((skill) => {
-        //         var li = document.createElement('li'),
-        //             ul;
-
-        //         li.textContent = skill.name;
-        //         parent.appendChild(li);
-        //         if (skill.children) {
-        //             ul = document.createElement('ul');
-        //             li.appendChild(ul);
-        //             this.renderList(ul, skill.children);
-        //         }
-        //     });
-        // }
     }
 };
 </script>
@@ -71,13 +43,12 @@ export default {
 <template>
     <div class="container">
         <h1>Cohorts: {{ cohort.name }}</h1>
-        <h2>Filters</h2>
         <h2>Members</h2>
         <ul>
             <li v-for="member in members">{{ member.username }}</li>
         </ul>
+        <h2>Filters</h2>
         <FilterParent />
-        <ul id="nested-skills"></ul>
     </div>
 </template>
 
