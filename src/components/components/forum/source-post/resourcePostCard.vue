7ca749ae451037aa260df77826ba4a455c552eec
SAID BIO WEDE <saidbiowede@gmail.com>
2024-09-14 21:32:58 +0100

    Make forum/post buttons web accessible

--- src/components/components/forum/source-post/resourcePostCard.vue
+++ src/components/components/forum/source-post/resourcePostCard.vue
@@ -145,7 +145,8 @@ export default {
             <div class="first-post-row">
                 <div class="d-flex flex-row justify-content-end gap-1">
                     <!-- Upvote Button -->
-                    <div
+                    <button
+                        class="vote-btn"
                         v-if="sessionDetailsStore.isLoggedIn"
                         @click="voteUp(post.id, post.userUpVote)"
                         b-tooltip.hover
@@ -179,7 +180,7 @@ export default {
                                 d="M5,9V21H1V9H5M9,21A2,2 0 0,1 7,19V9C7,8.45 7.22,7.95 7.59,7.59L14.17,1L15.23,2.06C15.5,2.33 15.67,2.7 15.67,3.11L15.64,3.43L14.69,8H21C22.11,8 23,8.9 23,10V12C23,12.26 22.95,12.5 22.86,12.73L19.84,19.78C19.54,20.5 18.83,21 18,21H9M9,19H18.03L21,12V10H12.21L13.34,4.68L9,9.03V19Z"
                             />
                         </svg>
-                    </div>
+                    </button>
                     <!-- Vote count Div -->
                     <span
                         b-on-hover
@@ -192,7 +193,8 @@ export default {
                         >{{ post.voteCount }}</span
                     >
                     <!-- Down vote button -->
-                    <div
+                    <button
+                        class="vote-btn"
                         v-if="sessionDetailsStore.isLoggedIn"
                         b-tooltip.hover
                         title="I Dislike This "
@@ -226,7 +228,7 @@ export default {
                                 d="M19,15V3H23V15H19M15,3A2,2 0 0,1 17,5V15C17,15.55 16.78,16.05 16.41,16.41L9.83,23L8.77,21.94C8.5,21.67 8.33,21.3 8.33,20.88L8.36,20.57L9.31,16H3C1.89,16 1,15.1 1,14V12C1,11.74 1.05,11.5 1.14,11.27L4.16,4.22C4.46,3.5 5.17,3 6,3H15M15,5H5.97L3,12V14H11.78L10.65,19.32L15,14.97V5Z"
                             />
                         </svg>
-                    </div>
+                    </button>
                     <!-- Actions Dropdown Component -->
                     <div
                         v-if="sessionDetailsStore.isLoggedIn"
@@ -396,6 +398,10 @@ export default {
     padding: 10px;
     border-radius: 5px;
     background-color: white;
+}
+.vote-btn{
+    background-color: transparent;
+    border: none;
 }
 /* Mobile */
 @media (max-width: 480px) {
