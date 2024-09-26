326951680f4a1f4ad20356050a3b121345f84c89
SAID BIO WEDE <saidbiowede@gmail.com>
2024-09-26 17:31:25 +0100

    Styling 404 page

--- src/components/pages/PageNotFoundView.vue
+++ src/components/pages/PageNotFoundView.vue
@@ -11,9 +11,16 @@ export default {
 </script>
 
 <template>
-    <h1>Sorry, this page does not exist!</h1>
-    <router-link :to="{name: 'vertical-tree'}">Back to Home</router-link>
+    <div class="d-flex flex-column my-4 align-items-center">
+        <h3 class="text-center text-xs">Sorry, this page does not exist!</h3>
+        <router-link class="btn purple-btn my-3 back-btn":to="{name: 'vertical-tree'}">Back to Home</router-link>
+    </div>
 </template>
 
-<style>
+<style scoped>
+    .back-btn{
+        width: 200px;
+        display: inline-flex;
+        justify-content: center;
+    }
 </style>
