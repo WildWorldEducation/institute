270b74951880e031a240dd8d56f82bb5adab37a9
RussiSunni <jonathandyason@hotmail.com>
2024-09-27 18:34:01 +0200

    .

--- src/components/pages/PageNotFoundView.vue
+++ src/components/pages/PageNotFoundView.vue
@@ -13,14 +13,16 @@ export default {
 <template>
     <div class="d-flex flex-column my-4 align-items-center">
         <h3 class="text-center text-xs">Sorry, this page does not exist!</h3>
-        <router-link class="btn purple-btn my-3 back-btn":to="{name: 'vertical-tree'}">Back to Home</router-link>
+        <router-link class="btn purple-btn my-3 back-btn" :to="{ name: 'hub' }"
+            >Back to Hub</router-link
+        >
     </div>
 </template>
 
 <style scoped>
-    .back-btn{
-        width: 200px;
-        display: inline-flex;
-        justify-content: center;
-    }
+.back-btn {
+    width: 200px;
+    display: inline-flex;
+    justify-content: center;
+}
 </style>
