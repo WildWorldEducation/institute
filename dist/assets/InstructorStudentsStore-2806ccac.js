1a2a91c3c48d8b699d153b9ab4d3e7420e2d92ca
RussiSunni <jonathandyason@hotmail.com>
2024-09-30 17:00:48 +0200

    .

---
+++ dist/assets/InstructorStudentsStore-2806ccac.js
@@ -0,0 +1,1 @@
+import{O as s}from"./main-6c2c7175.js";const e=s("instructorStudents",{state:()=>({instructorStudentsList:[]}),actions:{async getInstructorStudentsList(){const t=await(await fetch("/instructor-students/list")).json();return this.instructorStudentsList=t,this.$state}}});export{e as u};
