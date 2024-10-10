6820f86780c14efc8ec06255ec464f2e1aab0af6
RussiSunni <jonathandyason@hotmail.com>
2024-10-10 10:03:04 +0200

    .

--- src/components/components/ShowSkill.vue
+++ src/components/components/ShowSkill.vue
@@ -275,10 +275,66 @@ export default {
                                 />
                             </svg>
                         </router-link>
+                        <!-- Take assessment btn-->
+                        <!-- If this skill is not unlocked yet, and user is student, instead show link to its closest unlocked ancestor -->
+                        <router-link
+                            :to="'/skills/' + ancestor"
+                            v-if="
+                                userDetailsStore.role == 'student' &&
+                                !isUnlocked &&
+                                !isMastered &&
+                                showAncestorLink
+                            "
+                            class="btn purple-btn text-capitalize"
+                        >
+                            go to nearest unlockable skill
+                        </router-link>
+                        <!-- Take Assessment Button -->
+                        <router-link
+                            v-else-if="
+                                userDetailsStore.role == 'student' &&
+                                isUnlocked &&
+                                !isMastered
+                            "
+                            class="btn purple-btn"
+                            :to="skill.id + '/assessment'"
+                        >
+                            <svg
+                                fill="#ffffff"
+                                height="30"
+                                width="30"
+                                xmlns="http://www.w3.org/2000/svg"
+                                viewBox="0 0 640 512"
+                            >
+                                <!--!Font Awesome Free 6.5.2 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license/free Copyright 2024 Fonticons, Inc. -->
+                                <path
+                                    d="M320 32c-8.1 0-16.1 1.4-23.7 4.1L15.8 137.4C6.3 140.9 0 149.9 0 160s6.3 19.1 15.8 22.6l57.9 20.9C57.3 229.3 48 259.8 48 291.9v28.1c0 28.4-10.8 57.7-22.3 80.8c-6.5 13-13.9 25.8-22.5 37.6C0 442.7-.9 448.3 .9 453.4s6 8.9 11.2 10.2l64 16c4.2 1.1 8.7 .3 12.4-2s6.3-6.1 7.1-10.4c8.6-42.8 4.3-81.2-2.1-108.7C90.3 344.3 86 329.8 80 316.5V291.9c0-30.2 10.2-58.7 27.9-81.5c12.9-15.5 29.6-28 49.2-35.7l157-61.7c8.2-3.2 17.5 .8 20.7 9s-.8 17.5-9 20.7l-157 61.7c-12.4 4.9-23.3 12.4-32.2 21.6l159.6 57.6c7.6 2.7 15.6 4.1 23.7 4.1s16.1-1.4 23.7-4.1L624.2 182.6c9.5-3.4 15.8-12.5 15.8-22.6s-6.3-19.1-15.8-22.6L343.7 36.1C336.1 33.4 328.1 32 320 32zM128 408c0 35.3 86 72 192 72s192-36.7 192-72L496.7 262.6 354.5 314c-11.1 4-22.8 6-34.5 6s-23.5-2-34.5-6L143.3 262.6 128 408z"
+                                />
+                            </svg>
+                        </router-link>
+                        <router-link
+                            v-else-if="!sessionDetailsStore.isLoggedIn"
+                            class="btn purple-btn"
+                            to="/login"
+                        >
+                            <svg
+                                fill="#ffffff"
+                                height="30"
+                                width="30"
+                                xmlns="http://www.w3.org/2000/svg"
+                                viewBox="0 0 640 512"
+                            >
+                                <!--!Font Awesome Free 6.5.2 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license/free Copyright 2024 Fonticons, Inc. -->
+                                <path
+                                    d="M320 32c-8.1 0-16.1 1.4-23.7 4.1L15.8 137.4C6.3 140.9 0 149.9 0 160s6.3 19.1 15.8 22.6l57.9 20.9C57.3 229.3 48 259.8 48 291.9v28.1c0 28.4-10.8 57.7-22.3 80.8c-6.5 13-13.9 25.8-22.5 37.6C0 442.7-.9 448.3 .9 453.4s6 8.9 11.2 10.2l64 16c4.2 1.1 8.7 .3 12.4-2s6.3-6.1 7.1-10.4c8.6-42.8 4.3-81.2-2.1-108.7C90.3 344.3 86 329.8 80 316.5V291.9c0-30.2 10.2-58.7 27.9-81.5c12.9-15.5 29.6-28 49.2-35.7l157-61.7c8.2-3.2 17.5 .8 20.7 9s-.8 17.5-9 20.7l-157 61.7c-12.4 4.9-23.3 12.4-32.2 21.6l159.6 57.6c7.6 2.7 15.6 4.1 23.7 4.1s16.1-1.4 23.7-4.1L624.2 182.6c9.5-3.4 15.8-12.5 15.8-22.6s-6.3-19.1-15.8-22.6L343.7 36.1C336.1 33.4 328.1 32 320 32zM128 408c0 35.3 86 72 192 72s192-36.7 192-72L496.7 262.6 354.5 314c-11.1 4-22.8 6-34.5 6s-23.5-2-34.5-6L143.3 262.6 128 408z"
+                                />
+                            </svg>
+                        </router-link>
+
                         <!-- Sharable URL -->
                         <button
                             @click="copyShareableURLToClipBoard"
-                            class="btn purple-btn"
+                            class="btn"
                         >
                             <svg
                                 xmlns="http://www.w3.org/2000/svg"
@@ -288,7 +344,7 @@ export default {
                             >
                                 <!--!Font Awesome Free 6.6.0 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license/free Copyright 2024 Fonticons, Inc.-->
                                 <path
-                                    fill="white"
+                                    fill="#a48be6"
                                     d="M352 224c53 0 96-43 96-96s-43-96-96-96s-96 43-96 96c0 4 .2 8 .7 11.9l-94.1 47C145.4 170.2 121.9 160 96 160c-53 0-96 43-96 96s43 96 96 96c25.9 0 49.4-10.2 66.6-26.9l94.1 47c-.5 3.9-.7 7.8-.7 11.9c0 53 43 96 96 96s96-43 96-96s-43-96-96-96c-25.9 0-49.4 10.2-66.6 26.9l-94.1-47c.5-3.9 .7-7.8 .7-11.9s-.2-8-.7-11.9l94.1-47C302.6 213.8 326.1 224 352 224z"
                                 />
                             </svg>
@@ -297,7 +353,7 @@ export default {
                         <button
                             v-if="sessionDetailsStore.isLoggedIn"
                             @click="showFlaggingModal = true"
-                            class="btn purple-btn"
+                            class="btn"
                             b-tooltip.hover
                             title="Report this skill to the admin if it has errors"
                         >
@@ -307,7 +363,7 @@ export default {
                                 class="flag-icon"
                             >
                                 <path
-                                    fill="white"
+                                    fill="#a48be6"
                                     d="M64 32C64 14.3 49.7 0 32 0S0 14.3 0 32V64 368 480c0 17.7 14.3 32 32 32s32-14.3 32-32V352l64.3-16.1c41.1-10.3 84.6-5.5 122.5 13.4c44.2 22.1 95.5 24.8 141.7 7.4l34.7-13c12.5-4.7 20.8-16.6 20.8-30V66.1c0-23-24.2-38-44.8-27.7l-9.6 4.8c-46.3 23.2-100.8 23.2-147.1 0c-35.1-17.6-75.4-22-113.5-12.5L64 48V32z"
                                 />
                             </svg>
@@ -342,70 +398,6 @@ export default {
                 </div>
                 <div class="col-md-4 order-1 order-md-2">
                     <div class="info-box p-2">
-                        <!-- Take assessment -->
-                        <div
-                            v-if="
-                                userDetailsStore.role == 'student' ||
-                                !sessionDetailsStore.isLoggedIn
-                            "
-                            class="xs:flex xs:justify-content-center"
-                        >
-                            <div
-                                class="d-flex justify-content-center justify-content-sm-end align-items-start mb-2 mb-md-0"
-                            >
-                                <!-- If this skill is not unlocked yet, and user is student, instead show link to its closest unlocked ancestor -->
-                                <router-link
-                                    :to="'/skills/' + ancestor"
-                                    v-if="
-                                        !isUnlocked &&
-                                        !isMastered &&
-                                        showAncestorLink
-                                    "
-                                    class="btn purple-btn text-capitalize"
-                                >
-                                    go to nearest unlockable skill
-                                </router-link>
-                                <!-- Take Assessment Button -->
-                                <router-link
-                                    v-if="isUnlocked && !isMastered"
-                                    class="btn purple-btn assessment-btn"
-                                    :to="skill.id + '/assessment'"
-                                >
-                                    <svg
-                                        fill="#ffffff"
-                                        height="45px"
-                                        width="45px"
-                                        xmlns="http://www.w3.org/2000/svg"
-                                        viewBox="0 0 640 512"
-                                    >
-                                        <!--!Font Awesome Free 6.5.2 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license/free Copyright 2024 Fonticons, Inc. -->
-                                        <path
-                                            d="M320 32c-8.1 0-16.1 1.4-23.7 4.1L15.8 137.4C6.3 140.9 0 149.9 0 160s6.3 19.1 15.8 22.6l57.9 20.9C57.3 229.3 48 259.8 48 291.9v28.1c0 28.4-10.8 57.7-22.3 80.8c-6.5 13-13.9 25.8-22.5 37.6C0 442.7-.9 448.3 .9 453.4s6 8.9 11.2 10.2l64 16c4.2 1.1 8.7 .3 12.4-2s6.3-6.1 7.1-10.4c8.6-42.8 4.3-81.2-2.1-108.7C90.3 344.3 86 329.8 80 316.5V291.9c0-30.2 10.2-58.7 27.9-81.5c12.9-15.5 29.6-28 49.2-35.7l157-61.7c8.2-3.2 17.5 .8 20.7 9s-.8 17.5-9 20.7l-157 61.7c-12.4 4.9-23.3 12.4-32.2 21.6l159.6 57.6c7.6 2.7 15.6 4.1 23.7 4.1s16.1-1.4 23.7-4.1L624.2 182.6c9.5-3.4 15.8-12.5 15.8-22.6s-6.3-19.1-15.8-22.6L343.7 36.1C336.1 33.4 328.1 32 320 32zM128 408c0 35.3 86 72 192 72s192-36.7 192-72L496.7 262.6 354.5 314c-11.1 4-22.8 6-34.5 6s-23.5-2-34.5-6L143.3 262.6 128 408z"
-                                        />
-                                    </svg>
-                                    <span>Take Assessment</span>
-                                </router-link>
-                                <router-link
-                                    v-if="!sessionDetailsStore.isLoggedIn"
-                                    class="btn purple-btn assessment-btn"
-                                    to="/login"
-                                >
-                                    <svg
-                                        fill="#ffffff"
-                                        height="45px"
-                                        width="45px"
-                                        xmlns="http://www.w3.org/2000/svg"
-                                        viewBox="0 0 640 512"
-                                    >
-                                        <!--!Font Awesome Free 6.5.2 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license/free Copyright 2024 Fonticons, Inc. -->
-                                        <path
-                                            d="M320 32c-8.1 0-16.1 1.4-23.7 4.1L15.8 137.4C6.3 140.9 0 149.9 0 160s6.3 19.1 15.8 22.6l57.9 20.9C57.3 229.3 48 259.8 48 291.9v28.1c0 28.4-10.8 57.7-22.3 80.8c-6.5 13-13.9 25.8-22.5 37.6C0 442.7-.9 448.3 .9 453.4s6 8.9 11.2 10.2l64 16c4.2 1.1 8.7 .3 12.4-2s6.3-6.1 7.1-10.4c8.6-42.8 4.3-81.2-2.1-108.7C90.3 344.3 86 329.8 80 316.5V291.9c0-30.2 10.2-58.7 27.9-81.5c12.9-15.5 29.6-28 49.2-35.7l157-61.7c8.2-3.2 17.5 .8 20.7 9s-.8 17.5-9 20.7l-157 61.7c-12.4 4.9-23.3 12.4-32.2 21.6l159.6 57.6c7.6 2.7 15.6 4.1 23.7 4.1s16.1-1.4 23.7-4.1L624.2 182.6c9.5-3.4 15.8-12.5 15.8-22.6s-6.3-19.1-15.8-22.6L343.7 36.1C336.1 33.4 328.1 32 320 32zM128 408c0 35.3 86 72 192 72s192-36.7 192-72L496.7 262.6 354.5 314c-11.1 4-22.8 6-34.5 6s-23.5-2-34.5-6L143.3 262.6 128 408z"
-                                        />
-                                    </svg>
-                                    <span>Take Assessment</span>
-                                </router-link>
-                            </div>
-                        </div>
                         <!-- Image -->
                         <div>
                             <!-- Skill image -->
@@ -707,15 +699,7 @@ export default {
 .modal-btn {
     width: 25%;
 }
-.assessment-btn svg {
-    margin-right: 10px;
-}
-.assessment-btn {
-    height: 60px;
-    padding-left: 20px;
-    padding-right: 20px;
-    border-bottom: #7f56d9 solid 5px;
-}
+
 /* Specific phone view css */
 @media (max-width: 576px) {
     h1,
