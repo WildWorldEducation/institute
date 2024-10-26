c62c4ff0fb95cc20c5331721453c63f92188780d
Taan <tannguyen1220@gmail.com>
2024-10-26 18:12:48 +0700

    compare warn styling

--- src/components/components/show-skill-revision/components/CompareWithContent.vue
+++ src/components/components/show-skill-revision/components/CompareWithContent.vue
@@ -1,10 +1,17 @@
 <script>
 import { diffWords } from 'diff';
 import HtmlDiff from 'htmldiff-js';
+import LocalizedFormat from 'dayjs/plugin/localizedFormat';
+import dayjs from 'dayjs';
 import CompareString from './CompareString.vue';
 import CompareWithDropdown from './CompareWithDropdown.vue';
 export default {
-    props: ['viewingRevision', 'compareWithRevision', 'skillRevisionHistory'],
+    props: [
+        'viewingRevision',
+        'compareWithRevision',
+        'skillRevisionHistory',
+        'updateCompareWithRevision'
+    ],
     components: { CompareString, CompareWithDropdown },
     computed: {
         compareData() {
@@ -23,15 +30,21 @@ export default {
             const imageDiff =
                 this.viewingRevision.icon_image !==
                 this.compareWithRevision.icon_image;
-            console.log(masteryDiff);
-            console.log(nameDiff);
-            console.log(imageDiff);
+
             return {
                 masteryDiff: masteryDiff,
                 nameDiff: nameDiff,
                 imageDiff: imageDiff,
-                skillData: this.compareWithRevision
+                skillData: this.compareWithRevision,
+                currentViewingRevision: this.viewingRevision
             };
+        }
+    },
+    methods: {
+        formatDate(dateString) {
+            dayjs.extend(LocalizedFormat);
+            const formattedDate = dayjs(dateString).format('lll');
+            return formattedDate;
         }
     }
 };
@@ -50,24 +63,58 @@ export default {
             <hr class="border border-2 opacity-100 hr" />
 
             <div class="d-flex flex-column">
-                <div class="alert alert-warning d-flex" role="alert">
+                <div
+                    class="alert alert-warning d-flex gap-2 align-items-center"
+                    role="alert"
+                >
                     <svg
                         xmlns="http://www.w3.org/2000/svg"
                         viewBox="0 0 512 512"
-                        width="20"
-                        height="20"
+                        width="24"
+                        height="24"
                     >
                         <path
                             d="M256 32c14.2 0 27.3 7.5 34.5 19.8l216 368c7.3 12.4 7.3 27.7 .2 40.1S486.3 480 472 480L40 480c-14.3 0-27.6-7.7-34.7-20.1s-7-27.8 .2-40.1l216-368C228.7 39.5 241.8 32 256 32zm0 128c-13.3 0-24 10.7-24 24l0 112c0 13.3 10.7 24 24 24s24-10.7 24-24l0-112c0-13.3-10.7-24-24-24zm32 224a32 32 0 1 0 -64 0 32 32 0 1 0 64 0z"
                         />
                     </svg>
-                    <!-- <span v-if="!isCurrentVersion" class="ms-2">
-                        This is an old revision of this skill, as edited by
-                        {{ skillRevision.userName }}, at
-                        <span class="warning-date">
-                            {{ skillRevision.date }} </span
-                        >.
-                    </span> -->
+                    <div
+                        v-if="!isCurrentVersion"
+                        class="d-flex flex-column ms-2"
+                    >
+                        You Are Comparing:
+                        <div>
+                            - Revision
+                            {{
+                                compareData.currentViewingRevision
+                                    .version_number
+                            }}
+                            edited by
+                            {{ compareData.currentViewingRevision.userName }} at
+                            <span class="warning-date">
+                                {{
+                                    formatDate(
+                                        compareData.currentViewingRevision
+                                            .edited_date
+                                    )
+                                }}</span
+                            >
+                        </div>
+                        <div>With</div>
+
+                        <div>
+                            - Revision
+                            {{ compareData.skillData.version_number }}
+                            edited by
+                            {{ compareData.skillData.user_name }} at
+                            <span class="warning-date">
+                                {{
+                                    formatDate(
+                                        compareData.skillData?.edited_date
+                                    )
+                                }}</span
+                            >
+                        </div>
+                    </div>
                 </div>
 
                 <CompareWithDropdown
@@ -136,7 +183,24 @@ export default {
                     </div>
                 </div>
             </div>
-
+            <button class="btn purple-btn mt-4">
+                <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    viewBox="0 0 512 512"
+                    fill="white"
+                    height="25"
+                    width="25"
+                    class="me-2"
+                >
+                    <path
+                        d="M48.5 224L40 224c-13.3 0-24-10.7-24-24L16 72c0-9.7 5.8-18.5 14.8-22.2s19.3-1.7 26.2 5.2L98.6 96.6c87.6-86.5 228.7-86.2 315.8 1c87.5 87.5 87.5 229.3 0 316.8s-229.3 87.5-316.8 0c-12.5-12.5-12.5-32.8 0-45.3s32.8-12.5 45.3 0c62.5 62.5 163.8 62.5 226.3 0s62.5-163.8 0-226.3c-62.2-62.2-162.7-62.5-225.3-1L185 183c6.9 6.9 8.9 17.2 5.2 26.2s-12.5 14.8-22.2 14.8L48.5 224z"
+                    />
+                </svg>
+                <div>
+                    Go Back To version:
+                    {{ compareData.currentViewingRevision.version_number }}
+                </div>
+            </button>
             <p>&nbsp;</p>
         </div>
     </div>
