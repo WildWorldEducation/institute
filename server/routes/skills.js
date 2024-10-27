de53bbfa499873e64b160039ac29147f63304776
RussiSunni <jonathandyason@hotmail.com>
2024-10-27 10:16:16 +0200

    .

--- server/routes/skills.js
+++ server/routes/skills.js
@@ -1597,7 +1597,7 @@ async function openAIGenSkillIconImages() {
     let sqlQuery = `SELECT name, url, mastery_requirements FROM skills 
     WHERE type <> 'domain'  
     AND is_deleted = 0    
-    AND id BETWEEN 3723 AND 3730
+    AND id BETWEEN 3741 AND 3750
     ;`;
 
     conn.query(sqlQuery, async (err, results) => {
@@ -1690,6 +1690,6 @@ async function openAIGenSkillIconImages() {
     });
 }
 
-openAIGenSkillIconImages();
+//openAIGenSkillIconImages();
 
 module.exports = router;
