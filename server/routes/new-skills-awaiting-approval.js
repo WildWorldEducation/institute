a55ca6b600f6a9d86508d662633171c67760cf6d
RussiSunni <jonathandyason@hotmail.com>
2024-10-28 08:53:17 +0200

    .

--- server/routes/new-skills-awaiting-approval.js
+++ server/routes/new-skills-awaiting-approval.js
@@ -129,5 +129,90 @@ router.delete('/:id', (req, res, next) => {
     }
 });
 
+/**
+ * Accept skill submitted for review.
+ *
+ * @return response()
+ */
+router.post('/accept/:id', async (req, res, next) => {
+    if (req.session.userName) {
+        // Add the skill.
+        let data = {};
+        data = {
+            name: req.body.name,
+            parent: req.body.parent,
+            mastery_requirements: req.body.mastery_requirements,
+            type: req.body.type,
+            level: req.body.level
+        };
+
+        // Create the skill page url field.
+        data.url = data.name.replace(/\//g, 'or');
+        data.url = data.url.replace(/ /g, '_');
+
+        /*
+         * Send icon image to S3
+         */
+        // Check if empty.
+        if (req.body.icon_image.length > 1) {
+            // Get file from Base64 encoding (client sends as base64)
+            let fileData = Buffer.from(
+                req.body.icon_image.replace(/^data:image\/\w+;base64,/, ''),
+                'base64'
+            );
+
+            let fullSizeData = {
+                // The name it will be saved as on S3
+                Key: data.url,
+                // The image
+                Body: fileData,
+                ContentEncoding: 'base64',
+                ContentType: 'image/jpeg',
+                // The S3 bucket
+                Bucket: skillInfoboxImagesBucketName
+            };
+
+            // Send to the bucket.
+            const fullSizeCommand = new PutObjectCommand(fullSizeData);
+            await s3.send(fullSizeCommand);
+
+            const thumbnailFileData = await sharp(fileData)
+                .resize({ width: 330 })
+                .toBuffer();
+
+            let thumbnailData = {
+                // The name it will be saved as on S3
+                Key: data.url,
+                // The image
+                Body: thumbnailFileData,
+                ContentEncoding: 'base64',
+                ContentType: 'image/jpeg',
+                // The S3 bucket
+                Bucket: skillInfoboxImageThumbnailsBucketName
+            };
+
+            // Send to the bucket.
+            const thumbnailCommand = new PutObjectCommand(thumbnailData);
+            await s3.send(thumbnailCommand);
+        }
+
+        const insertQuery = `INSERT INTO skills SET ?;`;
+
+        conn.query(insertQuery, (err) => {
+            try {
+                if (err) {
+                    throw err;
+                }
+
+                res.end();
+            } catch (err) {
+                next(err);
+            }
+        });
+    } else {
+        res.redirect('/login');
+    }
+});
+
 // Export the router for app to use.
 module.exports = router;
