d0ba8ce3c97f85d801bd000eceb80940b862afd5
RussiSunni <jonathandyason@hotmail.com>
2024-11-06 10:45:33 +0200

    .

--- src/components/components/show-skill-revision/components/CompareWithContent.vue
+++ src/components/components/show-skill-revision/components/CompareWithContent.vue
@@ -72,53 +72,51 @@ export default {
                     class="alert alert-warning d-flex gap-2 align-items-center"
                     role="alert"
                 >
-                    <svg
-                        xmlns="http://www.w3.org/2000/svg"
-                        viewBox="0 0 512 512"
-                        width="24"
-                        height="24"
-                    >
-                        <path
-                            d="M256 32c14.2 0 27.3 7.5 34.5 19.8l216 368c7.3 12.4 7.3 27.7 .2 40.1S486.3 480 472 480L40 480c-14.3 0-27.6-7.7-34.7-20.1s-7-27.8 .2-40.1l216-368C228.7 39.5 241.8 32 256 32zm0 128c-13.3 0-24 10.7-24 24l0 112c0 13.3 10.7 24 24 24s24-10.7 24-24l0-112c0-13.3-10.7-24-24-24zm32 224a32 32 0 1 0 -64 0 32 32 0 1 0 64 0z"
-                        />
-                    </svg>
                     <div
                         v-if="!isCurrentVersion"
                         class="d-flex flex-column ms-2"
                     >
-                        You Are Comparing:
-                        <div>
-                            - Revision
-                            {{
-                                compareData.currentViewingRevision
-                                    .version_number
-                            }}
-                            edited by
-                            {{ compareData.currentViewingRevision.userName }} at
-                            <span class="warning-date">
+                        <p>
+                            <strong>You are comparing:</strong>
+                            <span>
+                                Revision
                                 {{
-                                    formatDate(
-                                        compareData.currentViewingRevision
-                                            .edited_date
-                                    )
-                                }}</span
-                            >
-                        </div>
-                        <div>With</div>
+                                    compareData.currentViewingRevision
+                                        .version_number
+                                }}
+                                edited by
+                                <em>{{
+                                    compareData.currentViewingRevision.userName
+                                }}</em>
+                                on
+                                <span class="warning-date">
+                                    {{
+                                        formatDate(
+                                            compareData.currentViewingRevision
+                                                .edited_date
+                                        )
+                                    }}</span
+                                >
+                            </span>
+                        </p>
+                        <p>
+                            <strong>with:</strong>
 
-                        <div>
-                            - Revision
-                            {{ compareData.skillData.version_number }}
-                            edited by
-                            {{ compareData.skillData.user_name }} at
-                            <span class="warning-date">
-                                {{
-                                    formatDate(
-                                        compareData.skillData?.edited_date
-                                    )
-                                }}</span
-                            >
-                        </div>
+                            <span>
+                                Revision
+                                {{ compareData.skillData.version_number }}
+                                edited by
+                                <em>{{ compareData.skillData.user_name }}</em>
+                                on
+                                <span class="warning-date">
+                                    {{
+                                        formatDate(
+                                            compareData.skillData?.edited_date
+                                        )
+                                    }}</span
+                                >
+                            </span>
+                        </p>
                     </div>
                 </div>
 
@@ -138,9 +136,7 @@ export default {
                     <div v-html="compareData.masteryDiff"></div>
                 </div>
                 <div class="info-box p-2 mb-2">
-                    <div v-if="compareData.imageDiff">
-                        Changed Icon Image to:
-                    </div>
+                    <div v-if="compareData.imageDiff">Icon image was:</div>
                     <img
                         :src="
                             compareData.skillData.icon_image
