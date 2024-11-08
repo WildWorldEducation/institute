6a3faa8ec9dabca58087521b04132dcb9b2e600f
Nguyen Viet Tan <tannguyen1220@gmail.com>
2024-11-08 09:47:23 +0700

    ui for proof of concept

--- src/components/pages/willBeDelete.vue
+++ src/components/pages/willBeDelete.vue
@@ -3,33 +3,48 @@ import LoadingSpinner from '../components/share-components/LoadingSpinner.vue';
 export default {
     data() {
         return {
-            countWords: null
+            countWords: null,
+            resultSkills: [],
+            userInput: '',
+            waitForSever: false
         };
     },
-    async created() {
-        const url = `/skills//find-with-context`;
-        const requestOption = {
-            method: 'POST',
-            headers: { 'Content-Type': 'application/json' },
-            body: JSON.stringify({
-                input: 'Meteor Shower'
-            })
-        };
-        const res = await fetch(url, requestOption);
-        const results = await res.json();
-        console.log(results);
-        this.countWords = results;
+    async created() {},
+    methods: {
+        async aiFindSkill(inputText) {
+            this.waitForSever = true;
+            const url = `/skills//find-with-context`;
+            const requestOption = {
+                method: 'POST',
+                headers: { 'Content-Type': 'application/json' },
+                body: JSON.stringify({
+                    input: inputText
+                })
+            };
+            const res = await fetch(url, requestOption);
+            const results = await res.json();
+            this.resultSkills = results;
+            this.waitForSever = false;
+        }
     },
     components: { LoadingSpinner }
 };
 </script>
 
 <template>
-    <div v-if="countWords">
-        {{ JSON.stringify(countWords) }}
-    </div>
-    <div v-else>
-        <LoadingSpinner />
+    <div class="container">
+        <div class="mx-auto mt-5 w-100 d-flex">
+            <input type="text" v-model="userInput" />
+            <button @click="aiFindSkill(userInput)">Search</button>
+        </div>
+        <div v-if="waitForSever">
+            <LoadingSpinner />
+        </div>
+        <div v-for="skill in resultSkills">
+            <div class="py-2 px-1">
+                {{ skill.skill_name }}
+            </div>
+        </div>
     </div>
 </template>
 
