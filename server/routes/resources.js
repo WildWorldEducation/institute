d4813b3eb62de268b30f935d1e66879d1268963c
RussiSunni <jonathandyason@hotmail.com>
2024-11-11 17:33:36 +0200

    .

--- server/routes/resources.js
+++ server/routes/resources.js
@@ -1156,4 +1156,20 @@ function deleteDownVotedSources(index, sourcesToDelete, userId, respond) {
     });
 }
 
+//const { autoGenerateSource } =
+// /require('../utilities/auto-generate-sources').default;
+
+let aspect = 'Transition to Permanent Settlements';
+let description =
+    'Exploring reasons behind the shift from nomadic hunter-gatherer communities to permanent settlements, influenced by geographical, climatic, and environmental factors.';
+
+//autoGenerateSource(2043, 'high_school', aspect, description);
+
+let { Exa } = require('exa-js');
+//const thing = await import("./main.mjs");
+//import { Exa } from 'exa-js';
+
+const exa = new Exa('2ba8221a-e9fd-41d5-bdc0-46ac4a198f3a');
+console.log(exa);
+
 module.exports = router;
---
+++ server/utilities/auto-generate-sources.js
@@ -0,0 +1,58 @@
+//import Exa from 'exa-js';
+//import { Exa } from 'exa-js';
+const Exa = require('exa-js');
+console.log(Exa);
+const exa = new Exa(process.env.EXA_API_KEY);
+/**
+ *  Get source from Exa.AI
+ *
+ * @param {*} skillId ID of the skill
+ * @param {*} skillLevel level of the skill
+ * @param {*} skillAspect the name of the part of the source we are asking about
+ * @param {*} aspectDescription the description of the part of the source we are asking about
+ *
+ * @return “title”, “url”, and “summary” from EXA.AI results. ENSURE NO DUPLICATES.
+ */
+const autoGenerateSource = async (
+    skillId,
+    skillLevel,
+    skillAspect,
+    aspectDescription
+) => {
+    let ageRange = '';
+    let level = '';
+    switch (skillLevel) {
+        case 'grade_school':
+            ageRange = '5 - 10';
+            level = 'grade school';
+            break;
+        case 'middle_school':
+            ageRange = '11 - 14';
+            level = 'middle school';
+            break;
+        case 'high_school':
+            ageRange = '15 - 18';
+            level = 'high school';
+            break;
+        case 'college':
+            ageRange = '19 - 24';
+            level = 'college';
+            break;
+        case 'phd':
+            ageRange = 'over 24';
+            level = 'PHD';
+    }
+
+    const prompt = `Please provide a website or youtube video or paper on the subject
+    ${skillAspect}, as described by ${aspectDescription}, aimed at ages ${ageRange}, and at a ${level} level.`;
+
+    const result = await exa.searchAndContents(prompt, {
+        type: 'neural',
+        useAutoprompt: true,
+        numResults: 3,
+        text: false
+    });
+    console.log(result);
+};
+
+module.exports = { autoGenerateSource };
