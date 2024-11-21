cbfa3dd033a042f0b97324cd2fb8340010f769a1
Nguyen Viet Tan <tannguyen1220@gmail.com>
2024-11-21 17:12:14 +0700

    todo new skill table

--- src/components/components/todo/new-skills-awaiting-approval/NewSkillsAwaitingApprovalList.vue
+++ src/components/components/todo/new-skills-awaiting-approval/NewSkillsAwaitingApprovalList.vue
@@ -1,6 +1,7 @@
 <script>
 import { useUserDetailsStore } from '../../../../stores/UserDetailsStore';
 import { useUsersStore } from '../../../../stores/UsersStore';
+import NewSkillTable from './NewSkillTable.vue';
 
 export default {
     setup() {
@@ -10,16 +11,20 @@ export default {
     },
     data() {
         return {
-            newSkillsAwaitingApproval: []
+            newSkillsAwaitingApproval: [],
+            loadingSkill: true
         };
     },
-    components: {},
+    components: {
+        NewSkillTable
+    },
     async mounted() {
         await this.getNewSkillsAwaitingApproval();
         if (this.usersStore.users.length < 1) {
             await this.usersStore.getUsers();
         }
         await this.addUserNameToSkillArray();
+        this.loadingSkill = false;
     },
     methods: {
         async getNewSkillsAwaitingApproval() {
@@ -46,10 +51,21 @@ export default {
 
 <template>
     <div class="mt-3 table-div h-100">
-        <div v-for="skill in newSkillsAwaitingApproval">
-            {{ skill.name }}, {{ skill.username }}
+        <!-- Banner -->
+        <div id="banner">
+            <img src="/images/banners/general-banner.png" class="img-fluid" />
         </div>
+        <!-- Page tile -->
+        <h2 class="ps-3 mt-2 page-title">Add New Skill Made By User</h2>
+        <NewSkillTable
+            :newSkillArray="newSkillsAwaitingApproval"
+            :loadingSkill="loadingSkill"
+        />
     </div>
 </template>
 
-<style scoped></style>
+<style scoped>
+.img-fluid {
+    width: 100% !important;
+}
+</style>
