f41139a191cc67885ac8185c956b016959f51343
RussiSunni <jonathandyason@hotmail.com>
2024-11-21 12:02:01 +0200

    .

--- src/components/components/skills-tree-search-bar/tooltips/TurnOnAiModeToolTip.vue
+++ src/components/components/skills-tree-search-bar/tooltips/TurnOnAiModeToolTip.vue
@@ -12,10 +12,10 @@ export default {
 <template>
     <div class="tool-tip-base">
         <div class="explain-tool-tip triangle-top-right">
-            <div>You are currently search skill tree with key word.</div>
+            <div>You are currently searching by keyword.</div>
             <div class="tool-tip-text">
                 <div class="d-flex flex-wrap">
-                    <div>Click on the</div>
+                    <div>Click the</div>
 
                     <svg
                         xmlns="http://www.w3.org/2000/svg"
@@ -29,12 +29,11 @@ export default {
                             d="M320 0c17.7 0 32 14.3 32 32l0 64 120 0c39.8 0 72 32.2 72 72l0 272c0 39.8-32.2 72-72 72l-304 0c-39.8 0-72-32.2-72-72l0-272c0-39.8 32.2-72 72-72l120 0 0-64c0-17.7 14.3-32 32-32zM208 384c-8.8 0-16 7.2-16 16s7.2 16 16 16l32 0c8.8 0 16-7.2 16-16s-7.2-16-16-16l-32 0zm96 0c-8.8 0-16 7.2-16 16s7.2 16 16 16l32 0c8.8 0 16-7.2 16-16s-7.2-16-16-16l-32 0zm96 0c-8.8 0-16 7.2-16 16s7.2 16 16 16l32 0c8.8 0 16-7.2 16-16s-7.2-16-16-16l-32 0zM264 256a40 40 0 1 0 -80 0 40 40 0 1 0 80 0zm152 40a40 40 0 1 0 0-80 40 40 0 1 0 0 80zM48 224l16 0 0 192-16 0c-26.5 0-48-21.5-48-48l0-96c0-26.5 21.5-48 48-48zm544 0c26.5 0 48 21.5 48 48l0 96c0 26.5-21.5 48-48 48l-16 0 0-192 16 0z"
                         />
                     </svg>
-                    <div>to turn on context search</div>
-                    <div>using AI</div>
+                    <div>icon to switch to searching by subject.</div>
                 </div>
             </div>
             <div class="d-none d-lg-flex tooltip-small-text">
-                (You can also press enter while typing to switch mode)
+                (Or just press enter)
             </div>
         </div>
     </div>
