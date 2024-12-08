854e5767ce6525fb6962547299c922ed5158a327
RussiSunni <jonathandyason@hotmail.com>
2024-12-08 15:44:20 +0200

    .

--- src/components/components/EditSkill.vue
+++ src/components/components/EditSkill.vue
@@ -527,8 +527,7 @@ export default {
 
 <template>
     <div class="container bg-light rounded p-3">
-        <h1 v-if="skill.type != 'domain'" class="heading">Edit Skill</h1>
-        <h1 v-else class="heading">Edit Category</h1>
+        <h1 class="heading">{{ skill.name }}</h1>
 
         <div v-if="!isAnotherInstanceOfExistingSkill">
             <div
@@ -1059,7 +1058,7 @@ export default {
                 <div
                     class="d-flex justify-content-end gap-md-4 gap-1 align-items-end"
                 >
-                    <router-link class="btn red-btn" to="/skills">
+                    <button class="btn red-btn" @click="$router.go(-1)">
                         <div class="d-none d-md-block">Cancel</div>
                         <!-- Exit Icon-->
                         <svg
@@ -1074,7 +1073,7 @@ export default {
                                 d="M502.6 278.6c12.5-12.5 12.5-32.8 0-45.3l-128-128c-12.5-12.5-32.8-12.5-45.3 0s-12.5 32.8 0 45.3L402.7 224 192 224c-17.7 0-32 14.3-32 32s14.3 32 32 32l210.7 0-73.4 73.4c-12.5 12.5-12.5 32.8 0 45.3s32.8 12.5 45.3 0l128-128zM160 96c17.7 0 32-14.3 32-32s-14.3-32-32-32L96 32C43 32 0 75 0 128L0 384c0 53 43 96 96 96l64 0c17.7 0 32-14.3 32-32s-14.3-32-32-32l-64 0c-17.7 0-32-14.3-32-32l0-256c0-17.7 14.3-32 32-32l64 0z"
                             />
                         </svg>
-                    </router-link>
+                    </button>
                     <button
                         v-if="
                             userDetailsStore.role == 'admin' ||
