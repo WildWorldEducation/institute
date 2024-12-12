9b74efd506d3257e0b7d05686582fcc5bb5536e9
RussiSunni <jonathandyason@hotmail.com>
2024-12-12 14:00:25 +0200

    .

--- src/components/pages/goals/ListStudentGoalsView.vue
+++ src/components/pages/goals/ListStudentGoalsView.vue
@@ -1,20 +1,35 @@
 <script>
 import { useUserSkillsStore } from '../../../stores/UserSkillsStore.js';
+import { useUsersStore } from '../../../stores/UsersStore.js';
 
 export default {
     setup() {
         const userSkillsStore = useUserSkillsStore();
+        const usersStore = useUsersStore();
         return {
-            userSkillsStore
+            userSkillsStore,
+            usersStore
         };
     },
     data() {
         return {
             studentId: this.$route.params.studentId,
+            studentName: '',
             goals: []
         };
     },
     async created() {
+        if (this.usersStore.users.length == 0) {
+            await this.usersStore.getUsers();
+        }
+
+        for (let i = 0; i < this.usersStore.users.length; i++) {
+            if (this.usersStore.users[i].id == this.studentId) {
+                this.studentName = this.usersStore.users[i].username;
+                break;
+            }
+        }
+
         await this.getGoals();
         await this.userSkillsStore.getFilteredUnnestedList(this.studentId);
 
@@ -73,13 +88,14 @@ export default {
 
 <template>
     <div class="container p-3 bg-light rounded">
-        <h1 class="heading">Goal Progress</h1>
+        <h1 class="heading">{{ studentName }}: Goal Progress</h1>
         <div id="goal-list">
             <div v-for="goal in goals">
-                <div class="d-flex mb-2">
+                <div class="d-flex">
                     <h2 class="goal secondary-heading h4">
                         {{ goal.name }}
 
+                        <!-- Expand/Collapse button -->
                         <button
                             class="btn"
                             @click="goal.showSteps = !goal.showSteps"
@@ -110,6 +126,25 @@ export default {
                                 <!--!Font Awesome Free 6.5.2 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license/free Copyright 2024 Fonticons, Inc. -->
                                 <path
                                     d="M432 256c0 17.7-14.3 32-32 32L48 288c-17.7 0-32-14.3-32-32s14.3-32 32-32l352 0c17.7 0 32 14.3 32 32z"
+                                />
+                            </svg>
+                        </button>
+                        <!-- Delete button -->
+                        <button
+                            class="btn"
+                            @click="goal.showSteps = !goal.showSteps"
+                        >
+                            <!-- Trash sign -->
+                            <svg
+                                xmlns="http://www.w3.org/2000/svg"
+                                viewBox="0 0 448 512"
+                                width="18"
+                                height="18"
+                                class="primary-icon"
+                            >
+                                <!--!Font Awesome Free 6.7.1 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license/free Copyright 2024 Fonticons, Inc.-->
+                                <path
+                                    d="M135.2 17.7L128 32 32 32C14.3 32 0 46.3 0 64S14.3 96 32 96l384 0c17.7 0 32-14.3 32-32s-14.3-32-32-32l-96 0-7.2-14.3C307.4 6.8 296.3 0 284.2 0L163.8 0c-12.1 0-23.2 6.8-28.6 17.7zM416 128L32 128 53.2 467c1.6 25.3 22.6 45 47.9 45l245.8 0c25.3 0 46.3-19.7 47.9-45L416 128z"
                                 />
                             </svg>
                         </button>
