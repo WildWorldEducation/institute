21443f01d4b2278bfb9a02b0bb611b15d6df6ca5
RussiSunni <jonathandyason@hotmail.com>
2024-12-24 15:46:32 +0200

    .

--- dist/assets/InstructorStudentsStore-14b56d53.js
+++
@@ -1,1 +0,0 @@
-import{P as s}from"./main-7e202c46.js";const e=s("instructorStudents",{state:()=>({instructorStudentsList:[]}),actions:{async getInstructorStudentsList(){const t=await(await fetch("/instructor-students/list")).json();return this.instructorStudentsList=t,this.$state}}});export{e as u};
--- dist/assets/ShowSkillStore-f7514992.js
+++
@@ -1,1 +0,0 @@
-import{P as i}from"./main-7e202c46.js";const o=i("showSkill",{state:()=>({skill:null}),actions:{async findSkill(s){const l=await fetch("/skills/url/"+s);this.skill=await l.json()}}});export{o as u};
