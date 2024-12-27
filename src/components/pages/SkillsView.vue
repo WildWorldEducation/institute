889f5a4fca6cc1785aa7a438b2011fc4a6f1b4de
Darko <darko.stojanovski64@gmail.com>
2024-12-27 15:48:09 +0100

    fix/changed-newskillbtn-positioning-adjusted-student-name-heading-refactored-rendering-of-buttons

--- src/components/pages/SkillsView.vue
+++ src/components/pages/SkillsView.vue
@@ -26,7 +26,26 @@ export default {
             chooseResult: null,
             // flag to make watcher do not react when user choose a result
             updateChooseResult: false,
-            nameList: []
+            nameList: [],
+            gradeLevels: [
+                {
+                    level: 'grade_school',
+                    text: 'Grade school',
+                    class: 'grade-school'
+                },
+                {
+                    level: 'middle_school',
+                    text: 'Middle school',
+                    class: 'middle-school'
+                },
+                {
+                    level: 'high_school',
+                    text: 'High school',
+                    class: 'high-school'
+                },
+                { level: 'college', text: 'College', class: 'college' },
+                { level: 'phd', text: 'PHD', class: 'phd' }
+            ]
         };
     },
     components: {
@@ -53,6 +72,10 @@ export default {
         },
         findNode(skillName) {
             this.$refs.skillList.findNode(skillName);
+        },
+        setGradeFilter(level) {
+            this.userDetailsStore.gradeFilter = level;
+            this.$refs.skillList.filter();
         }
     }
 };
@@ -65,6 +88,23 @@ export default {
                 <div v-if="instructorMode" class="col-lg-9">
                     <h1 class="heading h4">Student: {{ studentName }}</h1>
                 </div>
+                <!-- Add skill button -->
+                <router-link class="btn primary-btn me-2" to="/skills/add"
+                    >New skill&nbsp;
+                    <!-- Plus sign -->
+                    <svg
+                        width="18"
+                        height="18"
+                        viewBox="0 0 20 20"
+                        fill="none"
+                        xmlns="http://www.w3.org/2000/svg"
+                    >
+                        <path
+                            d="M6.34811 20.0423L6.34811 13.6494L-0.0358702 13.6583C-0.320945 13.6579 -0.594203 13.5444 -0.795782 13.3428C-0.997361 13.1412 -1.11082 12.868 -1.11132 12.5829L-1.11729 7.41477C-1.1168 7.1297 -1.00334 6.85644 -0.801757 6.65486C-0.600179 6.45328 -0.326921 6.33982 -0.0418461 6.33933L6.3481 6.34231L6.3481 -0.0506238C6.34659 -0.193451 6.3736 -0.335145 6.42756 -0.467396C6.48152 -0.599646 6.56134 -0.719794 6.66234 -0.820794C6.76334 -0.921794 6.88349 -1.00161 7.01574 -1.05557C7.14799 -1.10953 7.28969 -1.13655 7.43251 -1.13503L12.5827 -1.12308C12.8678 -1.12259 13.141 -1.00913 13.3426 -0.807549C13.5442 -0.60597 13.6577 -0.332713 13.6582 -0.047637L13.6552 6.34231L20.0481 6.34231C20.3325 6.34248 20.6052 6.45552 20.8063 6.65661C21.0074 6.8577 21.1204 7.13039 21.1206 7.41477L21.1325 12.565C21.1324 12.8494 21.0193 13.122 20.8182 13.3231C20.6171 13.5242 20.3444 13.6373 20.0601 13.6374L13.6552 13.6494L13.6641 20.0334C13.6636 20.3184 13.5502 20.5917 13.3486 20.7933C13.147 20.9948 12.8738 21.1083 12.5887 21.1088L7.43252 21.1267C7.28969 21.1282 7.148 21.1012 7.01575 21.0473C6.88349 20.9933 6.76335 20.9135 6.66235 20.8125C6.56135 20.7115 6.48153 20.5913 6.42757 20.4591C6.37361 20.3268 6.34659 20.1851 6.34811 20.0423Z"
+                            fill="white"
+                        />
+                    </svg>
+                </router-link>
                 <div class="search-mobile-row">
                     <!-- Search feature -->
                     <SkillTreeSearchBar
@@ -74,89 +114,51 @@ export default {
                 </div>
             </div>
             <div id="tablet-and-up-legend">
-                <div class="legend row">
+                <div class="legend row d-flex align-items-center">
                     <!-- Grade level filter -->
                     <div
                         v-if="
                             !instructorMode && userDetailsStore.role != 'admin'
                         "
-                        class="col-lg-9 d-flex"
+                        class="col d-flex align-items-center"
                     >
-                        <button
-                            class="btn grade-school me-2"
-                            :class="{
-                                'active-grade-filter':
-                                    this.userDetailsStore.gradeFilter ==
-                                    'grade_school'
-                            }"
-                            @click="
-                                this.userDetailsStore.gradeFilter =
-                                    'grade_school';
-                                $refs.skillList.filter();
-                            "
-                        >
-                            Grade school
-                        </button>
+                        <div class="d-flex flex-grow-1">
+                            <button
+                                v-for="grade in gradeLevels"
+                                :key="grade.level"
+                                class="btn me-2 w-100"
+                                :class="{
+                                    'active-grade-filter':
+                                        userDetailsStore.gradeFilter ==
+                                        grade.level,
+                                    [grade.class]: true
+                                }"
+                                @click="setGradeFilter(grade.level)"
+                            >
+                                {{ grade.text }}
+                            </button>
+                            <!-- Skill filters button -->
+                            <div
+                                v-if="userDetailsStore.role == 'admin'"
+                                class="d-flex gap-2"
+                            >
+                                <router-link class="btn primary-btn" to="/tags"
+                                    >Skill Filters</router-link
+                                >
+                            </div>
+                        </div>
+                    </div>
+                    <div v-else-if="instructorMode" class="col text-center">
+                        <h1 class="heading h5">Student: {{ studentName }}</h1>
+                    </div>
 
-                        <button
-                            class="btn middle-school me-2"
-                            :class="{
-                                'active-grade-filter':
-                                    this.userDetailsStore.gradeFilter ==
-                                    'middle_school'
-                            }"
-                            @click="
-                                this.userDetailsStore.gradeFilter =
-                                    'middle_school';
-                                $refs.skillList.filter();
-                            "
-                        >
-                            Middle school
-                        </button>
-                        <button
-                            class="btn high-school me-2"
-                            :class="{
-                                'active-grade-filter':
-                                    this.userDetailsStore.gradeFilter ==
-                                    'high_school'
-                            }"
-                            @click="
-                                this.userDetailsStore.gradeFilter =
-                                    'high_school';
-                                $refs.skillList.filter();
-                            "
-                        >
-                            High school
-                        </button>
-                        <button
-                            class="btn college me-2"
-                            :class="{
-                                'active-grade-filter':
-                                    this.userDetailsStore.gradeFilter ==
-                                    'college'
-                            }"
-                            @click="
-                                this.userDetailsStore.gradeFilter = 'college';
-                                $refs.skillList.filter();
-                            "
-                        >
-                            College
-                        </button>
-                        <button
-                            class="btn phd me-2"
-                            :class="{
-                                'active-grade-filter':
-                                    this.userDetailsStore.gradeFilter == 'phd'
-                            }"
-                            @click="
-                                this.userDetailsStore.gradeFilter = 'phd';
-                                $refs.skillList.filter();
-                            "
-                        >
-                            PHD
-                        </button>
+                    <div
+                        class="col d-flex justify-content-end align-items-center"
+                    >
                         <!-- Add skill button -->
-                        <router-link class="btn primary-btn" to="/skills/add"
+                        <router-link
+                            class="btn primary-btn mt-1 me-2"
+                            to="/skills/add"
                             >New skill&nbsp;
                             <!-- Plus sign -->
                             <svg
@@ -169,23 +171,9 @@ export default {
                                 <path
                                     d="M6.34811 20.0423L6.34811 13.6494L-0.0358702 13.6583C-0.320945 13.6579 -0.594203 13.5444 -0.795782 13.3428C-0.997361 13.1412 -1.11082 12.868 -1.11132 12.5829L-1.11729 7.41477C-1.1168 7.1297 -1.00334 6.85644 -0.801757 6.65486C-0.600179 6.45328 -0.326921 6.33982 -0.0418461 6.33933L6.3481 6.34231L6.3481 -0.0506238C6.34659 -0.193451 6.3736 -0.335145 6.42756 -0.467396C6.48152 -0.599646 6.56134 -0.719794 6.66234 -0.820794C6.76334 -0.921794 6.88349 -1.00161 7.01574 -1.05557C7.14799 -1.10953 7.28969 -1.13655 7.43251 -1.13503L12.5827 -1.12308C12.8678 -1.12259 13.141 -1.00913 13.3426 -0.807549C13.5442 -0.60597 13.6577 -0.332713 13.6582 -0.047637L13.6552 6.34231L20.0481 6.34231C20.3325 6.34248 20.6052 6.45552 20.8063 6.65661C21.0074 6.8577 21.1204 7.13039 21.1206 7.41477L21.1325 12.565C21.1324 12.8494 21.0193 13.122 20.8182 13.3231C20.6171 13.5242 20.3444 13.6373 20.0601 13.6374L13.6552 13.6494L13.6641 20.0334C13.6636 20.3184 13.5502 20.5917 13.3486 20.7933C13.147 20.9948 12.8738 21.1083 12.5887 21.1088L7.43252 21.1267C7.28969 21.1282 7.148 21.1012 7.01575 21.0473C6.88349 20.9933 6.76335 20.9135 6.66235 20.8125C6.56135 20.7115 6.48153 20.5913 6.42757 20.4591C6.37361 20.3268 6.34659 20.1851 6.34811 20.0423Z"
                                     fill="white"
-                                /></svg
-                        ></router-link>
-                        <!-- Skill filters button -->
-                        <div
-                            v-if="userDetailsStore.role == 'admin'"
-                            class="d-flex gap-2"
-                        >
-                            <router-link class="btn primary-btn" to="/tags"
-                                >Skill Filters</router-link
-                            >
-                        </div>
-                    </div>
-                    <div v-else-if="instructorMode" class="col-lg-9">
-                        <h1 class="heading">Student: {{ studentName }}</h1>
-                    </div>
-
-                    <div class="col-lg-3">
+                                />
+                            </svg>
+                        </router-link>
                         <!-- Search Feature -->
                         <SkillTreeSearchBar
                             class="me-4"
@@ -320,7 +308,7 @@ export default {
     }
 
     #mobile-legend {
-        display: block;
+        display: flex;
     }
 
     #tablet-and-up-legend {
