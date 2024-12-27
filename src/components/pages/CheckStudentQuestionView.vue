ea8192e4cf2304ad966e41dc8212ed1d7e47cab2
Darko <darko.stojanovski64@gmail.com>
2024-12-27 17:01:19 +0100

    fix/buttons-spacing-fix-answer-btn-made-same-size-as-remove-btn

--- src/components/pages/CheckStudentQuestionView.vue
+++ src/components/pages/CheckStudentQuestionView.vue
@@ -224,37 +224,38 @@ export default {
                         </div>
                     </div>
 
-                    <!-- Remove Answer Button (visible only if more than 2 answers) -->
-                    <button
-                        v-if="answers[2].show == true"
-                        @click="removeAnswer()"
-                        data-v-ea3cd1bf=""
-                        type="button"
-                        class="btn btn red-btn p-2"
-                        title="Delete answer"
-                        :disabled="!isEditMode"
-                    >
-                        <svg
+                    <div class="d-flex align-items-center">
+                        <!-- Remove Answer Button (visible only if more than 2 answers) -->
+                        <button
+                            v-if="answers[2].show == true"
+                            @click="removeAnswer()"
                             data-v-ea3cd1bf=""
-                            xmlns="http://www.w3.org/2000/svg"
-                            viewBox="0 0 512 512"
-                            width="15"
-                            height="15"
-                            fill="white"
+                            type="button"
+                            class="btn btn red-btn p-2 me-2"
+                            title="Delete answer"
+                            :disabled="!isEditMode"
                         >
-                            <path
+                            <svg
                                 data-v-ea3cd1bf=""
-                                d="M170.5 51.6L151.5 80h145l-19-28.4c-1.5-2.2-4-3.6-6.7-3.6H177.1c-2.7 0-5.2 1.3-6.7 3.6zm147-26.6L354.2 80H368h48 8c13.3 0 24 10.7 24 24s-10.7 24-24 24h-8V432c0 44.2-35.8 80-80 80H112c-44.2 0-80-35.8-80-80V128H24c-13.3 0-24-10.7-24-24S10.7 80 24 80h8H80 93.8l36.7-55.1C140.9 9.4 158.4 0 177.1 0h93.7c18.7 0 36.2 9.4 46.6 24.9zM80 128V432c0 17.7 14.3 32 32 32H336c17.7 0 32-14.3 32-32V128H80zm80 64V400c0 8.8-7.2 16-16 16s-16-7.2-16-16V192c0-8.8 7.2-16 16-16s16 7.2 16 16zm80 0V400c0 8.8-7.2 16-16 16s-16-7.2-16-16V192c0-8.8 7.2-16 16-16s16 7.2 16 16zm80 0V400c0 8.8-7.2 16-16 16s-16-7.2-16-16V192c0-8.8 7.2-16 16-16s16 7.2 16 16z"
-                            ></path></svg
-                        >&nbsp;Remove Answer
-                    </button>
+                                xmlns="http://www.w3.org/2000/svg"
+                                viewBox="0 0 512 512"
+                                width="15"
+                                height="15"
+                                fill="white"
+                            >
+                                <path
+                                    data-v-ea3cd1bf=""
+                                    d="M170.5 51.6L151.5 80h145l-19-28.4c-1.5-2.2-4-3.6-6.7-3.6H177.1c-2.7 0-5.2 1.3-6.7 3.6zm147-26.6L354.2 80H368h48 8c13.3 0 24 10.7 24 24s-10.7 24-24 24h-8V432c0 44.2-35.8 80-80 80H112c-44.2 0-80-35.8-80-80V128H24c-13.3 0-24-10.7-24-24S10.7 80 24 80h8H80 93.8l36.7-55.1C140.9 9.4 158.4 0 177.1 0h93.7c18.7 0 36.2 9.4 46.6 24.9zM80 128V432c0 17.7 14.3 32 32 32H336c17.7 0 32-14.3 32-32V128H80zm80 64V400c0 8.8-7.2 16-16 16s-16-7.2-16-16V192c0-8.8 7.2-16 16-16s16 7.2 16 16zm80 0V400c0 8.8-7.2 16-16 16s-16-7.2-16-16V192c0-8.8 7.2-16 16-16s16 7.2 16 16zm80 0V400c0 8.8-7.2 16-16 16s-16-7.2-16-16V192c0-8.8 7.2-16 16-16s16 7.2 16 16z"
+                                ></path>
+                            </svg>
+                            &nbsp;Remove Answer
+                        </button>
 
-                    <!-- Add Answer Button (max 5 answers) -->
-                    <div class="mb-3">
+                        <!-- Add Answer Button (max 5 answers) -->
                         <button
                             v-if="answers[4].show == false"
                             @click="addAnswer"
-                            class="btn primary-btn"
+                            class="btn btn add-answer-btn primary-btn p-2"
                             :disabled="!isEditMode"
                         >
                             <svg
@@ -272,7 +273,6 @@ export default {
                             Add Answer
                         </button>
                     </div>
-
                     <!-- Random Order Toggle -->
                     <div class="mb-3 form-check">
                         <input
@@ -441,6 +441,17 @@ export default {
 
 .red-btn:hover {
     background-color: #fc7d7d;
+}
+
+.add-answer-btn{
+    font-family: 'Poppins', sans-serif;
+    font-weight: 500;
+    font-size: 16px;
+    line-height: 24px;
+    display: flex;
+    align-items: center;
+    max-width: fit-content;
+    max-height: 44px;
 }
 
 /* The Warning Modal */
