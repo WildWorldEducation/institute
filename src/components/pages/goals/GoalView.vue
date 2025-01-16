b8c456a262754060bbc63846a2bba7528e096c6c
Darko <darko.stojanovski64@gmail.com>
2025-01-16 13:51:02 +0100

    feat/changed-to-btn-added-icon-within

--- src/components/pages/goals/GoalView.vue
+++ src/components/pages/goals/GoalView.vue
@@ -62,8 +62,8 @@ export default {
     <div class="container p-3 bg-light rounded">
         <h1 class="heading">Goal Progress</h1>
 
-        <p class="mb-2" v-for="(goalStep, index) in goalSteps">
-            {{ index + 1 }})
+        <div class="mb-2 d-flex align-items-center gap-1" v-for="(goalStep, index) in goalSteps">
+            <p id="goal-index">{{ index + 1 }})</p>
             <svg
                 v-if="goalStep.isMastered != 1"
                 xmlns="http://www.w3.org/2000/svg"
@@ -90,29 +90,30 @@ export default {
                     d="M64 80c-8.8 0-16 7.2-16 16l0 320c0 8.8 7.2 16 16 16l320 0c8.8 0 16-7.2 16-16l0-320c0-8.8-7.2-16-16-16L64 80zM0 96C0 60.7 28.7 32 64 32l320 0c35.3 0 64 28.7 64 64l0 320c0 35.3-28.7 64-64 64L64 480c-35.3 0-64-28.7-64-64L0 96zM337 209L209 337c-9.4 9.4-24.6 9.4-33.9 0l-64-64c-9.4-9.4-9.4-24.6 0-33.9s24.6-9.4 33.9 0l47 47L303 175c9.4-9.4 24.6-9.4 33.9 0s9.4 24.6 0 33.9z"
                 />
             </svg>
-            &nbsp;
-            <router-link
-                class="goals"
-                :to="`/skills/${goalStep.url}`"
-                target="_blank"
-            >
-                {{ goalStep.name }}
-            </router-link>
-            &nbsp;
-            <svg
-                v-if="index + 1 == goalSteps.length"
-                xmlns="http://www.w3.org/2000/svg"
-                viewBox="0 0 512 512"
-                class="primary-icon"
-                width="20"
-                heigth="20"
-            >
-                <!-- !Font Awesome Free 6.7.1 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license/free Copyright 2024 Fonticons, Inc. -->
-                <path
-                    d="M448 256A192 192 0 1 0 64 256a192 192 0 1 0 384 0zM0 256a256 256 0 1 1 512 0A256 256 0 1 1 0 256zm256 80a80 80 0 1 0 0-160 80 80 0 1 0 0 160zm0-224a144 144 0 1 1 0 288 144 144 0 1 1 0-288zM224 256a32 32 0 1 1 64 0 32 32 0 1 1 -64 0z"
-                />
-            </svg>
-        </p>
+            <button class="btn primary-btn">
+                <router-link
+                    class="goals"
+                    :to="`/skills/${goalStep.url}`"
+                    target="_blank"
+                >
+                    {{ goalStep.name }}
+                </router-link>
+                &nbsp;
+                <svg
+                    v-if="index + 1 == goalSteps.length"
+                    xmlns="http://www.w3.org/2000/svg"
+                    viewBox="0 0 512 512"
+                    fill="white"
+                    width="20"
+                    heigth="20"
+                >
+                    <!-- !Font Awesome Free 6.7.1 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license/free Copyright 2024 Fonticons, Inc. -->
+                    <path
+                        d="M448 256A192 192 0 1 0 64 256a192 192 0 1 0 384 0zM0 256a256 256 0 1 1 512 0A256 256 0 1 1 0 256zm256 80a80 80 0 1 0 0-160 80 80 0 1 0 0 160zm0-224a144 144 0 1 1 0 288 144 144 0 1 1 0-288zM224 256a32 32 0 1 1 64 0 32 32 0 1 1 -64 0z"
+                    />
+                </svg>
+            </button>
+        </div>
 
         <button class="btn red-btn mt-3" @click="deleteGoal()">Delete</button>
     </div>
@@ -120,9 +121,12 @@ export default {
 
 <style>
 .goals {
-    color: black;
+    color: white !important;
+    text-decoration: none !important;
 }
-
+#goal-index{
+    margin: 0;
+}
 /* Level colors */
 .grade-school {
     background-color: #40e0d0;
