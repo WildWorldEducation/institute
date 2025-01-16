89a9d8492788a80a9d6867267b8ef5c769c83cc3
RussiSunni <jonathandyason@hotmail.com>
2025-01-16 11:39:57 +0200

    Moved modal HTML to proper place, navbar no longer showing above modal

--- src/components/pages/TidyTreeView.vue
+++ src/components/pages/TidyTreeView.vue
@@ -328,20 +328,6 @@ export default {
 
 <template>
     <div class="container-fluid position-absolute legend-div">
-        <div v-if="showWelcomeModal" class="modal">
-            <div class="modal-content">
-                <h1 class="heading h3">Welcome to the Collins Institute</h1>
-                <p>Would you like to go through the walkthrough tutorial?</p>
-                <div class="d-flex justify-content-between">
-                    <button class="btn primary-btn mx-0" @click="startTutorial">
-                        Yes
-                    </button>
-                    <button class="btn primary-btn mx-0" @click="closeTutorial">
-                        No
-                    </button>
-                </div>
-            </div>
-        </div>
         <div class="mobile-legend">
             <div class="search-mobile-row">
                 <!-- Search feature -->
@@ -1491,7 +1477,21 @@ export default {
         </div>
     </div>
 
-    <!-- Onboarding tooltip modal -->
+    <!-- Onboarding tooltip modals -->
+    <div v-if="showWelcomeModal" class="modal">
+        <div class="modal-content">
+            <h1 class="heading h3">Welcome to the Collins Institute</h1>
+            <p>Would you like to go through the walkthrough tutorial?</p>
+            <div class="d-flex justify-content-between">
+                <button class="btn primary-btn mx-0" @click="startTutorial">
+                    Yes
+                </button>
+                <button class="btn primary-btn mx-0" @click="closeTutorial">
+                    No
+                </button>
+            </div>
+        </div>
+    </div>
     <div
         v-if="
             showTutorialTip1 ||
