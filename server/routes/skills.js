7d49cecf5bb19e32e37d5884bfb5b19a385ca23f
RussiSunni <jonathandyason@hotmail.com>
2025-01-17 10:51:03 +0200

    .

--- server/routes/skills.js
+++ server/routes/skills.js
@@ -1802,7 +1802,7 @@ let skillsLength;
 async function generateIntroductionText() {
     let sqlQuery = `SELECT id, level, name, mastery_requirements FROM skills 
         WHERE is_deleted = 0
-        AND id BETWEEN 501 AND 2000`;
+        AND id BETWEEN 597 AND 2000`;
 
     conn.query(sqlQuery, (err, results) => {
         try {
