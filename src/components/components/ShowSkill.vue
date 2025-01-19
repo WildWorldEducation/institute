13580552b3037c19956a27ee194fb1ea9057b3bc
RussiSunni <jonathandyason@hotmail.com>
2025-01-19 02:54:21 +0200

    .

--- src/components/components/ShowSkill.vue
+++ src/components/components/ShowSkill.vue
@@ -164,7 +164,9 @@ export default {
             );
             this.getUserSkills();
         },
-
+        async markComplete() {
+            console.log('test');
+        },
         closeFlagModal() {
             this.showModal = false;
         },
@@ -431,7 +433,8 @@ export default {
                         v-else-if="
                             userDetailsStore.role == 'student' &&
                             isUnlocked &&
-                            !isMastered
+                            !isMastered &&
+                            skill.type != 'domain'
                         "
                         class="btn me-1 assessment-btn secondary-btn"
                         :to="skill.id + '/assessment'"
@@ -463,6 +466,43 @@ export default {
                             />
                         </svg>
                     </router-link>
+                    <button
+                        v-else-if="
+                            userDetailsStore.role == 'student' &&
+                            isUnlocked &&
+                            !isMastered &&
+                            skill.type == 'domain'
+                        "
+                        @click="markComplete()"
+                        class="btn me-1 assessment-btn secondary-btn"
+                    >
+                        <!-- Half star icon -->
+                        <svg
+                            xmlns="http://www.w3.org/2000/svg"
+                            viewBox="0 0 576 512"
+                            width="22"
+                            class="primary-icon"
+                        >
+                            <!-- !Font Awesome Free 6.7.2 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license/free Copyright 2024 Fonticons, Inc. -->
+                            <path
+                                d="M288 0c-12.2 .1-23.3 7-28.6 18L195 150.3 51.4 171.5c-12 1.8-22 10.2-25.7 21.7s-.7 24.2 7.9 32.7L137.8 329 113.2 474.7c-2 12 3 24.2 12.9 31.3s23 8 33.8 2.3L288 439.8 288 0zM429.9 512c1.1 .1 2.1 .1 3.2 0l-3.2 0z"
+                            />
+                        </svg>
+                        Mark complete
+                        <!-- Half star icon -->
+                        <svg
+                            xmlns="http://www.w3.org/2000/svg"
+                            viewBox="0 0 576 512"
+                            width="22"
+                            class="primary-icon"
+                        >
+                            <!-- !Font Awesome Free 6.7.2 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license/free Copyright 2024 Fonticons, Inc. -->
+                            <path
+                                d="m 169.24356,0 c 12.2,0.1 23.3,7 28.6,18 l 64.4,132.3 143.6,21.2 c 12,1.8 22,10.2 25.7,21.7 3.7,11.5 0.7,24.2 -7.9,32.7 l -104.2,103.1 24.6,145.7 c 2,12 -3,24.2 -12.9,31.3 -9.9,7.1 -23,8 -33.8,2.3 l -128.1,-68.5 z M 27.343555,512 c -1.1,0.1 -2.1,0.1 -3.2,0 z"
+                                id="path17"
+                            />
+                        </svg>
+                    </button>
                     <!-- If not logged in, go to Login page -->
                     <router-link
                         v-else-if="!sessionDetailsStore.isLoggedIn"
@@ -481,7 +521,8 @@ export default {
                                 d="M288 0c-12.2 .1-23.3 7-28.6 18L195 150.3 51.4 171.5c-12 1.8-22 10.2-25.7 21.7s-.7 24.2 7.9 32.7L137.8 329 113.2 474.7c-2 12 3 24.2 12.9 31.3s23 8 33.8 2.3L288 439.8 288 0zM429.9 512c1.1 .1 2.1 .1 3.2 0l-3.2 0z"
                             />
                         </svg>
-                        Take the Test
+                        <span v-if="skill.type != 'domain'">Take the Test</span
+                        ><span v-else>Mark Complete</span>
                         <!-- Half star icon -->
                         <svg
                             xmlns="http://www.w3.org/2000/svg"
