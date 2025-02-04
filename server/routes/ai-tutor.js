70ec5fff594e5fe8dbd535d3aa4c58e1f9fd88b6
RussiSunni <jonathandyason@hotmail.com>
2025-02-04 20:14:25 +0200

    .

--- server/routes/ai-tutor.js
+++ server/routes/ai-tutor.js
@@ -14,43 +14,65 @@ const openai = new OpenAI({
     apiKey: process.env.OPENAI_API_KEY
 });
 
-// Create an Assistant
-async function main() {
-    const assistant = await openai.beta.assistants.create({
-        name: 'General Tutor',
-        instructions:
-            'You are a personal tutor. Answer any questions you are asked..',
-        tools: [{ type: 'code_interpreter' }],
-        model: 'gpt-4o'
-    });
+/**
+ * Create New Item
+ *
+ * @return response()
+ */
+router.post('/', async (req, res, next) => {
+    let userQuestion = req.body.message;
+    if (req.session.userName) {
+        // Create an Assistant
 
-    // Create a Thread
-    const thread = await openai.beta.threads.create();
-    // console.log(thread);
+        const assistant = await openai.beta.assistants.create({
+            name: 'General Tutor',
+            instructions:
+                'You are a personal tutor. Answer any questions you are asked..',
+            tools: [{ type: 'code_interpreter' }],
+            model: 'gpt-4o'
+        });
 
-    // Add a Message to the Thread
-    const message = await openai.beta.threads.messages.create(thread.id, {
-        role: 'user',
-        content: 'I need to solve the equation `3x + 11 = 14`. Can you help me?'
-    });
+        // Create a Thread
+        const thread = await openai.beta.threads.create();
+        // console.log(thread);
 
-    let run = await openai.beta.threads.runs.createAndPoll(thread.id, {
-        assistant_id: assistant.id,
-        instructions:
-            'Please address the user as Jane Doe. The user has a premium account.'
-    });
+        // Add a Message to the Thread
+        const message = await openai.beta.threads.messages.create(thread.id, {
+            role: 'user',
+            content: userQuestion
+        });
 
-    if (run.status === 'completed') {
-        const messages = await openai.beta.threads.messages.list(run.thread_id);
-        for (const message of messages.data.reverse()) {
-            console.log(`${message.role} > ${message.content[0].text.value}`);
+        let run = await openai.beta.threads.runs.createAndPoll(thread.id, {
+            assistant_id: assistant.id,
+            instructions:
+                'Please address the user as Jane Doe. The user has a premium account.'
+        });
+
+        if (run.status === 'completed') {
+            const messages = await openai.beta.threads.messages.list(
+                run.thread_id
+            );
+            console.log(messages.length);
+            for (const message of messages.data.reverse()) {
+                // console.log(
+                //     `${message.role} > ${message.content[0].text.value}`
+                // );
+                //console.log(message.content[0].text.value);
+                //    res.json({ answer: message.content[0].text.value });
+            }
+            res.json({ message: messages.data.reverse() });
+        } else {
+            console.log(run.status);
         }
-    } else {
-        console.log(run.status);
-    }
-}
 
-main();
+        // res.json({
+        //     message: `${message.role} > ${message.content[0].text.value}`
+        // });
+
+        //  res.json();
+    } else {
+    }
+});
 
 // Export the router for app to use.
 module.exports = router;
--- src/components/components/AITutor.vue
+++ src/components/components/AITutor.vue
@@ -3,14 +3,49 @@
 
 export default {
     data() {
-        return {};
+        return {
+            message: ''
+        };
     },
     mounted() {},
     computed: {},
-    methods: {}
+    methods: {
+        SendMessage() {
+            const requestOptions = {
+                method: 'POST',
+                headers: { 'Content-Type': 'application/json' },
+                body: JSON.stringify({
+                    message: this.message
+                })
+            };
+            var url = '/ai-tutor';
+            fetch(url, requestOptions)
+                .then(function (response) {
+                    return response.json();
+                })
+                .then(function (data) {
+                    console.log(data);
+                    for (let i = 0; i < data.message.length; i++) {
+                        console.log(data.message[i].content[0].text.value);
+                    }
+                });
+        }
+    }
 };
 </script>
 
-<template>test</template>
+<template>
+    <div class="container mt-3">
+        <h2 class="heading">Tutor</h2>
+        <div class="row">
+            <div class="mb-3">
+                <textarea v-model="message" type="text" class="form-control" />
+            </div>
+            <button class="btn primary-btn" @click="SendMessage()">
+                Submit
+            </button>
+        </div>
+    </div>
+</template>
 
-<style></style>
+<style scoped></style>
