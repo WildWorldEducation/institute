13b715cd6cf010b7af562135625f06287010fc04
RussiSunni <jonathandyason@hotmail.com>
2025-02-05 12:42:10 +0200

    .

--- server/utilities/save-image-to-aws.js
+++ server/utilities/save-image-to-aws.js
@@ -1,9 +1,15 @@
 // S3 needs access to the .env variables
-const { S3Client, PutObjectCommand, CopyObjectCommand, waitUntilObjectExists, } = require('@aws-sdk/client-s3');
+const {
+    S3Client,
+    PutObjectCommand,
+    CopyObjectCommand,
+    waitUntilObjectExists
+} = require('@aws-sdk/client-s3');
 require('dotenv').config();
 const skillInfoboxImagesBucketName =
     process.env.S3_SKILL_INFOBOX_IMAGE_BUCKET_NAME;
-const userAvatarImageThumbnailsBucketName = process.env.S3_USER_AVATAR_IMAGE_THUMBNAILS_BUCKET_NAME;
+const userAvatarImageThumbnailsBucketName =
+    process.env.S3_USER_AVATAR_IMAGE_THUMBNAILS_BUCKET_NAME;
 const userAvatarImagesBucketName = process.env.S3_USER_AVATAR_IMAGE_BUCKET_NAME;
 const skillInfoboxImageThumbnailsBucketName =
     process.env.S3_SKILL_INFOBOX_IMAGE_THUMBNAILS_BUCKET_NAME;
@@ -24,22 +30,18 @@ const sharp = require('sharp');
  * @param {*} iconImage data file string of the image
  * @param {*} skillUrl key and name of the image must be unique
  * @param {*} editUUID additional name if the image is an edit version
- * 
+ *
  * @return the name of the image that got saved to database Or null if pass no image
  */
 const saveIconToAWS = async (iconImage, skillUrl, editUUID) => {
     if (!iconImage) {
-        return null
+        return null;
     }
     // Get file from Base64 encoding (client sends as base64)
     let fileData = Buffer.from(
-        iconImage.replace(
-            /^data:image\/\w+;base64,/,
-            ''
-        ),
+        iconImage.replace(/^data:image\/\w+;base64,/, ''),
         'base64'
     );
-
 
     const imageName = `${skillUrl}${editUUID ? `_${editUUID}` : ''}`;
     let fullSizeData = {
@@ -54,9 +56,7 @@ const saveIconToAWS = async (iconImage, skillUrl, editUUID) => {
     };
 
     // Send to the bucket.
-    const fullSizeCommand = new PutObjectCommand(
-        fullSizeData
-    );
+    const fullSizeCommand = new PutObjectCommand(fullSizeData);
     await s3.send(fullSizeCommand);
 
     const thumbnailFileData = await sharp(fileData)
@@ -75,13 +75,11 @@ const saveIconToAWS = async (iconImage, skillUrl, editUUID) => {
     };
 
     // Send to the bucket.
-    const thumbnailCommand = new PutObjectCommand(
-        thumbnailData
-    );
-    await s3.send(thumbnailCommand)
+    const thumbnailCommand = new PutObjectCommand(thumbnailData);
+    await s3.send(thumbnailCommand);
 
-    return imageName
-}
+    return imageName;
+};
 /**
  * Copy an image with another image that already stored in AWS
  *
@@ -90,13 +88,13 @@ const saveIconToAWS = async (iconImage, skillUrl, editUUID) => {
  */
 const updateSkillIcon = async (sourceImageURL, destinationImageURL) => {
     try {
-        // Copy Icon 
+        // Copy Icon
         await s3.send(
             new CopyObjectCommand({
                 CopySource: `${skillInfoboxImagesBucketName}/${sourceImageURL}`,
                 Bucket: skillInfoboxImagesBucketName,
-                Key: destinationImageURL,
-            }),
+                Key: destinationImageURL
+            })
         );
 
         // Copy Thumbnail
@@ -104,19 +102,18 @@ const updateSkillIcon = async (sourceImageURL, destinationImageURL) => {
             new CopyObjectCommand({
                 CopySource: `${skillInfoboxImageThumbnailsBucketName}/${sourceImageURL}`,
                 Bucket: skillInfoboxImageThumbnailsBucketName,
-                Key: destinationImageURL,
-            }),
-        )
+                Key: destinationImageURL
+            })
+        );
 
         // Copy Thumbnails
     } catch (err) {
         console.error(err);
     }
-
-}
+};
 
 /**
- * Add skill icon base64 string into AWS 
+ * Add skill icon base64 string into AWS
  *
  * @param {*} base64Image - base64 string of the image
  * @param {*} imageName - name of the image to store in AWS
@@ -126,22 +123,24 @@ const saveBase64ImageToBucket = async (base64Image, imageName, bucket) => {
     if (!base64Image) return null;
 
     // Validate base64 format and extract MIME type
-    console.log('from ai')
-    console.log(base64Image.substring(0, 50))
-    console.log(imageName);
     const match = base64Image.match(/^data:(image\/(jpeg|png));base64,/);
 
     if (!match) {
-        throw new Error('Invalid Base64 image format or unsupported image type (only JPEG and PNG are allowed)');
+        throw new Error(
+            'Invalid Base64 image format or unsupported image type (only JPEG and PNG are allowed)'
+        );
     }
 
     const contentType = match[1]; // e.g., 'image/jpeg' or 'image/png'
-    const fileData = Buffer.from(base64Image.replace(/^data:image\/\w+;base64,/, ''), 'base64');
+    const fileData = Buffer.from(
+        base64Image.replace(/^data:image\/\w+;base64,/, ''),
+        'base64'
+    );
 
     if (!imageName) throw new Error('Image name is required');
     if (!bucket) throw new Error('Bucket name is required');
 
-    console.log(fileData)
+    console.log(fileData);
     const imageData = {
         Key: imageName,
         Body: fileData,
@@ -166,18 +165,26 @@ const saveUserAvatarToAWS = async (userId, base64Image) => {
 
     // Extract MIME type and decode Base64
 
-
     const match = base64Image.match(/^data:(image\/(jpeg|png));base64,/);
     if (!match) {
-        throw new Error('Invalid Base64 image format or unsupported image type (only JPEG and PNG are allowed)');
+        throw new Error(
+            'Invalid Base64 image format or unsupported image type (only JPEG and PNG are allowed)'
+        );
     }
 
     const contentType = match[1]; // 'image/jpeg' or 'image/png'
-    const fileData = Buffer.from(base64Image.replace(/^data:image\/\w+;base64,/, ''), 'base64');
+    const fileData = Buffer.from(
+        base64Image.replace(/^data:image\/\w+;base64,/, ''),
+        'base64'
+    );
 
     try {
         // Save original avatar image
-        await saveBase64ImageToBucket(base64Image, userId, userAvatarImagesBucketName);
+        await saveBase64ImageToBucket(
+            base64Image,
+            userId,
+            userAvatarImagesBucketName
+        );
 
         // Generate thumbnail using sharp
         const thumbnailBuffer = await sharp(fileData)
@@ -185,11 +192,16 @@ const saveUserAvatarToAWS = async (userId, base64Image) => {
             .toBuffer();
 
         // Convert thumbnail buffer to Base64 string
-        const thumbnailBase64 = `data:${contentType};base64,${thumbnailBuffer.toString('base64')}`;
-
+        const thumbnailBase64 = `data:${contentType};base64,${thumbnailBuffer.toString(
+            'base64'
+        )}`;
 
         // Save thumbnail image
-        await saveBase64ImageToBucket(thumbnailBase64, userId, userAvatarImageThumbnailsBucketName);
+        await saveBase64ImageToBucket(
+            thumbnailBase64,
+            userId,
+            userAvatarImageThumbnailsBucketName
+        );
 
         return userId;
     } catch (error) {
@@ -198,5 +210,9 @@ const saveUserAvatarToAWS = async (userId, base64Image) => {
     }
 };
 
-
-module.exports = { saveIconToAWS, updateSkillIcon, saveBase64ImageToBucket, saveUserAvatarToAWS }
+module.exports = {
+    saveIconToAWS,
+    updateSkillIcon,
+    saveBase64ImageToBucket,
+    saveUserAvatarToAWS
+};
