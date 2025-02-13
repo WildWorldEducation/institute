d1918b7efd2d7969c8ce9d0eebe9dd4ae108c159
RussiSunni <jonathandyason@hotmail.com>
2025-02-13 11:50:35 +0200

    .

--- dist/assets/ShowSkillStore-167e449f.js
+++
@@ -1,1 +0,0 @@
-import{q as i}from"./main-c27f96a5.js";const o=i("showSkill",{state:()=>({skill:null}),actions:{async findSkill(s){const l=await fetch("/skills/url/"+s);this.skill=await l.json()}}});export{o as u};
