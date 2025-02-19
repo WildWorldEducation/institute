89caf83504c9c4efa41eedd3f0287f87c52474b8
RussiSunni <jonathandyason@hotmail.com>
2025-02-19 13:44:02 +0200

    .

---
+++ dist/assets/AssessmentsStore-9aa095b0.js
@@ -0,0 +1,1 @@
+import{q as t}from"./main-430b8aab.js";const n=t("assessments",{state:()=>({assessments:[]}),actions:{async getAssessments(){const s=await(await fetch("/assessments/list")).json();return this.assessments=s,this.$state},async deleteAssessment(e){const s=await fetch("/assessments/"+e,{method:"DELETE"});s.error&&console.log(s.error)}}});export{n as u};
--- dist/assets/ShowSkillStore-6e59107d.js
+++
@@ -1,1 +0,0 @@
-import{q as i}from"./main-f9c6bfb7.js";const o=i("showSkill",{state:()=>({skill:null}),actions:{async findSkill(s){const l=await fetch("/skills/url/"+s);this.skill=await l.json()}}});export{o as u};
