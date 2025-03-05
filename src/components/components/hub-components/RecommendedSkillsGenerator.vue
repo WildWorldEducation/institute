476d9203c99d514ba5d437eb21e30c66f347acaa
Nguyen Viet Tan <tannguyen1220@gmail.com>
2025-03-05 17:44:48 +0700

    smaller remove btn

--- src/components/components/hub-components/RecommendedSkillsGenerator.vue
+++ src/components/components/hub-components/RecommendedSkillsGenerator.vue
@@ -197,6 +197,7 @@ export default {
                 v-for="(
                     recommendedSkill, index
                 ) in recommendedSkillsOrderedByRelevance"
+                class="d-flex flex-row align-items-end"
             >
                 <router-link
                     v-if="sessionDetailsStore.isLoggedIn"
@@ -225,19 +226,21 @@ export default {
                 </router-link>
                 <!-- Delete button -->
                 <button
+                    b-tooltip.hover
+                    tile="remove skill from learning track"
                     v-if="sessionDetailsStore.isLoggedIn"
-                    class="btn red-btn"
+                    class="red-btn small-btn"
                     @click="removeRecommendedSkill(index)"
                 >
                     <svg
                         xmlns="http://www.w3.org/2000/svg"
-                        viewBox="0 0 448 512"
-                        height="18"
-                        class="tertiary-icon"
+                        viewBox="0 0 384 512"
+                        fill="white"
+                        height="11"
+                        width="11"
                     >
-                        <!--! Font Awesome Free 6.7.2 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license/free Copyright 2025 Fonticons, Inc. -->
                         <path
-                            d="M135.2 17.7L128 32 32 32C14.3 32 0 46.3 0 64S14.3 96 32 96l384 0c17.7 0 32-14.3 32-32s-14.3-32-32-32l-96 0-7.2-14.3C307.4 6.8 296.3 0 284.2 0L163.8 0c-12.1 0-23.2 6.8-28.6 17.7zM416 128L32 128 53.2 467c1.6 25.3 22.6 45 47.9 45l245.8 0c25.3 0 46.3-19.7 47.9-45L416 128z"
+                            d="M342.6 150.6c12.5-12.5 12.5-32.8 0-45.3s-32.8-12.5-45.3 0L192 210.7 86.6 105.4c-12.5-12.5-32.8-12.5-45.3 0s-12.5 32.8 0 45.3L146.7 256 41.4 361.4c-12.5 12.5-12.5 32.8 0 45.3s32.8 12.5 45.3 0L192 301.3 297.4 406.6c12.5 12.5 32.8 12.5 45.3 0s12.5-32.8 0-45.3L237.3 256 342.6 150.6z"
                         />
                     </svg>
                 </button>
@@ -259,6 +262,17 @@ export default {
     border: 1px solid black;
     background-color: #5f31dd;
     color: white;
+}
+
+.small-btn {
+    height: 20px !important;
+    width: 20px !important;
+    margin-bottom: 3px;
+    border: none;
+    border-radius: 5px;
+    display: flex;
+    justify-content: center;
+    align-items: center;
 }
 
 .search-bar {
