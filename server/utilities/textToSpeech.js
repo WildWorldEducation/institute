98dcccb821d870183e7affaf339f340b8afab4c6
RussiSunni <jonathandyason@hotmail.com>
2025-03-09 09:01:08 +0200

    .

--- server/utilities/textToSpeech.js
+++ server/utilities/textToSpeech.js
@@ -7,13 +7,13 @@ const openai = new OpenAI({
     apiKey: process.env.OPENAI_API_KEY
 });
 
-async function textToSpeech() {
+async function textToSpeech(mostRecentMessage) {
     const speechFile = path.resolve('./speech.mp3');
 
     const mp3 = await openai.audio.speech.create({
         model: 'tts-1',
-        voice: 'alloy',
-        input: 'Today is a wonderful day to build something people love!'
+        voice: 'fable',
+        input: mostRecentMessage
     });
 
     const buffer = Buffer.from(await mp3.arrayBuffer());
