9e865c598bbcf07053ba6ad248e72f05361a1def
RussiSunni <jonathandyason@hotmail.com>
2025-03-14 16:59:46 +0200

    .

--- server/config/socketConfig.js
+++ server/config/socketConfig.js
@@ -1,42 +1,42 @@
 const { Server } = require('socket.io');
 const {
-    getSkillThread,
+    // getSkillThread,
     createRunStream,
-    createAssistantAndThread,
-    saveAITutorSkillThread,
+    //  createAssistantAndThread,
+    //  saveAITutorSkillThread,
     createAssessmentStream
 } = require('../utilities/openAIAssistant');
 
 let io = null;
 
-const getAssistantData = async (
-    userId,
-    skillId,
-    skillName,
-    skillLevel,
-    skillUrl
-) => {
-    const assistantData = await getSkillThread(skillId, userId, skillUrl);
-    if (assistantData.length === 0) {
-        const newAssistant = await createAssistantAndThread(
-            skillName,
-            skillLevel
-        );
-        const newAssistantData = {
-            userId: userId,
-            skillUrl: skillUrl,
-            assistantId: newAssistant.assistant.id,
-            threadId: newAssistant.thread.id
-        };
-        await saveAITutorSkillThread(newAssistantData);
-        return newAssistantData;
-    } else {
-        return {
-            assistantId: assistantData[0].assistant_id,
-            threadId: assistantData[0].thread_id
-        };
-    }
-};
+// const getAssistantData = async (
+//     userId,
+//     skillId,
+//     skillName,
+//     skillLevel,
+//     skillUrl
+// ) => {
+//     const assistantData = await getSkillThread(skillId, userId, skillUrl);
+//     if (assistantData.length === 0) {
+//         const newAssistant = await createAssistantAndThread(
+//             skillName,
+//             skillLevel
+//         );
+//         const newAssistantData = {
+//             userId: userId,
+//             skillUrl: skillUrl,
+//             assistantId: newAssistant.assistant.id,
+//             threadId: newAssistant.thread.id
+//         };
+//         await saveAITutorSkillThread(newAssistantData);
+//         return newAssistantData;
+//     } else {
+//         return {
+//             assistantId: assistantData[0].assistant_id,
+//             threadId: assistantData[0].thread_id
+//         };
+//     }
+// };
 
 const createSocket = (server) => {
     io = new Server(server);
