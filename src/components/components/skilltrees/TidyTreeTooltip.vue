6c1075fe025ede5aaa1f0cef452c25f835d43813
TanNguyen120 <tannguyen1220@gmail.com>
2025-03-24 19:46:23 +0700

    tooltip style

--- src/components/components/skilltrees/TidyTreeTooltip.vue
+++ src/components/components/skilltrees/TidyTreeTooltip.vue
@@ -9,19 +9,39 @@ export default {
     },
     computed: {},
     async mounted() {},
-    methods: {},
+    methods: {
+        // getting first paragraph from html string
+        getFirstParagraph(htmlString) {
+            const el = document.createElement('html');
+            el.innerHTML = htmlString;
+            ``;
+            const listOfParagraph = el.getElementsByTagName('p');
+
+            return listOfParagraph[0].innerText;
+        },
+        snakeToTile(string) {
+            const result = string.replace(/^_*(.)|_+(.)/g, (s, c, d) =>
+                c ? c.toUpperCase() : ' ' + d.toUpperCase()
+            );
+            return result;
+        }
+    },
     watch: {
         tooltipData: {
             deep: true,
-            handler(newItem) {
-                console.log(newItem);
+            handler(newItem, oldItem) {
+                if (newItem.skillId === oldItem.skillId) {
+                    return;
+                }
+                console.log('getting call');
                 fetch(`/skills/introduction-data?skillId=${newItem.skillId}`)
                     .then((res) => {
                         return res.json();
                     })
                     .then((result) => {
-                        console.log(result[0].introduction);
-                        this.introduction = result[0].introduction;
+                        const fullIntroduction = result[0].introduction;
+                        this.introduction =
+                            this.getFirstParagraph(fullIntroduction);
                     });
             }
         }
@@ -35,29 +55,31 @@ export default {
             <div class="d-flex tooltip-header">
                 <img
                     :src="tooltipData.thumbnail"
-                    class="rounded img-fluid mx-auto skill-thumbnail"
+                    class="rounded-2 skill-thumbnail"
                     @error="imageUrlAlternative"
-                    height="30"
-                    width="100"
+                    height="220"
+                    width="220"
                 />
-                <div class="tooltip-skill-name d-flex flex-column">
-                    <h4>
+                <div class="tooltip-skill-name">
+                    <div class="skill-name">
                         {{ tooltipData.skillName }}
-                    </h4>
-                    <h6>{{ tooltipData.skillLevel }}</h6>
+                    </div>
+                    <div class="skill-level">
+                        {{ snakeToTile(tooltipData.skillLevel) }}
+                    </div>
                 </div>
             </div>
         </div>
         <div class="d-flex tooltip-skill-introduction">
-            <div v-html="introduction"></div>
+            <div>{{ introduction }}</div>
         </div>
     </div>
 </template>
 
 <style scoped>
 .tool-tip {
-    width: 200px;
-    height: 400px;
+    width: 400px;
+    height: 300px;
     position: absolute;
     display: flex;
     flex-direction: column;
@@ -67,28 +89,49 @@ export default {
     color: black;
     border-bottom-left-radius: 12px;
     border-bottom-right-radius: 12px;
+    border-radius: 10px;
 }
 
 .tooltip-skill-name {
     color: black;
-
-    padding: 30px 40px;
     margin-bottom: 0px;
+    margin-left: 10px;
 }
 
 .tooltip-header {
-    background: url('images/frameBorder.png');
-    background-size: 100% 100%;
-    padding: 10px;
+    background-color: white;
+    gap: 0px;
+    width: 400px;
+    border: 2px solid #e8ecf4;
+    border-top-left-radius: 10px;
+    border-top-right-radius: 10px;
+}
+
+.skill-level {
+    font-size: 0.8rem;
+    color: v-bind('tooltipData.borderColor');
 }
 
 .tooltip-skill-introduction {
-    background-color: #e8ecf4d7;
+    background-color: #e8ecf4ea;
+    padding: 10px 15px;
+    border-bottom-left-radius: 10px;
+    border-bottom-right-radius: 10px;
+}
+
+.skill-thumbnail {
+    padding: 2px;
 }
 
 .tooltip-skill-name-background {
     background-color: #ffffff;
     padding: 0px;
     display: flex;
+    width: 100%;
+}
+
+.skill-name {
+    font-size: 1.3rem;
+    overflow-wrap: break-word;
 }
 </style>
