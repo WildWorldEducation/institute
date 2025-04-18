52c7d63ae659c04084ae7e45f5ac0e9352e7c644
RussiSunni <jonathandyason@hotmail.com>
2025-04-18 13:56:24 +0200

    .

--- server/routes/subscriptions.js
+++ server/routes/subscriptions.js
@@ -35,8 +35,6 @@ router.post('/create-checkout-session', async (req, res) => {
         } else {
             priceId = process.env.INFINITE_PLAN_PRICE_ID;
         }
-        // console.log(req.body);
-        console.log(priceId);
 
         const session = await stripe.checkout.sessions.create({
             payment_method_types: ['card'],
