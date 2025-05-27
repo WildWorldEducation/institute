8169093b74aac45c5517f4b09339344631e9fb9e
RussiSunni <jonathandyason@hotmail.com>
2025-05-27 09:24:43 +0200

    .

--- src/components/components/ai-tutor/AITutor.vue
+++ src/components/components/ai-tutor/AITutor.vue
@@ -1791,7 +1791,8 @@ export default {
 
 /* Modal mode input */
 .modal-input {
-    padding: 8px 0;
+    padding-bottom: 8px;
+    padding-top: 4px;
     width: 100%;
     border-top: 1px solid #e0e0e0;
 }
