c659cd10e61f14f82f4ad29f1b50a26199c5b0d8
RussiSunni <jonathandyason@hotmail.com>
2025-06-24 17:42:25 +0200

    .

---
+++ src/components/pages/teacher-analytics/TrackingReportView.vue
@@ -0,0 +1,18 @@
+<script>
+export default {
+    setup() {},
+    components: {},
+    data() {
+        return {};
+    },
+    async created() {},
+
+    methods: {}
+};
+</script>
+
+<template>
+    <div class="container-fluid">tracking report</div>
+</template>
+
+<style></style>
