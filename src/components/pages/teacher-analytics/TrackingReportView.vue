c1d84a0f9049526e195ea62ef2aa452b0711f550
RussiSunni <jonathandyason@hotmail.com>
2025-06-25 09:42:01 +0200

    .

--- src/components/pages/teacher-analytics/TrackingReportView.vue
+++ src/components/pages/teacher-analytics/TrackingReportView.vue
@@ -6,13 +6,19 @@ export default {
         return {};
     },
     async created() {},
-
     methods: {}
 };
 </script>
 
 <template>
-    <div class="container-fluid">tracking report</div>
+    <div class="container-fluid">
+        <h1 class="heading">Tracking report</h1>
+
+        <h2>Skill activity</h2>
+        <h2>total time on platform</h2>
+        <h2>assessment status</h2>
+        <h2>progress</h2>
+    </div>
 </template>
 
 <style></style>
