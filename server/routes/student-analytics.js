0e634570d578dd3cb7adf8363d21249a814946e5
RussiSunni <jonathandyason@hotmail.com>
2025-06-27 13:19:20 +0200

    .

---
+++ server/routes/student-analytics.js
@@ -0,0 +1,47 @@
+/*------------------------------------------
+--------------------------------------------
+Middleware.
+--------------------------------------------
+--------------------------------------------*/
+const express = require('express');
+// Router.
+const router = express.Router();
+const bodyParser = require('body-parser');
+router.use(bodyParser.json());
+// DB
+const conn = require('../config/db');
+// for querying DB
+const util = require('util');
+const query = util.promisify(conn.query).bind(conn);
+
+/*------------------------------------------
+--------------------------------------------
+Routes
+--------------------------------------------
+--------------------------------------------*/
+router.get('/last-visited-skills/:studentId', async (req, res, next) => {
+    if (req.session.userName) {
+        let sqlQuery = `
+            SELECT skills.id, name, skills.url, level, icon, visited_at
+            FROM user_visited_skills
+            INNER JOIN skills 
+            ON skills.id = user_visited_skills.skill_id
+            WHERE user_id = ${conn.escape(req.params.studentId)}
+            AND skills.is_deleted = 0
+            ORDER BY visited_at DESC;
+        `;
+
+        conn.query(sqlQuery, (err, results) => {
+            if (err) return next(err); // Pass error to error handler
+
+            if (results.length === 0) {
+                return res.status(404).json({ error: 'No recent skills' });
+            }
+
+            res.json(results);
+        });
+    }
+});
+
+// Export the router for app to use.
+module.exports = router;
--- src/components/pages/teacher-analytics/SkillActivityReportView.vue
+++ src/components/pages/teacher-analytics/SkillActivityReportView.vue
@@ -3,16 +3,56 @@ export default {
     setup() {},
     components: {},
     data() {
-        return {};
+        return {
+            studentId: this.$route.params.studentId,
+            visitedSkills: []
+        };
     },
-    async created() {},
-    methods: {}
+    async created() {
+        this.getLastVisitedSkills();
+    },
+
+    methods: {
+        getLastVisitedSkills() {
+            // Fetch last visited skills for the student
+            fetch(`/student-analytics/last-visited-skills/${this.studentId}`)
+                .then((response) => response.json())
+                .then((data) => {
+                    this.visitedSkills = data;
+                })
+                .catch((error) => {
+                    console.error('Error fetching last visited skills:', error);
+                });
+        },
+        visitedDate(date) {
+            let jsDate = new Date(date);
+            return jsDate.toLocaleString('en-US', {
+                year: 'numeric',
+                month: '2-digit',
+                day: '2-digit',
+                hour: '2-digit',
+                minute: '2-digit',
+                second: '2-digit'
+            });
+        }
+    }
 };
 </script>
 
 <template>
     <div class="container">
         <h1 class="heading">Skill Activity Report</h1>
+        <div class="skill-list">
+            <h2>Last Visited Skills</h2>
+            <ul>
+                <li v-for="skill in visitedSkills" :key="skill.id">
+                    <router-link target="_blank" :to="'/skills/' + skill.url">{{
+                        skill.name
+                    }}</router-link>
+                    {{ visitedDate(skill.visited_at) }}
+                </li>
+            </ul>
+        </div>
     </div>
 </template>
 
