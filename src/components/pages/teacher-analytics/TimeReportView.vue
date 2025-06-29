e693e66e1de763380987616524b27553cd3d3893
RussiSunni <jonathandyason@hotmail.com>
2025-06-29 19:30:59 +0200

    .

--- src/components/pages/teacher-analytics/TimeReportView.vue
+++ src/components/pages/teacher-analytics/TimeReportView.vue
@@ -1,13 +1,28 @@
 <script>
+import { useUsersStore } from '../../../stores/UsersStore';
 export default {
-    setup() {},
+    setup() {
+        const usersStore = useUsersStore();
+        return {
+            usersStore
+        };
+    },
     data() {
         return {
             studentId: this.$route.params.studentId,
+            studentName: null,
             skillDurations: []
         };
     },
     async created() {
+        if (this.usersStore.users.length < 1) await this.usersStore.getUsers();
+        const foundObject = this.usersStore.users.find(
+            (student) => student.id === this.studentId
+        );
+        if (foundObject) {
+            this.studentName = foundObject.username;
+        }
+
         this.getSkillDuration();
     },
     methods: {
@@ -28,7 +43,34 @@ export default {
 
 <template>
     <div class="container">
-        <h1 class="heading">Time Report</h1>
+        <h1 class="heading">Time Report: {{ studentName }}</h1>
+        <div v-if="this.skillDurations.length > 0" class="mb-4">
+            <table class="table">
+                <tr>
+                    <th>Skill</th>
+                    <th>Duration</th>
+                </tr>
+                <tr
+                    v-for="skillDuration in skillDurations"
+                    :key="skillDuration.id"
+                    class="table-rows"
+                >
+                    <td>
+                        <router-link
+                            target="_blank"
+                            :to="'/skills/' + skillDuration.url"
+                            >{{ skillDuration.name }}</router-link
+                        >
+                    </td>
+                    <td>
+                        {{ skillDuration.duration }}
+                    </td>
+                </tr>
+            </table>
+        </div>
+        <div v-else>
+            <p>No skills visited by this student.</p>
+        </div>
     </div>
 </template>
 
