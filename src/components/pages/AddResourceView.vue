1072f58f0540e3f767b11d61cd34a7604a995b07
RussiSunni <jonathandyason@hotmail.com>
2025-07-01 13:10:33 +0200

    .

--- src/components/pages/AddResourceView.vue
+++ src/components/pages/AddResourceView.vue
@@ -1,10 +1,26 @@
-<script setup>
-import AddResource from '../components/AddResource.vue'
+<script>
+import AddResource from '../components/AddResource.vue';
+import SkillTimeTracker from '../components/student-analytics/SkillTimeTracker.vue';
+export default {
+    setup() {},
+    data() {
+        return {};
+    },
+    components: {
+        AddResource,
+        SkillTimeTracker
+    },
+    beforeRouteLeave(to, from, next) {
+        this.$refs.skillTimeTracker.saveDuration();
+        next();
+    }
+};
 </script>
 
 <template>
     <AddResource />
+    <!-- To track student time for this skill -->
+    <SkillTimeTracker ref="skillTimeTracker" />
 </template>
-  
+
 <style></style>
-  
