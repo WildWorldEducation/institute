d65b2cc94692c91bdedad2bff900f494f34eaff9
RussiSunni <jonathandyason@hotmail.com>
2025-07-06 10:51:02 +0200

    .

--- src/components/pages/teacher-analytics/ProgressReportView.vue
+++ src/components/pages/teacher-analytics/ProgressReportView.vue
@@ -15,6 +15,7 @@ export default {
         <h1 class="heading">Progress Report</h1>
         <p>Number of skills mastered over time</p>
         <p>Line chart</p>
+        <p>May have to record when the user started on the platform</p>
     </div>
 </template>
 
