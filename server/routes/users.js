d086c6abed4a6a3cd43bf7e990c3b50152170af1
RussiSunni <jonathandyason@hotmail.com>
2025-07-14 12:22:46 +0200

    .

--- server/routes/users.js
+++ server/routes/users.js
@@ -911,7 +911,7 @@ router.get('/show/:id', (req, res, next) => {
         // Note: avatar has query param to deal with image caching by browser,
         // in case image is changed.
         let sqlQuery = `
-    SELECT users.id, first_name, last_name, username,
+    SELECT users.id, tenant_id, first_name, last_name, username,
     CONCAT('https://${userAvatarImagesBucketName}.s3.${bucketRegion}.amazonaws.com/', users.id, '?v=', UNIX_TIMESTAMP()) AS avatar, 
     email, role, is_deleted, is_google_auth, grade_filter, theme,
     is_language_filter, is_math_filter, is_history_filter, is_life_filter, is_computer_science_filter, is_science_and_invention_filter, is_dangerous_ideas_filter, reputation_score, is_unlocked_skills_only_filter, cohort_id, is_audio_auto_play, tokens
@@ -1109,7 +1109,8 @@ router.put(
             username = ${conn.escape(req.body.username)}, 
             email = ${conn.escape(req.body.email)}, 
             password = ${conn.escape(req.body.password)},
-            role = ${conn.escape(req.body.role)} 
+            role = ${conn.escape(req.body.role)},
+            tenant_id = ${conn.escape(req.body.tenant_id)}
             WHERE id = ${conn.escape(req.params.id)};`;
 
             conn.query(sqlQuery, async (err) => {
