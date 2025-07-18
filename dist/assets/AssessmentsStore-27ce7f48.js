a97c3f8b1bf79c1edf3834a68743da67a665fb9e
RussiSunni <jonathandyason@hotmail.com>
2025-07-18 14:48:07 +0200

    .

--- dist/assets/AssessmentsStore-27ce7f48.js
+++
@@ -1,1 +0,0 @@
-import{x as t}from"./main-2a4db0ea.js";const n=t("assessments",{state:()=>({assessments:[]}),actions:{async getAssessments(){const s=await(await fetch("/assessments/list")).json();return this.assessments=s,this.$state},async deleteAssessment(e){const s=await fetch("/assessments/"+e,{method:"DELETE"});s.error&&console.log(s.error)}}});export{n as u};
