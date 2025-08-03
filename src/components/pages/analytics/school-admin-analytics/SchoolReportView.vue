1c20ae476cf59467fc042668741a8ded2d037737
jonathan <jonathandyason@hotmail.com>
2025-08-03 16:03:40 +0200

    .

--- src/components/pages/analytics/school-admin-analytics/SchoolReportView.vue
+++ src/components/pages/analytics/school-admin-analytics/SchoolReportView.vue
@@ -55,7 +55,7 @@ export default {
             avgTokensToMasterSkills: [],
             totalTokensPerSkill: [],
             totalTokensPerDay: [],
-            isDataWeekly: false
+            isDataLastWeek: false
         };
     },
     async created() {
@@ -375,6 +375,9 @@ export default {
             link.href = URL.createObjectURL(blob);
             link.download = name + '.csv';
             link.click();  
+        },
+        toggleWeeklyCumulativeData(){
+            this.isDataLastWeek = !this.isDataLastWeek
         }
     }
 };
@@ -463,6 +466,9 @@ export default {
                 >
             </div> -->
             <h4 class="d-flex justify-content-between">Time spent on platform per day 
+                <span>
+                <button v-if="!isDataLastWeek" @click="toggleWeeklyCumulativeData" class="btn">Last week</button>
+                <button v-else @click="toggleWeeklyCumulativeData" class="btn">Cumulative</button>
                 <button 
                     class="btn"
                     @click="downloadData(durationPerDay, 'Time-per-day')">
@@ -472,7 +478,8 @@ export default {
                         height="18">
                         <!-- Font Awesome Free v7.0.0 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license/free Copyright 2025 Fonticons, Inc. --> <path d="M0 64C0 28.7 28.7 0 64 0L213.5 0c17 0 33.3 6.7 45.3 18.7L365.3 125.3c12 12 18.7 28.3 18.7 45.3L384 448c0 35.3-28.7 64-64 64L64 512c-35.3 0-64-28.7-64-64L0 64zm208-5.5l0 93.5c0 13.3 10.7 24 24 24L325.5 176 208 58.5zM175 441c9.4 9.4 24.6 9.4 33.9 0l64-64c9.4-9.4 9.4-24.6 0-33.9s-24.6-9.4-33.9 0l-23 23 0-86.1c0-13.3-10.7-24-24-24s-24 10.7-24 24l0 86.1-23-23c-9.4-9.4-24.6-9.4-33.9 0s-9.4 24.6 0 33.9l64 64z"/>
                     </svg>
-            </button>
+                </button>
+                </span>
             </h4>
             <TenantDurationPerDayLineChart
                 v-if="durationPerDay.length > 0"
