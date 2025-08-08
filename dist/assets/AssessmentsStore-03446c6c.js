1409bf91908a76e3ba18bc8b9be81a3058a60d25
RussiSunni <jonathandyason@hotmail.com>
2025-08-08 18:01:19 +0200

    .

--- dist/assets/AssessmentsStore-03446c6c.js
+++
@@ -1,5 +0,0 @@
-<<<<<<<< HEAD:dist/assets/AssessmentsStore-03446c6c.js
-import{x as t}from"./main-91882d5c.js";const n=t("assessments",{state:()=>({assessments:[]}),actions:{async getAssessments(){const s=await(await fetch("/assessments/list")).json();return this.assessments=s,this.$state},async deleteAssessment(e){const s=await fetch("/assessments/"+e,{method:"DELETE"});s.error&&console.log(s.error)}}});export{n as u};
-========
-import{x as t}from"./main-4609db7c.js";const n=t("assessments",{state:()=>({assessments:[]}),actions:{async getAssessments(){const s=await(await fetch("/assessments/list")).json();return this.assessments=s,this.$state},async deleteAssessment(e){const s=await fetch("/assessments/"+e,{method:"DELETE"});s.error&&console.log(s.error)}}});export{n as u};
->>>>>>>> dev:dist/assets/AssessmentsStore-8a4ba33b.js
--- dist/assets/InstructorStudentsStore-8597a5a6.js
+++
@@ -1,5 +0,0 @@
-<<<<<<<< HEAD:dist/assets/InstructorStudentsStore-8597a5a6.js
-import{x as s}from"./main-91882d5c.js";const e=s("instructorStudents",{state:()=>({instructorStudentsList:[]}),actions:{async getInstructorStudentsList(){const t=await(await fetch("/instructor-students/list")).json();return this.instructorStudentsList=t,this.$state}}});export{e as u};
-========
-import{x as s}from"./main-4609db7c.js";const e=s("instructorStudents",{state:()=>({instructorStudentsList:[]}),actions:{async getInstructorStudentsList(){const t=await(await fetch("/instructor-students/list")).json();return this.instructorStudentsList=t,this.$state}}});export{e as u};
->>>>>>>> dev:dist/assets/InstructorStudentsStore-e99f47e4.js
