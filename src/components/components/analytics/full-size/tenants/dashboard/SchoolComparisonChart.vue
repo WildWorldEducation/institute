79ad5c02f8d64b8081e60c65099c9264ee5dd4e5
RussiSunni <jonathandyason@hotmail.com>
2025-08-28 12:00:04 +0200

    .

--- src/components/components/analytics/full-size/tenants/dashboard/SchoolComparisonChart.vue
+++ src/components/components/analytics/full-size/tenants/dashboard/SchoolComparisonChart.vue
@@ -3,7 +3,7 @@ import * as d3 from 'd3';
 
 export default {
     name: 'SchoolComparisonChart',
-    props: ['data', 'colour'],
+    props: ['colour'],
     data() {
         return {};
     },
@@ -62,26 +62,6 @@ export default {
                 .attr('width', (d) => x(d.quantity) - x(0))
                 .attr('height', y.bandwidth());
 
-            // Append a label for each name.
-            svg.append('g')
-                .attr('fill', 'white')
-                .attr('text-anchor', 'end')
-                .selectAll()
-                .data(data)
-                .join('text')
-                .attr('x', (d) => x(d.quantity))
-                .attr('y', (d) => y(d.name) + y.bandwidth() / 2)
-                .attr('dy', '0.35em')
-                .attr('dx', -4)
-                .text((d) => format(d.quantity))
-                .call((text) =>
-                    text
-                        .filter((d) => x(d.quantity) - x(0) < 20) // short bars
-                        .attr('dx', +4)
-                        .attr('fill', 'black')
-                        .attr('text-anchor', 'start')
-                );
-
             // Create the axes.
             svg.append('g')
                 .attr('transform', `translate(0,${marginTop})`)
@@ -90,6 +70,7 @@ export default {
 
             svg.append('g')
                 .attr('transform', `translate(${marginLeft},0)`)
+                .style('font', '20px sans-serif')
                 .call(d3.axisLeft(y).tickSizeOuter(0));
         }
     }
