c9d0eafd67ae95cfbf6a9540b283321a58fc87ac
RussiSunni <jonathandyason@hotmail.com>
2025-09-03 10:00:33 +0200

    .

--- src/components/components/analytics/full-size/students/StudentFailedAssessmentsByRootSubjectHorizontalBarChart.vue
+++ src/components/components/analytics/full-size/students/StudentFailedAssessmentsByRootSubjectHorizontalBarChart.vue
@@ -8,21 +8,17 @@ export default {
         return {};
     },
     mounted() {
-        const container = d3.select(
-            '#student-failed-assessments-by-root-subject-chart-container'
-        );
-      
         // Specify the chart’s dimensions, based on a bar’s height.
-        const barHeight = 25;
         const marginTop = 0;
         const marginRight = 0;
-        const marginBottom = 10;
-        const marginLeft = 300;
-        const width = 700;
-        const height =
-            Math.ceil((this.data.length + 0.1) * barHeight) +
-            marginTop +
-            marginBottom;
+        const marginBottom = 0;
+        const marginLeft = 200;
+        const width = document.getElementById(
+            'fails-by-subject-chart-container'
+        ).clientWidth;
+        const height = document.getElementById(
+            'fails-by-subject-chart-container'
+        ).clientHeight;
 
         // Create the scales.
         const x = d3
@@ -34,35 +30,41 @@ export default {
             .scaleBand()
             .domain(d3.sort(this.data, (d) => -d.quantity).map((d) => d.name))
             .rangeRound([marginTop, height - marginBottom])
-            .padding(0.1);
+            .padding(0.2);
 
         // Create a value format.
         const format = x.tickFormat(20);
 
         // Create the SVG container.
         const svg = d3
-            .select(
-                '#student-failed-assessments-by-root-subject-chart-container'
-            )
+            .select('#fails-by-subject-chart-container')
             .append('svg')
-            .attr('width', width)
-            .attr('height', height)
-            .attr('viewBox', [0, 0, width, height])
-            .attr(
-                'style',
-                'max-width: 100%; height: 100%; font: 14px sans-serif;'
-            );
+            .attr('width', '100%')
+            .attr('height', '100%')
+            .attr('viewBox', [0, 0, '100%', '100%'])
+            .attr('preserveAspectRatio', 'xMinYMin');
 
         // Append a rect for each skill.
         svg.append('g')
-            .attr('fill', this.colour)
             .selectAll()
             .data(this.data)
             .join('rect')
             .attr('x', x(0))
             .attr('y', (d) => y(d.name))
             .attr('width', (d) => x(d.quantity) - x(0))
-            .attr('height', y.bandwidth());
+            .attr('height', y.bandwidth())
+            .attr('fill', function (d) {
+                if (d.name == 'Language') return 'DarkMagenta'; // turquoise
+                else if (d.name == 'Mathematics') return 'blue'; // blue
+                else if (d.name == 'Science & Invention')
+                    return 'purple'; // purple
+                else if (d.name == 'Computer Science')
+                    return 'deeppink'; // pink
+                else if (d.name == 'Dangerous Ideas') return 'brown'; // brown
+                else if (d.name == 'History') return 'cyan'; // cyan
+                else if (d.name == 'Life') return 'magenta'; // magenta
+                else return '#ff7f0e'; // orange
+            });
 
         // Append a label for each name.
         svg.append('g')
@@ -81,7 +83,7 @@ export default {
                     .filter((d) => x(d.quantity) - x(0) < 20) // short bars
                     .attr('dx', +4)
                     .attr('fill', 'white')
-                    .attr('text-anchor', 'start')                    
+                    .attr('text-anchor', 'start')
             );
 
         // Create the axes.
@@ -98,8 +100,6 @@ export default {
 };
 </script>
 
-<template>
-    <div id="student-failed-assessments-by-root-subject-chart-container"></div>
-</template>
+<template></template>
 
 <style scoped></style>
--- src/components/pages/analytics/teacher-analytics/student/StudentMultipleFailsReportView.vue
+++ src/components/pages/analytics/teacher-analytics/student/StudentMultipleFailsReportView.vue
@@ -55,50 +55,71 @@ export default {
 </script>
 
 <template>
-    <div class="container">
+    <div class="container chart-page">
         <span class="d-flex justify-content-between w-100">
             <h1 class="heading h4">Multiple fails</h1>
             <h2 class="tertiary-heading h4">{{ studentName }}</h2>
         </span>
-        <div class="row">
+        <div class="row h-100">
             <div class="col-md">
-                <h3>By subject</h3>
-                <StudentFailedAssessmentsByRootSubjectHorizontalBarChart
-                    v-if="
-                        analyticsStore.studentRootSubjectsFailedAssessments
-                            .length > 0
-                    "
-                    :data="analyticsStore.studentRootSubjectsFailedAssessments"
-                    colour="darkred"
-                    class="mb-5"
-                />
-                <p v-else>No data yet</p>
+                <h3 class="secondary-heading h5">By subject</h3>
+                <div id="fails-by-subject-chart-container">
+                    <StudentFailedAssessmentsByRootSubjectHorizontalBarChart
+                        v-if="
+                            analyticsStore.studentRootSubjectsFailedAssessments
+                                .length > 0
+                        "
+                        :data="
+                            analyticsStore.studentRootSubjectsFailedAssessments
+                        "
+                    />
+                    <p v-else>No data yet</p>
+                </div>
             </div>
 
             <div class="col-md position-relative">
-                <h3>By skill</h3>
-                <DownloadCSVBtn
-                    :data="teacherAnalyticsStore.studentMultipleFails"
-                    :fileName="`Assessment Status Report - ${studentName}`"
-                    toolTip="Download failed assessments data as CSV"
-                    class="position-absolute download-btn"
-                />
-                <FailedAssessmentsHorizontalBarChart
-                    v-if="teacherAnalyticsStore.studentMultipleFails.length > 0"
-                    :data="teacherAnalyticsStore.studentMultipleFails"
-                    colour="darkred"
-                    class="mb-5"
-                />
-                <p v-else>
-                    This student has not failed any assessments more than once
-                    yet.
-                </p>
+                <h3 class="secondary-heading h5">By skill</h3>
+                <div id="fails-by-skill-chart-container">
+                    <DownloadCSVBtn
+                        :data="teacherAnalyticsStore.studentMultipleFails"
+                        :fileName="`Assessment Status Report - ${studentName}`"
+                        toolTip="Download failed assessments data as CSV"
+                        class="position-absolute download-btn"
+                    />
+                    <FailedAssessmentsHorizontalBarChart
+                        v-if="
+                            teacherAnalyticsStore.studentMultipleFails.length >
+                            0
+                        "
+                        :data="teacherAnalyticsStore.studentMultipleFails"
+                        colour="darkred"
+                    />
+
+                    <p v-else>
+                        This student has not failed any assessments more than
+                        once yet.
+                    </p>
+                </div>
             </div>
         </div>
     </div>
 </template>
 
 <style scoped>
+.chart-page {
+    height: calc(100vh - 88px);
+    overflow: hidden;
+}
+
+.chart-row {
+    height: 50%;
+}
+
+#fails-by-subject-chart-container,
+#fails-by-skill-chart-container {
+    height: 80%;
+}
+
 .download-btn {
     right: 10px;
     top: 10px;
