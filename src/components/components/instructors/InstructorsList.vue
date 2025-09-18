010ee78b4529e7c0ff2674ec2a7dc245d1f506cd
RussiSunni <jonathandyason@hotmail.com>
2025-09-18 10:02:29 +0200

    .

--- src/components/components/instructors/InstructorsList.vue
+++ src/components/components/instructors/InstructorsList.vue
@@ -11,7 +11,8 @@ export default {
     props: ['instructors'],
     data() {
         return {
-            showInformationModal: false
+            showInformationModal: false,
+            isMobileCheck: window.innerWidth
         };
     },
     async created() {
@@ -99,6 +100,35 @@ export default {
                 >
                     {{ teacher.username }}
                 </button>
+                <router-link
+                    v-if="isMobileCheck < 576"
+                    :to="
+                        '/teachers/' + this.$parent.selectedTeacher.id + '/edit'
+                    "
+                    class="btn ms-2 edit-btn"
+                >
+                    <!-- Pencil icon -->
+                    <svg
+                        width="19"
+                        height="20"
+                        viewBox="0 0 19 20"
+                        fill="none"
+                        xmlns="http://www.w3.org/2000/svg"
+                    >
+                        <path
+                            d="M0.75558 19.3181C0.77635 19.5132 0.87137 19.6928 1.02096 19.8198C1.17055 19.9468 1.36325 20.0114 1.55915 20.0002L5.27701 19.8288L0.398438 15.6145L0.75558 19.3181Z"
+                            fill="white"
+                        />
+                        <path
+                            d="M11.8467 2.24484L0.801758 15.0315L5.6802 19.2454L16.7251 6.45877L11.8467 2.24484Z"
+                            fill="white"
+                        />
+                        <path
+                            d="M18.2555 3.11796L14.934 0.260817C14.832 0.172259 14.7134 0.104756 14.5852 0.0621907C14.4569 0.0196256 14.3215 0.00283902 14.1868 0.0127967C14.052 0.0227543 13.9205 0.0592596 13.7999 0.120212C13.6793 0.181165 13.572 0.265362 13.484 0.36796L12.4805 1.50725L17.359 5.71439L18.3519 4.56082C18.5289 4.35602 18.6181 4.08969 18.6 3.81958C18.582 3.54948 18.4582 3.29738 18.2555 3.11796Z"
+                            fill="white"
+                        />
+                    </svg>
+                </router-link>
             </div>
         </div>
         <!-- Add Teacher Link -->
@@ -109,6 +139,16 @@ export default {
 </template>
 
 <style scoped>
+.edit-btn {
+    background-color: var(--primary-color);
+    border: none;
+    border-radius: 8px;
+    padding: 10px 16px;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+}
+
 .cohort-buttons {
     font-family: 'Poppins', sans-serif;
     height: 80px;
