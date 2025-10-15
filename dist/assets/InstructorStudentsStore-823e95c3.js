04717c217ae8e8e5a140ccd2bff81892498a61df
jonathan <jonathandyason@hotmail.com>
2025-10-15 13:29:49 +0100

    .

---
+++ dist/assets/InstructorStudentsStore-823e95c3.js
@@ -0,0 +1,1 @@
+import{q as s}from"./main-c8c8d3f4.js";const e=s("instructorStudents",{state:()=>({instructorStudentsList:[]}),actions:{async getInstructorStudentsList(){const t=await(await fetch("/instructor-students/list")).json();return this.instructorStudentsList=t,this.$state}}});export{e as u};
